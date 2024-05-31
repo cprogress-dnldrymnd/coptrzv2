e600abf3942b693d90878b401bf0c4321bbdf92f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-31 14:37:50 +0800

    Update single-popups.php

--- single-popups.php
+++ single-popups.php
@@ -2,37 +2,23 @@
 <?php while (have_posts()) : the_post(); ?>
 
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal">
         Launch Popup
     </button>
     <!-- Modal -->
-    <div class="modal fade modal-v2" id="modalSearch" tabindex="-1" aria-labelledby="modalSearchLabel" aria-hidden="true">
+    <div class="modal fade modal-v2 popup-form" id="modal" tabindex="-1" aria-labelledby="modalSearchLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content background-white">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalSearchLabel">Search</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-body p-0">
+                    <div class="row">
+                        <div class="col-lg-6">
+
+                        </div>
+                        <div class="col-lg-6">
+                            <img src="<?= get_the_post_thumbnail_url() ?>" alt="">
+                        </div>
+                    </div>
                 </div>
-
-                <form action="/" method="GET">
-                    <div class="modal-body p-0">
-                        <input type="text" class="form-control" id="Search" name="s" placeholder="Enter text here.." required>
-                    </div>
-                    <div class="modal-footer">
-                        <div class="button-box button-secondary button-small">
-                            <button type="button" data-bs-dismiss="modal">Cancel</button>
-                        </div>
-
-
-                        <div class="button-box button-accent button-small">
-                            <button type="submit">
-                                Search
-                            </button>
-                        </div>
-
-                    </div>
-                </form>
-
             </div>
         </div>
     </div>
