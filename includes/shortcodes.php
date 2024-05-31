54aa01f72dd759ba18b699f4d105ce98836df134
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-31 15:30:07 +0800

    css

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1156,10 +1156,40 @@ class Shortcodes
 		</div>
 	<?php
 	}
+
+	function search_form()
+	{
+		$SVG = new SVG;
+
+		ob_start();
+	?>
+		<div class="search-form d-flex background-white">
+			<select name="eael-adv-search-cate-list">
+				<option value="">All Categories</option>
+				<option value="30">Accessories &amp; Parts</option>
+				<option value="1316">Approved Used</option>
+				<option value="28">Bundle Packages</option>
+				<option value="27">Drones</option>
+				<option value="789">E-learning Courses</option>
+				<option value="1196">Other</option>
+				<option value="29">Payloads &amp; Attachments</option>
+				<option value="776">Software</option>
+				<option value="32">Training</option>
+			</select>
+			<input type="text" placeholder="Search for Drones, Software, Guides and more…" class="eael-advanced-search" autocomplete="off">
+			<button class="eael-advanced-search-button"><?= $SVG->search() ?></button>
+			<div id="search-result">
+
+			</div>
+		</div>
+	<?php
+		return ob_get_clean();
+	}
 }
 
 
 $Shortcodes = new Shortcodes;
+add_shortcode('search_form', array($Shortcodes, 'search_form'));
 add_shortcode('woocommerce_mini_cart_dropdown_shortcode', array($Shortcodes, 'woocommerce_mini_cart_dropdown_shortcode'));
 add_shortcode('popup', array($Shortcodes, 'popup'));
 add_shortcode('announcement_bar', array($Shortcodes, 'announcement_bar'));
--- template-parts/header/header-main.php
+++ template-parts/header/header-main.php
@@ -25,25 +25,7 @@ $SVG = new SVG;
                     </div>
                 </div>
                 <div class="col-6">
-                    <div class="search-form d-flex background-white">
-                        <select name="eael-adv-search-cate-list">
-                            <option value="">All Categories</option>
-                            <option value="30">Accessories &amp; Parts</option>
-                            <option value="1316">Approved Used</option>
-                            <option value="28">Bundle Packages</option>
-                            <option value="27">Drones</option>
-                            <option value="789">E-learning Courses</option>
-                            <option value="1196">Other</option>
-                            <option value="29">Payloads &amp; Attachments</option>
-                            <option value="776">Software</option>
-                            <option value="32">Training</option>
-                        </select>
-                        <input type="text" placeholder="Search for Drones, Software, Guides and more…" class="eael-advanced-search" autocomplete="off">
-                        <button class="eael-advanced-search-button"><?= $SVG->search() ?></button>
-                        <div id="search-result">
-
-                        </div>
-                    </div>
+                    <?= do_shortcode('[search_form]') ?>
                 </div>
                 <div class="col-3">
                     <div class="column-right d-flex align-items-center justify-content-end">
