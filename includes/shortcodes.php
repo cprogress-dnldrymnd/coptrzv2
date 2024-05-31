c62c5cf60f3b7072a0d9810864779ab0d65fd80e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-31 21:44:45 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1085,7 +1085,7 @@ class Shortcodes
 				'menu' => $id,
 				'container'      => false,
 				'menu_class'     => '',
-				'items_wrap'     => '<ul id="%1$s" class="menu-items %2$s">%3$s</ul>',
+				'items_wrap'     => '<ul id="%1$s" class="menu-items list-inline p-0 %2$s">%3$s</ul>',
 				'depth'          => 3,
 				'echo' => false
 			)
