ccf668928d5f74765aac954bb171d3c6bd640f54
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-31 15:02:50 +0800

    cs

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -4,6 +4,7 @@
 
 class Shortcodes
 {
+
 	function contact_number()
 	{
 		return Theme_Options::contact_number();
@@ -1095,10 +1096,50 @@ class Shortcodes
 			return do_shortcode('[wpf-filters id=1]');
 		}
 	}
+
+	function popup($atts)
+	{
+		ob_start();
+		extract(
+			shortcode_atts(
+				array(
+					'id' => '',
+				),
+				$atts
+			)
+		);
+	?>
+		<!-- Modal -->
+		<div class="modal fade modal-v2 popup-form" id="modal-<?= $id ?>" tabindex="-1" aria-labelledby="modalSearchLabel" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content background-white">
+					<div class="modal-body p-0">
+						<div class="row g-0">
+							<div class="col-lg-6">
+								<div class="form-holder p-4 h-100 d-flex align-items-center">
+									<div class="form-inner w-100">
+										<?= do_shortcode(get_the_content(NULL, false, $id)) ?>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 bg-image">
+								<div class="position-relative h-100">
+									<img src="<?= get_the_post_thumbnail_url($id, 'large') ?>" alt="<?= get_the_title($id) ?>">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php
+		return ob_get_clean();
+	}
 }
 
 
 $Shortcodes = new Shortcodes;
+add_shortcode('popup', array($Shortcodes, 'popup'));
 add_shortcode('announcement_bar', array($Shortcodes, 'announcement_bar'));
 add_shortcode('shop_filter', array($Shortcodes, 'shop_filter'));
 add_shortcode('menu', array($Shortcodes, 'menu'));
@@ -1635,7 +1676,7 @@ function get_empty_images()
 	while ($query_images->have_posts()) {
 		$query_images->the_post();
 
-		echo get_the_ID() .'<br>';
+		echo get_the_ID() . '<br>';
 	}
 	return ob_get_clean();
 }
--- single-popups.php
+++ single-popups.php
@@ -1,32 +1,9 @@
 <?php get_header() ?>
 <?php while (have_posts()) : the_post(); ?>
-
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-<?= get_the_ID() ?>">
         Launch Popup
     </button>
-    <!-- Modal -->
-    <div class="modal fade modal-v2 popup-form" id="modal" tabindex="-1" aria-labelledby="modalSearchLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content background-white">
-                <div class="modal-body p-0">
-                    <div class="row g-0">
-                        <div class="col-lg-6">
-                            <div class="form-holder p-4 h-100 d-flex align-items-center">
-                                <div class="form-inner w-100">
-                                    <?php the_content() ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 bg-image">
-                            <div class="position-relative h-100">
-                                <img src="<?= get_the_post_thumbnail_url(get_the_ID(), 'large') ?>" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?= do_shortcode('[popup id=' . get_the_ID() . ']') ?>
 <?php endwhile; ?>
 <?php get_footer() ?>
