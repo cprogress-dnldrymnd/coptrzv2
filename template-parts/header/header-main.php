2b4b4297cb624ebeb242b9361631fbd63a6cf99b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-31 13:08:29 +0800

    Update header-main.php

--- template-parts/header/header-main.php
+++ template-parts/header/header-main.php
@@ -22,21 +22,26 @@
             </div>
             <div class="col-auto">
                 <div class="column-holder">
-                    <?php
-                    wp_nav_menu(
-                        array(
-                            'theme_location' => 'header-menu-2',
-                            'container'      => false,
-                            'menu_class'     => '',
-                            'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
-                            'items_wrap'     => '<ul id="%1$s" class="navbar-nav align-items-center h-100 %2$s">%3$s</ul>',
-                            'depth'          => 3,
-                            'walker'         => new bootstrap_5_wp_nav_menu_walker()
-                        )
-                    );
-                    ?>
+
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="navigation-holder">
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'header-menu-2',
+                    'container'      => false,
+                    'menu_class'     => '',
+                    'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
+                    'items_wrap'     => '<ul id="%1$s" class="navbar-nav align-items-center h-100 %2$s">%3$s</ul>',
+                    'depth'          => 3,
+                    'walker'         => new bootstrap_5_wp_nav_menu_walker()
+                )
+            );
+            ?>
         </div>
     </div>
 </header>
