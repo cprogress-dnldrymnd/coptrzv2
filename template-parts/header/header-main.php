2c69ef3d8dba1086d8172e30a3b3ebb229c27ff6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-31 13:14:16 +0800

    cs

--- template-parts/header/header-main.php
+++ template-parts/header/header-main.php
@@ -56,7 +56,7 @@ $SVG = new SVG;
                     'container'      => false,
                     'menu_class'     => '',
                     'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
-                    'items_wrap'     => '<ul id="%1$s" class="navbar-nav align-items-center h-100 %2$s">%3$s</ul>',
+                    'items_wrap'     => '<ul id="%1$s" class="navbar-nav align-items-center justify-content-between %2$s">%3$s</ul>',
                     'depth'          => 3,
                     'walker'         => new bootstrap_5_wp_nav_menu_walker()
                 )
