b9c0ee3050e9f260b790a5ecba1f6a7b8f9b72ae
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-05-31 13:33:40 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -416,11 +416,11 @@ $Guide_Category->args = array(
 );
 
 $Slides = new newPostType();
-$Testimonials->name = 'Slides';
-$Testimonials->singular_name = 'Slide';
-$Testimonials->icon = 'dashicons-testimonial';
-$Testimonials->supports = array('title', 'revisions');
-$Testimonials->exclude_from_search = true;
+$Slides->name = 'Slides';
+$Slides->singular_name = 'Slide';
+$Slides->icon = 'dashicons-testimonial';
+$Slides->supports = array('title', 'revisions');
+$Slides->exclude_from_search = true;
 $Testimonials->publicly_queryable = false;
-$Testimonials->show_in_admin_bar = false;
-$Testimonials->has_archive = false;
+$Slides->show_in_admin_bar = false;
+$Slides->has_archive = false;
