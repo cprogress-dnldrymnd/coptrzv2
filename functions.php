baf2dd5426cc013589c0d669b28b67f211677096
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 15:22:01 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -283,7 +283,7 @@ function action_admin_footer()
 		jQuery(document).on("change", '.trigger-selector select', function(event) {
 			$value = jQuery(this).val();
 			console.log($value);
-			$selector = jQuery(this).parents('.cf-complex__group-body').find('.page-selector');
+			$selector = jQuery(this).parent().parent().next().next().find('.page-selector');
 			if ($value == 'page') {
 				$selector.html('<?= $select_page ?>');
 			} else if ($value == 'post') {
