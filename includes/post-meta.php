62d9fea72477f6b74e02da5ba1a19b5a64698e13
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 11:10:47 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -828,6 +828,14 @@ Container::make('post_meta', 'Modules')
 			->add_fields(
 				'contact_form',
 				array(
+					Field::make('complex', 'styles', __('Styles'))
+						->add_fields(
+							'background_color',
+							array(
+								Field::make('color', 'background_color', __('Background Color')),
+							)
+						)
+						->set_layout('tabbed-vertical'),
 					Field::make('text', 'heading', __('Heading')),
 					Field::make('rich_text', 'description', __('Description')),
 					Field::make('text', 'contact_form_shortcode', __('Contact Form Shortcode'))->set_classes('field-contact-form'),
