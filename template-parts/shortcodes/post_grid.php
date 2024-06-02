2e8bbd6855be25c443369d06a95fcb15811c6b74
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 20:50:36 +0800

    Update post_grid.php

--- template-parts/shortcodes/post_grid.php
+++ template-parts/shortcodes/post_grid.php
@@ -1,5 +1,7 @@
 <div class="post-grid rounded-corner p-4 post-<?= $id ?> <?= $class ?>" style="--border-radius: 10px">
-    <?= do_shortcode('[_image id="' . get_post_thumbnail_id($id) . '"]'); ?>
-    <?= do_shortcode('[_heading heading="' . get_the_title($id) . '" tag="h3"]') ?>
-    <?= do_shortcode('[_description description="' . get_the_excerpt($id) . '" ]') ?>
+    <div class="content-margin">
+        <?= do_shortcode('[_image id="' . get_post_thumbnail_id($id) . '"]'); ?>
+        <?= do_shortcode('[_heading heading="' . get_the_title($id) . '" tag="h3"]') ?>
+        <?= do_shortcode('[_description description="' . get_the_excerpt($id) . '" ]') ?>
+    </div>
 </div>
