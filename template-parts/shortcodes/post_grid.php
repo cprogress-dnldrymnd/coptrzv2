be30954c77014cee717f84b99c7a2df90e4379da
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 20:53:05 +0800

    Update post_grid.php

--- template-parts/shortcodes/post_grid.php
+++ template-parts/shortcodes/post_grid.php
@@ -3,5 +3,10 @@
         <?= do_shortcode('[_image id="' . get_post_thumbnail_id($id) . '"]'); ?>
         <?= do_shortcode('[_heading heading="' . get_the_title($id) . '" tag="h3"]') ?>
         <?= do_shortcode('[_description description="' . get_the_excerpt($id) . '" ]') ?>
+        <div class="button-box button-accent">
+            <button>
+                View Model
+            </button>
+        </div>
     </div>
 </div>
