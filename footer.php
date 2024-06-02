a5fb2d847d2eb23c47c2e4e635eb75d91d51c1e1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 10:08:34 +0800

    csss

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
 <footer id="footer" class="background-primary">
     <div class="container">
         <div class="row">
-        <div class="col">
+        <div class="col-lg col-md-12">
                 <?php dynamic_sidebar('footer_column_1') ?>
             </div>
-            <div class="col">
+            <div class="col-lg col-md-6">
                 <?php dynamic_sidebar('footer_column_2') ?>
             </div>
-            <div class="col">
+            <div class="col-lg col-md-6">
                 <?php dynamic_sidebar('footer_column_3') ?>
             </div>
-            <div class="col">
+            <div class="col-lg col-md-6">
                 <?php dynamic_sidebar('footer_column_4') ?>
             </div>
-            <div class="col">
+            <div class="col-lg col-md-6">
                 <?php dynamic_sidebar('footer_column_5') ?>
             </div>
         </div>
