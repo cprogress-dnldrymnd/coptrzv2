982371264d6f152afc79940e8d60d86980ada3fa
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 17:03:18 +0800

    Update post_tabs.php

--- template-parts/modules/post_tabs.php
+++ template-parts/modules/post_tabs.php
@@ -9,17 +9,19 @@ $terms = get_terms(array(
         <ul class="nav nav-tabs" id="post-tab-<?= $module_id ?>" role="tablist">
             <?php foreach ($terms as $key => $term) { ?>
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link <?= $key == 0 ? 'active' : '' ?>" id="home-tab" data-bs-toggle="tab" data-bs-target="#term-<?= $term->term_id ?>" type="button" role="tab" aria-controls="tab-<?= $term->term_id ?>" aria-selected="<?= $key == 0 ? 'true' : 'false' ?>">
+                    <button class="nav-link <?= $key == 0 ? 'active' : '' ?>" id="term-<?= $term->term_id ?>-tab" data-bs-toggle="tab" data-bs-target="#term-<?= $term->term_id ?>" type="button" role="tab" aria-controls="tab-<?= $term->term_id ?>" aria-selected="<?= $key == 0 ? 'true' : 'false' ?>">
                         <?= $term->name ?>
                     </button>
                 </li>
-
             <?php } ?>
         </ul>
         <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+            <?php foreach ($terms as $key => $term) { ?>
+                <div class="tab-pane fade  <?= $key == 0 ? 'show active' : '' ?>" id="term-<?= $term->term_id ?>" role="tabpanel" aria-labelledby="term-<?= $term->term_id ?>-tab">
+                    <?= $term->name ?>
+
+                </div>
+            <?php } ?>
         </div>
     </div>
 </section>
