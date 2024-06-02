56fe7783be6a7a3e735140fe159bb84d3457a5eb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 17:18:50 +0800

    Update post_tabs.php

--- template-parts/modules/post_tabs.php
+++ template-parts/modules/post_tabs.php
@@ -6,7 +6,7 @@ $terms = get_terms(array(
 ?>
 <section class="post-tabs <?= $classes ?>" id="<?= $module_id ?>">
     <div class="container">
-        <ul class="nav nav-tabs mb-5" id="post-tab-<?= $module_id ?>" role="tablist">
+        <ul class="nav nav-tabs mb-5 justify-content-center" id="post-tab-<?= $module_id ?>" role="tablist">
             <?php foreach ($terms as $key => $term) { ?>
                 <li class="nav-item" role="presentation">
                     <button class="nav-link <?= $key == 0 ? 'active' : '' ?>" id="term-<?= $term->term_id ?>-tab" data-bs-toggle="tab" data-bs-target="#term-<?= $term->term_id ?>" type="button" role="tab" aria-controls="tab-<?= $term->term_id ?>" aria-selected="<?= $key == 0 ? 'true' : 'false' ?>">
