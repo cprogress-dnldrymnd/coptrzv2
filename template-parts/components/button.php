4509c210059214077cf8bcfb8a011cafcdc6b8d5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 16:11:39 +0800

    Update button.php

--- template-parts/components/button.php
+++ template-parts/components/button.php
@@ -2,12 +2,18 @@
 $GetData = new GetData;
 $SVG = new SVG;
 $button_type = $button_type ? $button_type : '';
+if ($button_type != 'popup') {
+	$tag = 'a';
+} else {
+	$tag = 'button';
+}
+
 ?>
 <div class="button-box <?= $class ?>" <?= $GetData->get_data_aos($data_aos) ?> <?= $button_attribute ? $button_attribute : '' ?>>
-	<a href="<?= $button_link ?>" <?= $button_action ?>>
+	<<?= $tag ?> href="<?= $button_link ?>" <?= $button_action ?> <?= $button_type == 'popup'  ? 'data-bs-toggle="modal"' : '' ?>>
 		<?php if ($button_icon) { ?>
 			<span class="icon"><?= $SVG->{$button_icon} ?></span>
 		<?php } ?>
 		<span class="text"><?= $button_text ? do_shortcode($button_text) : get_the_title($button_type) ?></span>
-	</a>
+	</<?= $tag ?>>
 </div>
