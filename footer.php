4d7de898c97d1997c64b07d175f16ad5323424d6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 10:01:44 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
+<footer class="background-primary">
+    <div class="container">
+        <div class="row">
+        <div class="col">
+                <?php dynamic_sidebar('footer_column_1') ?>
+            </div>
+            <div class="col">
+                <?php dynamic_sidebar('footer_column_2') ?>
+            </div>
+            <div class="col">
+                <?php dynamic_sidebar('footer_column_3') ?>
+            </div>
+            <div class="col">
+                <?php dynamic_sidebar('footer_column_4') ?>
+            </div>
+            <div class="col">
+                <?php dynamic_sidebar('footer_column_5') ?>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <?= do_shortcode('[popup id=268179]') ?>
 <?php wp_footer(); ?>
 </body>
