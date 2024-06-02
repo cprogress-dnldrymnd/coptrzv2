6c488b2c938b0bf68edb60be79a4fb8c6c693e42
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-06-03 00:20:22 +0800

    Update description.php

--- template-parts/components/description.php
+++ template-parts/components/description.php
@@ -2,7 +2,7 @@
 $GetData = new GetData;
 ?>
 <?php if(isset($description)) { ?>
-	<div class="description-box<?= $GetData->get_class($class) ?>" <?= $GetData->get_data_aos($data_aos) ?>>
+	<div class="description-box <?= $class ?>" <?= $GetData->get_data_aos($data_aos) ?>>
 		<?= wpautop($description) ?>
 	</div>
 	<?php } ?>
