3fb9deaeb1fb3a17350f3796a0a7d8e188bbfa3a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 12:50:13 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -907,5 +907,6 @@ Container::make('post_meta', 'Modules')
 					Field::make('text', 'contact_form_shortcode', __('Contact Form Shortcode'))->set_classes('field-contact-form'),
 				)
 			)
+			->set_collapsed(true)
 
 	));
