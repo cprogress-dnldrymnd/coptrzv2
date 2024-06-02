7326de62ec3b99952e099560309534d9b5d8e8f0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 15:53:55 +0800

    Update button.php

--- template-parts/components/button.php
+++ template-parts/components/button.php
@@ -2,7 +2,6 @@
 $GetData = new GetData;
 $SVG = new SVG;
 $button_type = $button_type ? $button_type : '';
-$button_link = get_permalink($button_type) ? get_permalink($button_type) : do_shortcode($button_type);
 ?>
 <div class="button-box <?= $button_class ?>" <?= $GetData->get_data_aos($data_aos) ?> <?= $button_attribute ? $button_attribute : '' ?>>
 	<a href="<?= $button_link ?>" <?= $button_action ?>>
