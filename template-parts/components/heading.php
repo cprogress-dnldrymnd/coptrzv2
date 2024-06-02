08944614c20c4992db983507bcf6512da8b353e6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 11:05:10 +0800

    Update heading.php

--- template-parts/components/heading.php
+++ template-parts/components/heading.php
@@ -5,7 +5,7 @@ $heading_small = isset($heading_small) ? $heading_small : false;
 <?php
 if (isset($heading) && $heading != '') {
 	$tag = isset($tag) ? $tag : 'h2';
-	?>
+?>
 	<div class="heading-box<?= $GetData->get_class($class) ?>" <?= $GetData->get_data_aos($data_aos) ?>>
 		<?php if ($heading_small) { ?>
 			<span class="prefix"><?= $heading_small ?></span>
