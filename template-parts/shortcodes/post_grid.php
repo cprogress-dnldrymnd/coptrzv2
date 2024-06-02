f639c82170387fd4a945e9213205324df6978c8f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 20:55:21 +0800

    Update post_grid.php

--- template-parts/shortcodes/post_grid.php
+++ template-parts/shortcodes/post_grid.php
@@ -3,7 +3,7 @@ $captured_by = get__post_meta_by_id($id, 'captured_by');
 ?>
 <div class="post-grid rounded-corner p-4 post-<?= $id ?> <?= $class ?>" style="--border-radius: 10px">
     <div class="content-margin">
-        <?= do_shortcode('[_image id="' . get_post_thumbnail_id($id) . '"]'); ?>
+        <?= do_shortcode('[_image class="text-center" id="' . get_post_thumbnail_id($id) . '"]'); ?>
         <?= do_shortcode('[_heading heading="' . get_the_title($id) . '" tag="h3"]') ?>
         <?= do_shortcode('[_description description="' . get_the_excerpt($id) . '" ]') ?>
         <div class="button-box button-accent">
