c8e6c14a7a03af69b59253f19cb21aae65cc00d1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 10:29:50 +0800

    Update mini_cart.php

--- template-parts/shortcodes/mini_cart.php
+++ template-parts/shortcodes/mini_cart.php
@@ -2,10 +2,10 @@
 $SVG = new SVG;
 ?>
 <div class="cart-dropdown position-relative me-4">
-    <a class="cart-icon position-relative" href="<?php echo wc_get_cart_url(); ?>">
+    <a class="cart-icon " href="<?php echo wc_get_cart_url(); ?>">
         <div class="d-inline-flex align-items-center">
             <span class="cart-subtotal me-4"><?php echo WC()->cart->get_cart_subtotal(); ?></span>
-            <span class="cart-icon">
+            <span class="cart-icon position-relative">
                 <?= $SVG->cart() ?>
                 <span class="cart-contents-count"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
             </span>
