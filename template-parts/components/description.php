8db350febbae673773180a27e7d924d95fa06ca4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 01:48:34 +0800

    Update description.php

--- template-parts/components/description.php
+++ template-parts/components/description.php
@@ -1,7 +1,7 @@
 <?php
 $GetData = new GetData;
 ?>
-<?php if ($descriptions) { ?>
+<?php if ($description) { ?>
 	<div class="description-box <?= $class ?>" <?= $GetData->get_data_aos($data_aos) ?>>
 		<?= do_shortcode(wpautop($description)) ?>
 	</div>
