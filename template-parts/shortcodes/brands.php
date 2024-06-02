6f64a94fb5f01157fd83aee3a56f1049c7104ec4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 01:37:27 +0800

    Update brands.php

--- template-parts/shortcodes/brands.php
+++ template-parts/shortcodes/brands.php
@@ -1,3 +1,63 @@
+
+<?php
+$terms = get_terms(
+  array(
+    'taxonomy'   => 'pa_brands',
+    'hide_empty' => false,
+    'orderby'    => 'name',
+    'order'      => 'ASC',
+  )
+);
+
+$featured_vendor_arr = array();
+$a_f_array = array();
+$f_p_array = array();
+$q_z_array = array();
+foreach ($terms as $term) {
+  $image = get__term_meta($term->term_id, 'image');
+  $hide_vendor = get__term_meta($term->term_id, 'hide_vendor');
+  $featured_vendor = get__term_meta($term->term_id, 'featured_vendor');
+
+  if ($featured_vendor) {
+    $featured_vendor_arr[$term->term_id] = array(
+      'image' => $image,
+      'name'  => $term->name
+    );
+  }
+
+  if (!$hide_vendor) {
+    if (str_starts_with($term->name, 'A') || str_starts_with($term->name, 'B') || str_starts_with($term->name, 'C') || str_starts_with($term->name, 'D') || str_starts_with($term->name, 'E')) {
+      $a_f_array[$term->term_id] = array(
+        'image' => $image,
+        'name'  => $term->name
+      );
+    }
+
+
+    if (str_starts_with($term->name, 'F') || str_starts_with($term->name, 'G') || str_starts_with($term->name, 'H') || str_starts_with($term->name, 'I') || str_starts_with($term->name, 'J') || str_starts_with($term->name, 'K') || str_starts_with($term->name, 'L') || str_starts_with($term->name, 'M') || str_starts_with($term->name, 'N') || str_starts_with($term->name, 'O') || str_starts_with($term->name, 'P')) {
+      $f_p_array[$term->term_id] = array(
+        'image' => $image,
+        'name'  => $term->name
+      );
+    }
+
+    if (str_starts_with($term->name, 'Q') || str_starts_with($term->name, 'R') || str_starts_with($term->name, 'S') || str_starts_with($term->name, 'T') || str_starts_with($term->name, 'U') || str_starts_with($term->name, 'V') || str_starts_with($term->name, 'W') || str_starts_with($term->name, 'X') || str_starts_with($term->name, 'Y') || str_starts_with($term->name, 'Z')) {
+      $q_z_array[$term->term_id] = array(
+        'image' => $image,
+        'name'  => $term->name
+      );
+    }
+  }
+
+}
+$DisplayData = new DisplayData;
+?>
+<?php
+if (!get__post_meta('hide_page_banner')) {
+  get_template_part('template-parts/section/content', 'banner');
+}
+?>
+
 <section class="vendors-nav sm-padding-bottom overflow-visible">
   <div class="container">
     <ul class="list-inline d-flex">
@@ -20,7 +80,15 @@
           ?>
           <div class="col-xl-3 col-lg-4 col-sm-6 col-6 vendor-box">
             <a class="inner h-100 background-white d-block" href="<?= get_term_link($key) ?>">
-              <?= do_shortcode('[_image id="' . $vendor['image'] . '" size="medium"]') ?>
+              <?php
+              $DisplayData->image(
+                array(
+                  'image_id' => $vendor['image'],
+                  'size'     => 'medium'
+                ),
+                'position-relative image-contain-transform mb-3'
+              );
+              ?>
               <div class="vendor-title">
                 <h4 class="mb-0">
                   <?= $vendor['name'] ?>
@@ -47,7 +115,15 @@
           ?>
           <div class="col-xl-3 col-lg-4 col-sm-6 col-6 vendor-box">
             <a class="inner h-100 background-white d-block" href="<?= get_term_link($key) ?>">
-              <?= do_shortcode('[_image id="' . $vendor['image'] . '" size="medium"]') ?>
+              <?php
+              $DisplayData->image(
+                array(
+                  'image_id' => $vendor['image'],
+                  'size'     => 'medium'
+                ),
+                'position-relative image-contain-transform mb-3'
+              );
+              ?>
               <div class="vendor-title">
                 <h4 class="mb-0">
                   <?= $vendor['name'] ?>
@@ -73,7 +149,15 @@
           ?>
           <div class="col-xl-3 col-lg-4 col-sm-6 col-6 vendor-box">
             <a class="inner h-100 background-white d-block" href="<?= get_term_link($key) ?>">
-              <?= do_shortcode('[_image id="' . $vendor['image'] . '" size="medium"]') ?>
+              <?php
+              $DisplayData->image(
+                array(
+                  'image_id' => $vendor['image'],
+                  'size'     => 'medium'
+                ),
+                'position-relative image-contain-transform mb-3'
+              );
+              ?>
               <div class="vendor-title">
                 <h4 class="mb-0">
                   <?= $vendor['name'] ?>
@@ -99,7 +183,15 @@
           ?>
           <div class="col-xl-3 col-lg-4 col-sm-6 col-6 vendor-box">
             <a class="inner h-100 background-white d-block" href="<?= get_term_link($key) ?>">
-              <?= do_shortcode('[_image id="' . $vendor['image'] . '" size="medium"]') ?>
+              <?php
+              $DisplayData->image(
+                array(
+                  'image_id' => $vendor['image'],
+                  'size'     => 'medium'
+                ),
+                'position-relative image-contain-transform mb-3'
+              );
+              ?>
               <div class="vendor-title">
                 <h4 class="mb-0">
                   <?= $vendor['name'] ?>
