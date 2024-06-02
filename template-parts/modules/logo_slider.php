a25b9a6281d509baaf6e30db2341cdf25bf45ef1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 16:27:43 +0800

    Update logo_slider.php

--- template-parts/modules/logo_slider.php
+++ template-parts/modules/logo_slider.php
@@ -1,7 +1,7 @@
 <section class="logo-slider <?= $classes ?>">
     <div class="container">
         <div class="section-heading-description">
-            <?= do_shortcode('[_heading heading="' . $module['heading'] . '" class="big-heading"]') ?>
+            <?= do_shortcode('[_heading heading="' . $module['heading'] . '" ]') ?>
             <?= do_shortcode('[_description description="' . $module['description'] . '" ]') ?>
         </div>
 
