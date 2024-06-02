fc78e2c0de0b39b2fd744a72909892930f3f5db6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 11:07:09 +0800

    Update contact_form.php

--- template-parts/modules/contact_form.php
+++ template-parts/modules/contact_form.php
@@ -2,8 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
-                <?= do_shortcode('[_heading heading="' . $module['heading'] .'" ]') ?>
-                <?= do_shortcode('[_description description="' . $module['description'] .'" ]') ?>
+                <div class="column-holder">
+                    <?= do_shortcode('[_heading heading="' . $module['heading'] . '" ]') ?>
+                    <?= do_shortcode('[_description description="' . $module['description'] . '" ]') ?>
+                </div>
             </div>
         </div>
     </div>
