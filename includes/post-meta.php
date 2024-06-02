3d5b13fe802daa77eb718a14903fcac2f6a9ef00
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 16:53:39 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1089,6 +1089,69 @@ Container::make('post_meta', 'Modules')
 					Field::make('media_gallery', 'images', __('Image')),
 				)
 			)
+			->add_fields(
+				'post_tabs',
+				array(
+					Field::make('complex', 'styles', __('Styles'))
+						->set_duplicate_groups_allowed(false)
+						->add_fields(
+							'background_color',
+							array(
+								Field::make('select', 'background_color', 'Background Color')
+									->set_options(
+										array(
+											'background-primary' => 'Primary',
+											'background-secondary' => 'Secondary',
+											'background-accent' => 'Accent',
+											'background-white' => 'White',
+											'background-custom' => 'Custom',
+										)
+									),
+								Field::make('color', 'background_color_custom', __('Background Color'))
+									->set_conditional_logic(
+										array(
+											array(
+												'field' => 'background_color',
+												'value' => 'background-custom',
+											)
+										)
+									),
+							)
+						)
+						->add_fields(
+							'padding',
+							array(
+								Field::make('select', 'padding_top', 'Padding Top')
+									->set_options(
+										array(
+											'' => 'No Padding',
+											'xl-padding-top' => 'Extra Large',
+											'lg-padding-top' => 'Large',
+											'md-padding-top' => 'Medium',
+											'sm-padding-top' => 'Small',
+											'xxs-padding-top' => 'Extra Small',
+										)
+									),
+								Field::make('select', 'padding_bottom', 'Padding Bottom')
+									->set_options(
+										array(
+											'' => 'No Padding',
+											'xl-padding-bottom' => 'Extra Large',
+											'lg-padding-bottom' => 'Large',
+											'md-padding-bottom' => 'Medium',
+											'sm-padding-bottom' => 'Small',
+											'xxs-padding-bottom' => 'Extra Small',
+										)
+									),
+
+							)
+						)
+						->set_layout('tabbed-vertical'),
+					Field::make('text', 'heading', __('Heading')),
+					Field::make('textarea', 'description', __('Description')),
+					Field::make('text', 'post_type_key', __('Post Type Key')),
+				)
+			)
 	));
 
 
