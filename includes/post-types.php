4bf987e3a2e5245e3e4dd8e0e0680f65e8ff3602
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 16:33:32 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -455,3 +455,36 @@ $Model3d->exclude_from_search = true;
 $Model3d->publicly_queryable = true;
 $Model3d->show_in_admin_bar = true;
 $Model3d->has_archive = false;
+
+
+$Model3D = new newTaxonomy();
+$Model3D->taxonomy = '3dModel_Category';
+$Model3D->post_type = '3dmodellibraries';
+$Model3D->args = array(
+    'label'        => 'Categories',
+    'labels' => array(
+        'name'                       => _x('Categories', 'Taxonomy General Name', 'text_domain'),
+        'singular_name'              => _x('Category', 'Taxonomy Singular Name', 'text_domain'),
+        'menu_name'                  => __('Category', 'text_domain'),
+        'all_items'                  => __('All Items', 'text_domain'),
+        'parent_item'                => __('Parent Item', 'text_domain'),
+        'parent_item_colon'          => __('Parent Item:', 'text_domain'),
+        'new_item_name'              => __('New Item Name', 'text_domain'),
+        'add_new_item'               => __('Add New Item', 'text_domain'),
+        'edit_item'                  => __('Edit Item', 'text_domain'),
+        'update_item'                => __('Update Item', 'text_domain'),
+        'view_item'                  => __('View Item', 'text_domain'),
+        'separate_items_with_commas' => __('Separate items with commas', 'text_domain'),
+        'add_or_remove_items'        => __('Add or remove items', 'text_domain'),
+        'choose_from_most_used'      => __('Choose from the most used', 'text_domain'),
+        'popular_items'              => __('Popular Items', 'text_domain'),
+        'search_items'               => __('Search Items', 'text_domain'),
+        'not_found'                  => __('Not Found', 'text_domain'),
+        'no_terms'                   => __('No items', 'text_domain'),
+        'items_list'                 => __('Items list', 'text_domain'),
+        'items_list_navigation'      => __('Items list navigation', 'text_domain'),
+    ),
+    'hierarchical' => true,
+    'query_var'    => true,
+
+);
