1f263125d96e68f9b7a78ccd488beff350bd263f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 15:57:04 +0800

    Update elements-shortcodes.php

--- includes/elements-shortcodes.php
+++ includes/elements-shortcodes.php
@@ -102,8 +102,7 @@ function _elements($data)
                 if ($d['button_type'] == 'custom') {
                     $button_link = $d['button_url_custom'];
                 } else {
-                    $id = $d['button_url_' . $d['button_type']];
-                    $button_link = get_permalink($id);
+                    $button_link = get_permalink($d['button_url']);
                 }
                 echo do_shortcode('[_button class="button-accent" button_type="' . $d['button_type'] . '" button_text="' . $d['button_text'] . '" button_link="' . $button_link . '"]');
                 break;
