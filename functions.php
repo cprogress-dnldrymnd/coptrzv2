c1fb5efae95707b6a9911a9430a66ae89fdd77bc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 15:44:02 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -299,25 +299,7 @@ function action_admin_footer()
 		});
 
 
-		setTimeout(function() {
-			jQuery('.trigger-selector select').each(function(index, element) {
-				$value = jQuery(this).val();
-				$selector = jQuery(this).parent().parent().parent().find('.page-selector');
-				$input = jQuery(this).parent().parent().parent().find('.page-selector');
-				active_link_type($selector, $value, $input)
-			});
-
-
-			jQuery('.trigger-selector-single select').each(function(index, element) {
-				$value = jQuery(this).val();
-				$selector = jQuery(this).parent().parent().next().next().next().find('.page-selector');
-				$input = jQuery(this).parent().parent().next().next().find('input').val();
-				active_link_type($selector, $value, $input);
-			});
-
-		}, 2000);
-
-		function active_link_type($selector, $value, $input) {
+		function active_link_type($selector, $value, $input = '') {
 			if ($value == 'page') {
 				$selector.html('<?= $select_page ?>');
 			} else if ($value == 'post') {
@@ -329,9 +311,25 @@ function action_admin_footer()
 			} else {
 				$selector.html('');
 			}
-			console.log($input);
-			$selector.find('.select-page-selector').val($input);
+
+		
 		}
+
+		setTimeout(function() {
+			jQuery('.trigger-selector select').each(function(index, element) {
+				$value = jQuery(this).val();
+				$selector = jQuery(this).parent().parent().parent().find('.page-selector');
+				active_link_type($selector, $value)
+			});
+
+
+			jQuery('.trigger-selector-single select').each(function(index, element) {
+				$value = jQuery(this).val();
+				$selector = jQuery(this).parent().parent().next().next().next().find('.page-selector');
+				active_link_type($selector, $value)
+			});
+
+		}, 2000);
 	</script>
 <?php
 }
