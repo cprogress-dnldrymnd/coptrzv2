ba72f91d16c441738985618766f5b5debf3c677c
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-06-03 00:09:07 +0800

    Update image.php

--- template-parts/components/image.php
+++ template-parts/components/image.php
@@ -22,7 +22,7 @@ else {
 ?>
 <?php if ($image_url || $placeholder) { ?>
 
-	<div class="image-box <?= $same_height ?> <?= $class ?>" <?= $GetData->get_data_aos($data_aos) ?>>
+	<div class="image-box <?= $same_height == 'true' ? 'image-absolute' : '' ?> <?= $class ?>" <?= $GetData->get_data_aos($data_aos) ?>>
 		<?php
 		if ($link) {
 			echo '<a href="' . $link . '" class="d-block">';
