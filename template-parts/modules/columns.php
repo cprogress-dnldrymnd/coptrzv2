85be53afc67b68f6a564b33bb0a3b1ec7427ab19
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 15:01:11 +0800

    Update columns.php

--- template-parts/modules/columns.php
+++ template-parts/modules/columns.php
@@ -18,32 +18,3 @@
         </div>
     </div>
 </section>
-
-<?php
-$pages = get__posts('page');
-$select_page = '<select name="select-page-selector">';
-foreach ($pages as $key => $page) {
-    $select_page .= '<option value="' . $key . '"> ' . $page . ' </option>';
-}
-$select_page .= '</select>';
-
-$posts = get__posts('post');
-$select_post = '<select name="select-page-selector">';
-foreach ($posts as $key => $post) {
-    $select_post .= '<option value="' . $key . '"> ' . $post . ' </option>';
-}
-$select_post .= '</select>';
-
-$solutions = get__posts('solutions');
-$select_solution = '<select name="select-page-selector">';
-foreach ($solutions as $key => $solution) {
-    $select_solution .= '<option value="' . $key . '"> ' . $solution . ' </option>';
-}
-$select_solution .= '</select>';
-
-$popups = get__posts('popups');
-$select_popup = '<select name="select-page-selector">';
-foreach ($popups as $key => $popup) {
-    $select_popup .= '<option value="' . $key . '"> ' . $popup . ' </option>';
-}
-$select_popup .= '</select>';?>
