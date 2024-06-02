59bb90374b68e97843a39371fc5c9fbdfd249d5d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 15:54:03 +0800

    Update button.php

--- template-parts/components/button.php
+++ template-parts/components/button.php
@@ -5,10 +5,9 @@ $button_type = $button_type ? $button_type : '';
 ?>
 <div class="button-box <?= $button_class ?>" <?= $GetData->get_data_aos($data_aos) ?> <?= $button_attribute ? $button_attribute : '' ?>>
 	<a href="<?= $button_link ?>" <?= $button_action ?>>
-		<?php if ($button_icon && $button_icon != 'sixty-seconds') { ?>
+		<?php if ($button_icon) { ?>
 			<span class="icon"><?= $SVG->{$button_icon} ?></span>
 		<?php } ?>
-
 		<span class="text"><?= $button_text ? do_shortcode($button_text) : get_the_title($button_type) ?></span>
 	</a>
 </div>
