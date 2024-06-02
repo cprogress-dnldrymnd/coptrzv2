5877ab53a609c65dda6e47fa7f1171f18d938556
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 12:04:55 +0800

    css

--- template-parts/modules/contact_form.php
+++ template-parts/modules/contact_form.php
@@ -8,7 +8,7 @@
                 </div>
             </div>
             <div class="col-lg-6">
-                <div class="column-holder content-margin">
+                <div class="column-holder content-margin form-holder">
                     <?= do_shortcode($module['contact_form_shortcode']) ?>
                 </div>
             </div>
