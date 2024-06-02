16346b3079eadef2acc7f3203fa60a8b046b479f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 10:40:12 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -824,7 +824,7 @@ Container::make('post_meta', 'Modules')
 	->where('post_template', '=', 'templates/page-modules.php')
 	->set_priority('high')
 	->add_fields(array(
-		Field::make('complex', 'submenu', __('Submenu'))
+		Field::make('complex', 'modules', __('Modules'))
 			->add_fields(
 				'contact_form',
 				array(
