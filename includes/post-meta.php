712b0796bba6dc8d76e24a8caa00f986b0303b38
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 11:16:01 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -832,7 +832,53 @@ Container::make('post_meta', 'Modules')
 						->add_fields(
 							'background_color',
 							array(
-								Field::make('color', 'background_color', __('Background Color')),
+								Field::make('select', 'background_color', 'Background Color')
+									->set_options(
+										array(
+											'background-primary' => 'Primary',
+											'background-secondary' => 'Secondary',
+											'background-accent' => 'Accent',
+											'background-white' => 'White',
+											'background-custom' => 'Custom',
+										)
+									),
+								Field::make('color', 'background_color_custom', __('Background Color'))
+									->set_conditional_logic(
+										array(
+											array(
+												'field' => 'background_color',
+												'value' => 'background-custom',
+											)
+										)
+									),
+							)
+						)
+						->add_fields(
+							'padding',
+							array(
+								Field::make('select', 'padding_top', 'Padding Top')
+									->set_options(
+										array(
+											'' => 'No Padding',
+											'xl-padding-top' => 'Extra Large',
+											'lg-padding-top' => 'Large',
+											'md-padding-top' => 'Medium',
+											'sm-padding-top' => 'Small',
+											'xxs-padding-top' => 'Extra Small',
+										)
+									),
+								Field::make('select', 'padding_bottom', 'Padding Bottom')
+									->set_options(
+										array(
+											'' => 'No Padding',
+											'xl-padding-bottom' => 'Extra Large',
+											'lg-padding-bottom' => 'Large',
+											'md-padding-bottom' => 'Medium',
+											'sm-padding-bottom' => 'Small',
+											'xxs-padding-bottom' => 'Extra Small',
+										)
+									),
+
 							)
 						)
 						->set_layout('tabbed-vertical'),
