6729a8b3c3caa6d73d0354be7e2b068ec68f9278
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-02 13:15:18 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -970,7 +970,7 @@ Container::make('post_meta', 'Modules')
 									->add_fields(
 										'description',
 										array(
-											Field::make('rich_text', 'item_description', __('Description')),
+											Field::make('textarea', 'description', __('Description')),
 										)
 									)
 									->add_fields(
