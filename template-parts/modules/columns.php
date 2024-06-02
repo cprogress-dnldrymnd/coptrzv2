385aceb5c189ef826674e660e22ad26985c86757
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-06-02 22:55:40 +0800

    Update columns.php

--- template-parts/modules/columns.php
+++ template-parts/modules/columns.php
@@ -1,4 +1,4 @@
-<section class="columns <?= $classes ?>" id="<?= $module_id ?>" <?= $style ?>>
+<section class="columns <?= $classes ?>" id="<?= $module_id ?>" style="<?= $style ?>">
     <div class="container">
         <div class="section-heading-description">
             <?= do_shortcode('[_heading heading="' . $module['heading'] . '" class="big-heading"]') ?>
