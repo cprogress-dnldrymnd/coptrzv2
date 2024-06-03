45d52ea7bd0e49a133e55d61a958c5109f23536f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 11:38:43 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1592,8 +1592,35 @@ Container::make('post_meta', 'Modules')
 											'image',
 											array(
 												Field::make('image', 'image', __('Image')),
-												Field::make('text', 'image_width', __('Custom Image Width')),
-												Field::make('text', 'image_height', __('Custom Image Height')),
+												Field::make('select', 'size', __('Size'))
+													->set_options(
+														array(
+															''          => 'Default',
+															'full'      => 'Full',
+															'large'      => 'Large',
+															'medium' => 'Medium',
+															'thumbnail'    => 'Thumbnail',
+														)
+													),
+												Field::make('checkbox', 'custom_size', __('Custom Size')),
+												Field::make('text', 'image_width', __('Custom Image Width'))
+													->set_conditional_logic(
+														array(
+															array(
+																'field' => 'custom_size',
+																'value' => true,
+															)
+														)
+													),
+												Field::make('text', 'image_height', __('Custom Image Height'))
+													->set_conditional_logic(
+														array(
+															array(
+																'field' => 'custom_size',
+																'value' => true,
+															)
+														)
+													),
 												Field::make('checkbox', 'rounded_corners', __('Rounder Corners')),
 												Field::make('text', 'border_radius', __('Border Radius'))->set_help_text('Custom border radius')
 													->set_conditional_logic(
