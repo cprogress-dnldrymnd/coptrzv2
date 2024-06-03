668b3692c877d80dd0367cc947281b7293f09c01
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 11:54:41 +0800

    Update accordion.php

--- template-parts/modules/accordion.php
+++ template-parts/modules/accordion.php
@@ -1,24 +1,21 @@
 <section class="accordion-section <?= $classes ?>" id="<?= $module_id ?>">
   <div class="container">
     <div class="section-heading-description mb-5">
-      <?= do_shortcode('[_heading heading="' . $module['heading'] . '" class="big-heading"]') ?>
+      <?= do_shortcode('[_heading heading="' . $module['heading'] . '" class="' . $module['size'] . '"]') ?>
       <?= do_shortcode('[_description description="' . $module['description'] . '" ]') ?>
     </div>
     <div class="accordion accordion-v2 accordion-flush" id="accordion-<?= $module_id ?>">
       <?php foreach ($module['accordion'] as $key => $accordion_item) { ?>
         <div class="accordion-item">
           <h2 class="accordion-header" id="flush-heading<?= $key ?>">
-            <button class="accordion-button justify-content-between p-0" type="button" data-bs-toggle="collapse"
-              data-bs-target="#flush-collapse<?= $key ?>" aria-expanded="<?= $key == 0 ? 'true' : 'false' ?>"
-              aria-controls="flush-collapse<?= $key ?>">
+            <button class="accordion-button justify-content-between p-0" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse<?= $key ?>" aria-expanded="<?= $key == 0 ? 'true' : 'false' ?>" aria-controls="flush-collapse<?= $key ?>">
               <span>
                 <?= do_shortcode('[_heading heading="' . $accordion_item['heading'] . '" tag="h4"]') ?>
               </span>
               <span class="plus-minus"></span>
             </button>
           </h2>
-          <div id="flush-collapse<?= $key ?>" class="accordion-collapse collapse <?= $key == 0 ? 'show' : '' ?>"
-            aria-labelledby="flush-heading<?= $key ?>" data-bs-parent="#accordion-<?= $module_id ?>">
+          <div id="flush-collapse<?= $key ?>" class="accordion-collapse collapse <?= $key == 0 ? 'show' : '' ?>" aria-labelledby="flush-heading<?= $key ?>" data-bs-parent="#accordion-<?= $module_id ?>">
             <?= do_shortcode('[_description description="' . $accordion_item['description'] . '" ]') ?>
           </div>
         </div>
