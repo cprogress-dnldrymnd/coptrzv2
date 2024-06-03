57661c7a3170ca9da2aaacccdb49a98bbc2e49b5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 17:46:42 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -143,10 +143,10 @@ function archive_ajax()
 				$the_query->the_post();
 		?>
 				<div class="<?= $class ?> col-sm-6 post-item">
-					<div class="post-grid h-100 rounded-corner overflow-hidden p-5">
+					<div class="post-grid background-white h-100 rounded-corner overflow-hidden p-5">
 						<div class="image-holder position-relative">
 							<?php
-							echo do_shortcode('[_image class="image-absolute image-absolute-contain" size="large" id="' . get_post_thumbnail_id(). '"]');
+							echo do_shortcode('[_image class="image-absolute image-absolute-cover" size="large" id="' . get_post_thumbnail_id(). '"]');
 							if ($is_search) {
 								if (get_post_type() == 'post') {
 									$post_type_val = 'blog';
