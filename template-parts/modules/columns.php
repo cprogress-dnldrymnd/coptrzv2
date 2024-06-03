ad6810e0076154a14a21ac0454424f14b68dc154
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 11:08:57 +0800

    Update columns.php

--- template-parts/modules/columns.php
+++ template-parts/modules/columns.php
@@ -2,7 +2,7 @@
     <div class="container">
         <?php if ($module['display_heading_description'] && ($module['heading'] || $module['description'])) {  ?>
             <div class="section-heading-description mb-5">
-                <?= do_shortcode('[_heading heading="' . $module['heading'] . '" class="big-heading"]') ?>
+                <?= do_shortcode('[_heading tag="' . $module['tag'] . '" heading="' . $module['heading'] . '" class="big-heading ' . $module['text_align'] . '"]') ?>
                 <?= do_shortcode("[_description description='" . $module['description'] . "']") ?>
             </div>
         <?php } ?>
