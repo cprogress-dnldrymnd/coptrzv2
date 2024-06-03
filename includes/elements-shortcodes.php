0c8d756da88b6d48d5c743781057d63c7dee060f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 11:49:24 +0800

    Update elements-shortcodes.php

--- includes/elements-shortcodes.php
+++ includes/elements-shortcodes.php
@@ -99,7 +99,7 @@ function _elements($data, $module_id, $same_height_images)
         $type = $d['_type'];
         switch ($type) {
             case 'heading':
-                echo do_shortcode('[_heading tag="' . $d['tag'] . '" heading="' . $d['heading'] . '" size="' . $d['size'] . '"]');
+                echo do_shortcode('[_heading tag="' . $d['tag'] . '" heading="' . $d['heading'] . '" class="' . $d['size'] . '"]');
             case 'description':
                 echo do_shortcode('[_description description="' . $d['description'] . '"]');
                 break;
