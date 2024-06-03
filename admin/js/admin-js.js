28baff232db98b6d8a0bb30c789dd1c3d17fcbd7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 09:54:24 +0800

    Update admin-js.js

--- admin/js/admin-js.js
+++ admin/js/admin-js.js
@@ -58,29 +58,32 @@ function codemirror() {
         if (typeof tinymce !== 'undefined') {
             console.log('TinyMCE is  loaded.');
 
+            jQuery('.activate-tinymce textarea').each(function (index, element) {
+                var textarea = jQuery(this);
+                $name = jQuery(this).attr('name');
+                // Replace textarea with TinyMCE editor
+
+
+                tinymce.init({
+                    selector: 'textarea[name="' + $name + '"]',
+                    plugins: 'lists link charmap paste textcolor',
+                    toolbar: 'formatselect | bold italic | bullist numlist | link | forecolor | charmap | pastetext | removeformat',
+                    block_formats: 'Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Preformatted=pre', // Like classic editor
+                    toolbar_location: 'top',
+                    menubar: false,
+                    statusbar: false,
+                    branding: false // Hide TinyMCE logo
+                });
+
+
+                // Handle form submission (update textarea with TinyMCE content)
+                textarea.closest('form').submit(function (e) {
+                    // Update the textarea's value before submitting the form
+                    textarea.val(tinymce.get('carbon_fields_compact_input[_modules][0][_description]').getContent());
+                });
+            });
             // Find the textarea element
-            var textarea = jQuery('textarea[name="carbon_fields_compact_input[_modules][0][_description]"]');
 
-            // Replace textarea with TinyMCE editor
-      
-
-            tinymce.init({
-                selector: 'textarea[name="carbon_fields_compact_input[_modules][0][_description]"]',
-                plugins: 'lists link charmap paste textcolor',
-                toolbar: 'formatselect | bold italic | bullist numlist | link | forecolor | charmap | pastetext | removeformat',
-                block_formats: 'Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Preformatted=pre', // Like classic editor
-                toolbar_location: 'top',
-                menubar: false,
-                statusbar: false,
-                branding: false // Hide TinyMCE logo
-            });
-    
-
-            // Handle form submission (update textarea with TinyMCE content)
-            textarea.closest('form').submit(function (e) {
-                // Update the textarea's value before submitting the form
-                textarea.val(tinymce.get('carbon_fields_compact_input[_modules][0][_description]').getContent());
-            });
         } else {
             console.log('TinyMCE is not loaded.');
         }
