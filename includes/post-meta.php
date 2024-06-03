34e51f3219b2f2ae7926ffb2bff57a96ac951803
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 10:49:45 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1273,7 +1273,7 @@ Container::make('post_meta', 'Modules')
 							)
 							->set_layout('tabbed-vertical'),
 						Field::make('checkbox', 'same_height_images', __('Same Height Images'))->set_width(20),
-						Field::make('checkbox', 'display_heading_description', __('Display Section Heading and Description')) > set_width(80),
+						Field::make('checkbox', 'display_heading_description', __('Display Section Heading and Description'))->set_width(80),
 						Field::make('text', 'heading', __('Heading'))
 							->set_conditional_logic(
 								array(
