4772deddadae64cab235f6febb416b15d6d7bcb2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 13:20:50 +0800

    css

--- archive-solutions.php
+++ archive-solutions.php
@@ -42,5 +42,4 @@ get_header(); // This fxn gets the header.php file and renders it
 </section>
 
 
-<?php get_footer(); // This fxn gets the footer.php file and renders it 
-?>
+<?php get_footer(); // This fxn gets the footer.php file and renders it ?>
