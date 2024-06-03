1565091c2b90c6c3aa4678c959cacd21f3fbbbc4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 11:39:43 +0800

    Update elements-shortcodes.php

--- includes/elements-shortcodes.php
+++ includes/elements-shortcodes.php
@@ -106,7 +106,7 @@ function _elements($data, $module_id, $same_height_images)
             case 'image':
                 $same_height = $same_height_images ? 'same_height="true"' : 'same_height="false"';
                 $rounded_corners = $d['rounded_corners'] ? 'true' : 'false';
-                echo do_shortcode('[_image rounded_corners="' . $rounded_corners . '" border_radius="' . $d['border_radius'] . '" ' . $same_height . '  id="' . $d['image'] . '" image_width="' . $d['image_width'] . '" image_height="' . $d['image_height'] . '"]');
+                echo do_shortcode('[_image size="' . $d['size'] . '" rounded_corners="' . $rounded_corners . '" border_radius="' . $d['border_radius'] . '" ' . $same_height . '  id="' . $d['image'] . '" image_width="' . $d['image_width'] . '" image_height="' . $d['image_height'] . '"]');
                 break;
             case 'button':
                 echo do_shortcode('[_button id="' . $d['button_url'] . '" button_url_custom="' . $d['button_url_custom'] . '" button_type="' . $d['button_type'] . '" button_text="' . $d['button_text'] . '" ]');
