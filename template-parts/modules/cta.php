6b411af0975b814d14deceb659d7147bff8df78d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 09:19:26 +0800

    cs

--- template-parts/modules/cta.php
+++ template-parts/modules/cta.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="inner rounded-corner <?= $classes ?>" id="<?= $module_id ?>">
             <div class="row g-4 align-items-end">
-                <div class="col-lg-6">
+                <div class="col-lg-7">
                     <div class="column-holder content-margin">
                         <?= do_shortcode('[_heading heading="' . $module['heading'] . '" class="big-heading" tag="' . $module['tag'] . '"]') ?>
                         <?= do_shortcode("[_description description='" . $module['description'] . "']") ?>
                     </div>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-5">
                     <div class="column-holder text-end">
                         <?= do_shortcode('[_button class="' . $module['button_style'] . '" id="' . $module['button_url'] . '" custom_url="' . $module['button_url_custom'] . '" button_type="' . $module['button_type'] . '" button_text="' . $module['button_text'] . '" ]'); ?>
                     </div>
