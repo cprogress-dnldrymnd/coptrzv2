2b43ef084b7627f01c617a7fa093cb2102bcdd86
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 15:07:38 +0800

    cs

--- includes/_required_files.php
+++ includes/_required_files.php
@@ -14,5 +14,4 @@ require_once('woocommerce.php');
 require_once('ajax.php');
 require_once('svg.php');
 require_once('theme-widgets.php');
-//require_once('elementor.php');
 require_once('checkout.php');
