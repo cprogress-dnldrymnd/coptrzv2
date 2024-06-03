6e3647fa49d2dc1a64d134b7f5666c343a8722d9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 12:52:09 +0800

    Update archive-solutions.php

--- archive-solutions.php
+++ archive-solutions.php
@@ -9,22 +9,25 @@ get_header(); // This fxn gets the header.php file and renders it
 ?>
 
 <section class="archive-grid-v2 md-padding">
-    <div class="section-heading-description content-margin mb-5 text-center">
-        <?= do_shortcode('[_heading heading="Explore our Industry Solutions"]') ?>
-        <?= do_shortcode('[_description description="Before we can help, you need to tell us a bit about you. Which sector below suits your business needs the most? Then we’ll introduce you to your industry expert." ]') ?>
-    </div>
+    <div class="container">
+        <div class="section-heading-description content-margin mb-5 text-center">
+            <?= do_shortcode('[_heading heading="Explore our Industry Solutions"]') ?>
+            <?= do_shortcode('[_description description="Before we can help, you need to tell us a bit about you. Which sector below suits your business needs the most? Then we’ll introduce you to your industry expert." ]') ?>
+        </div>
 
-</section>
-
-<?php if (have_posts()) { ?>
-    <div class="row">
-        <?php while (have_posts()) {
-            the_post() ?>
-            <div class="col-lg-4">
-                <?= do_shortcode('[post_grid class="background-primary" id="' . get_the_ID() . '"]') ?>
+        <?php if (have_posts()) { ?>
+            <div class="row">
+                <?php while (have_posts()) {
+                    the_post() ?>
+                    <div class="col-lg-4">
+                        <?= do_shortcode('[post_grid  id="' . get_the_ID() . '"]') ?>
+                    </div>
+                <?php } ?>
             </div>
         <?php } ?>
     </div>
-<?php } ?>
+</section>
+
+
 <?php get_footer(); // This fxn gets the footer.php file and renders it 
 ?>
