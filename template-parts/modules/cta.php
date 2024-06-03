a133898e6d9e6a52f83cd5bfeb43f8b8e4779fd6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 09:10:59 +0800

    Update cta.php

--- template-parts/modules/cta.php
+++ template-parts/modules/cta.php
@@ -8,13 +8,12 @@
                         <?= do_shortcode("[_description description='" . $module['description'] . "']") ?>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="column-holder">
-                    <?= do_shortcode('[_button id="' . $module['button_url'] . '" custom_url="' . $module['button_url_custom'] . '" button_type="' . $module['button_type'] . '" button_text="' . $module['button_text'] . '" ]'); ?>
+                <div class="col-lg-6">
+                    <div class="column-holder">
+                        <?= do_shortcode('[_button id="' . $module['button_url'] . '" custom_url="' . $module['button_url_custom'] . '" button_type="' . $module['button_type'] . '" button_text="' . $module['button_text'] . '" ]'); ?>
+                    </div>
                 </div>
             </div>
-
         </div>
     </div>
 </section>
