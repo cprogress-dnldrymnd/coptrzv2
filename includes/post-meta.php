4a19367623b1f914d4bbd5a4630a9c94c3462d9b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 12:00:17 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1676,6 +1676,7 @@ Container::make('post_meta', 'Modules')
 															Field::make('textarea', 'description', __('Description')),
 														)
 													)
+													->set_layout('tabbed-vertical')
 													->set_header_template('<%- heading  %>'),
 											)
 										)
@@ -2123,6 +2124,7 @@ Container::make('post_meta', 'Modules')
 									Field::make('textarea', 'description', __('Description')),
 								)
 							)
+							->set_layout('tabbed-vertical'),
 							->set_header_template('<%- heading  %>'),
 					)
 				)
