d89a5ec6daae1b78e2463e6733df842bedb1f31d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 10:08:48 +0800

    Update admin-js.js

--- admin/js/admin-js.js
+++ admin/js/admin-js.js
@@ -54,6 +54,7 @@ function codemirror() {
             wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_body_scripts]"'), cm_settings.ce_html);
         }
 
+        /*
         // Check if TinyMCE is loaded
         if (typeof tinymce !== 'undefined') {
 
@@ -86,7 +87,36 @@ function codemirror() {
             });
         } else {
             console.error('TinyMCE is not loaded.');
-        }
+        }*/
+
+        jQuery(document).on("focus", '.activate-tinymce textarea', function (event) {
+            // Check if TinyMCE is loaded
+            if (typeof tinymce !== 'undefined') {
+                // Loop through each textarea
+                var textareaId = jQuery(this).attr('id');
+                console.log(textareaId);
+                // TinyMCE settings to mimic classic editor
+                tinymce.init({
+                    selector: '#' + textareaId,  // Use the unique ID
+                    plugins: 'lists link charmap paste textcolor',
+                    toolbar: 'formatselect | bold italic | bullist numlist | link | forecolor | charmap | pastetext | removeformat',
+                    block_formats: 'Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Preformatted=pre',
+                    toolbar_location: 'top',
+                    menubar: false,
+                    statusbar: false,
+                    branding: false
+                });
+
+                // Save content on form submission (adjust if your form has a different ID)
+                jQuery('form').submit(function (e) {
+                    var textareaId = jQuery(this).attr('id');
+                    jQuery(this).val(tinymce.get(textareaId).getContent());
+                });
+            } else {
+                console.error('TinyMCE is not loaded.');
+            }
+        });
+
 
     }, 1000);
 }
