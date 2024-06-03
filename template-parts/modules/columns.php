c01a2c40f753dc65ea5096ad966f2b0d2f529d7a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 22:47:31 +0800

    Update columns.php

--- template-parts/modules/columns.php
+++ template-parts/modules/columns.php
@@ -52,7 +52,7 @@
                             ?>
 
 
-                            <div class="column-holder h-100 d-flex <?= $classes ?>" style="<?= $style_attribute ?>">
+                            <div class="column-holder h-100 d-flex overflow-hidden <?= $classes ?>" style="<?= $style_attribute ?>">
                                 <div class="inner content-margin w-100" style="<?= $style_attribute_inner ?>">
                                     <?= _elements($column['items'], $module_id, $module['same_height_images']) ?>
                                 </div>
