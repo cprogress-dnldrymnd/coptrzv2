15da76e345c43c730dfaa984a4ee4446067d8d5d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 15:03:48 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -125,7 +125,7 @@ function enqueue_scripts()
 
 	if (is_checkout()) {
 		wp_enqueue_style('checkout-style', assets_dir . 'stylesheets/checkout/checkout.css', NULL, coptz_version);
-		wp_enqueue_script('checkout-js', assets_dir . 'javascripts/checkout.js', ['jquery'], coptz_version);
+		//wp_enqueue_script('checkout-js', assets_dir . 'javascripts/checkout.js', ['jquery'], coptz_version);
 	}
 }
 
