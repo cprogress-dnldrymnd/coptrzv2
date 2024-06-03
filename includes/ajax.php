9c7a7ead5c0a17222ed505eab7e11cd92bb1cf7c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 17:44:07 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -143,7 +143,7 @@ function archive_ajax()
 				$the_query->the_post();
 		?>
 				<div class="<?= $class ?> col-sm-6 post-item">
-					<div class="post-grid h-100 rounded-corner overflow-hidden">
+					<div class="post-grid h-100 rounded-corner overflow-hidden p-5">
 						<div class="image-holder position-relative">
 							<?php
 							echo do_shortcode('[_image class="image-absolute image-absolute-contain" size="large" id="' . get_post_thumbnail_id(). '"]');
