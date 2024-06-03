3a0b3151f47d619a523ab88c3dc0cda6bb00f7a6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 09:03:59 +0800

    cs

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -852,6 +852,103 @@ Container::make('post_meta', 'Modules')
 					)
 				)
 				->add_fields(
+					'cta',
+					array(
+						Field::make('complex', 'styles', __('Styles'))
+							->set_duplicate_groups_allowed(false)
+							->add_fields(
+								'background_color',
+								array(
+									Field::make('select', 'background_color', 'Background Color')
+										->set_options(
+											array(
+												'background-primary'   => 'Primary',
+												'background-secondary' => 'Secondary',
+												'background-accent'    => 'Accent',
+												'background-white'     => 'White',
+												'background-light-gray'     => 'Light Gray',
+												'background-body-color'     => 'Body',
+												'background-custom'    => 'Custom',
+											)
+										),
+									Field::make('color', 'background_color_custom', __('Background Color'))
+										->set_conditional_logic(
+											array(
+												array(
+													'field' => 'background_color',
+													'value' => 'background-custom',
+												)
+											)
+										),
+								)
+							)
+							->add_fields(
+								'padding',
+								array(
+									Field::make('select', 'padding_top', 'Padding Top')
+										->set_options(
+											array(
+												''                => 'No Padding',
+												'xl-padding-top'  => 'Extra Large',
+												'lg-padding-top'  => 'Large',
+												'md-padding-top'  => 'Medium',
+												'sm-padding-top'  => 'Small',
+												'xxs-padding-top' => 'Extra Small',
+											)
+										),
+									Field::make('select', 'padding_bottom', 'Padding Bottom')
+										->set_options(
+											array(
+												''                   => 'No Padding',
+												'xl-padding-bottom'  => 'Extra Large',
+												'lg-padding-bottom'  => 'Large',
+												'md-padding-bottom'  => 'Medium',
+												'sm-padding-bottom'  => 'Small',
+												'xxs-padding-bottom' => 'Extra Small',
+											)
+										),
+
+								)
+							)
+
+							->set_layout('tabbed-vertical'),
+						Field::make('text', 'heading', __('Heading')),
+						Field::make('textarea', 'description', __('Description')),
+						Field::make('select', 'button_type', __('Button Type'))->set_classes('trigger-selector')
+							->set_options(
+								array(
+									''          => 'Select Button Type',
+									'page'      => 'Page',
+									'post'      => 'Post',
+									'solutions' => 'Solution',
+									'popups'    => 'Popup',
+								)
+							),
+						Field::make('text', 'button_text', __('Button Text')),
+						Field::make('text', 'button_url', __('Button URL'))->set_classes('field-url')
+							->set_conditional_logic(
+								array(
+									array(
+										'field'   => 'button_type',
+										'value'   => 'custom',
+										'compare' => '!='
+									)
+								)
+							),
+						Field::make('html', 'html')
+							->set_html('<div class="page-selector">  </div>'),
+						Field::make('text', 'button_url_custom', __('Button URL'))
+							->set_conditional_logic(
+								array(
+									array(
+										'field' => 'button_type',
+										'value' => 'custom',
+									)
+								)
+							),
+					)
+				)
+				->add_fields(
 					'contact_form',
 					array(
 						Field::make('complex', 'styles', __('Styles'))
---
+++ template-parts/modules/cta.php
@@ -0,0 +1,20 @@
+<section class="call-to-action">
+    <div class="container">
+        <div class="inner <?= $classes ?>" id="<?= $module_id ?>">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="column-holder">
+                        <?= do_shortcode('[_heading heading="' . $module['heading'] . '" class="big-heading"]') ?>
+                        <?= do_shortcode("[_description description='" . $module['description'] . "']") ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="column-holder">
+                    <?= do_shortcode('[_button id="' . $module['button_url'] . '" custom_url="' . $module['button_url_custom'] . '" button_type="' . $module['button_type'] . '" button_text="' . $module['button_text'] . '" ]'); ?>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
