006a0ee54c55167bc892a95f4121ce77cec28888
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-03 13:25:32 +0800

    css

--- woocommerce/loop/loop-start.php
+++ woocommerce/loop/loop-start.php
@@ -19,4 +19,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<ul class="products row g-3 columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
+<ul class="custom-products row g-3 columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
