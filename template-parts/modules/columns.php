6920b21ebc6a5644bc45e333ef4651292a30f5da
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-04 12:53:15 +0800

    Update columns.php

--- template-parts/modules/columns.php
+++ template-parts/modules/columns.php
@@ -46,7 +46,7 @@
                                         $style_attribute_inner .= 'max-width: ' . $style['max_width'] . ';';
                                         break;
                                     case 'column_width':
-                                        $column_class =  $style['column_width'] . ' ' . $style['column_width_tablet'] . ' ' . $style['column_width_mobile'] ? $style['column_width_mobile'] : 'col-sm-12';
+                                        $column_class =  $style['column_width'] . ' ' . $style['column_width_tablet'] . ' ' . ($style['column_width_mobile'] ? $style['column_width_mobile'] : 'col-sm-12');
                                         break;
                                 }
                             }
