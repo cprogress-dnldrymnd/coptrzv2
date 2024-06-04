4f6adcd196e7ed57b0cb6f4641619d6dd93367a1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-04 12:31:13 +0800

    Update elements-shortcodes.php

--- includes/elements-shortcodes.php
+++ includes/elements-shortcodes.php
@@ -109,7 +109,7 @@ function _elements($data, $module_id, $same_height_images)
                 echo do_shortcode('[_image size="' . $d['size'] . '" rounded_corners="' . $rounded_corners . '" border_radius="' . $d['border_radius'] . '" ' . $same_height . '  id="' . $d['image'] . '" image_width="' . $d['image_width'] . '" image_height="' . $d['image_height'] . '"]');
                 break;
             case 'button':
-                echo do_shortcode('[_button id="' . $d['button_url'] . '" button_url_custom="' . $d['button_url_custom'] . '" button_type="' . $d['button_type'] . '" button_text="' . $d['button_text'] . '" ]');
+                echo do_shortcode('[_button class="'.$d['button_style'].'" id="' . $d['button_url'] . '" button_url_custom="' . $d['button_url_custom'] . '" button_type="' . $d['button_type'] . '" button_text="' . $d['button_text'] . '" ]');
                 break;
             case 'accordion':
                 $accordion = $d['accordion'];
