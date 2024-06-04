3bfea5b1603eb6e09eda8d60d8ac2264ebea6a2c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-04 13:02:06 +0800

    Update elements-shortcodes.php

--- includes/elements-shortcodes.php
+++ includes/elements-shortcodes.php
@@ -101,9 +101,9 @@ function _elements($data, $module_id, $same_height_images)
             case 'heading':
                 echo do_shortcode('[_heading tag="' . $d['tag'] . '" heading="' . $d['heading'] . '" class="' . $d['size'] . '"]');
             case 'description':
-                echo $d['description'];
 
-              //  echo do_shortcode('[_description description="' . $d['description'] . '"]');
+                echo '[_description description="' . $d['description'] . '"]';
+                //  echo do_shortcode('[_description description="' . $d['description'] . '"]');
                 break;
             case 'image':
                 $same_height = $same_height_images ? 'same_height="true"' : 'same_height="false"';
@@ -111,7 +111,7 @@ function _elements($data, $module_id, $same_height_images)
                 echo do_shortcode('[_image size="' . $d['size'] . '" rounded_corners="' . $rounded_corners . '" border_radius="' . $d['border_radius'] . '" ' . $same_height . '  id="' . $d['image'] . '" image_width="' . $d['image_width'] . '" image_height="' . $d['image_height'] . '"]');
                 break;
             case 'button':
-                echo do_shortcode('[_button class="'.$d['button_style'].'" id="' . $d['button_url'] . '" button_url_custom="' . $d['button_url_custom'] . '" button_type="' . $d['button_type'] . '" button_text="' . $d['button_text'] . '" ]');
+                echo do_shortcode('[_button class="' . $d['button_style'] . '" id="' . $d['button_url'] . '" button_url_custom="' . $d['button_url_custom'] . '" button_type="' . $d['button_type'] . '" button_text="' . $d['button_text'] . '" ]');
                 break;
             case 'accordion':
                 $accordion = $d['accordion'];
