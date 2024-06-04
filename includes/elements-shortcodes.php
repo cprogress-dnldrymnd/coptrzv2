4edd474c4876a214a7244e664fad57305b65ce30
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-04 13:06:48 +0800

    Update elements-shortcodes.php

--- includes/elements-shortcodes.php
+++ includes/elements-shortcodes.php
@@ -103,7 +103,9 @@ function _elements($data, $module_id, $same_height_images)
                 break;
             case 'description':
                 //echo '[_description description="' . $d['description'] . '"]';
-                 echo do_shortcode('[_description description="' . $d['description'] . '"]');
+                 //echo do_shortcode('[_description description="' . $d['description'] . '"]');
+                 echo do_shortcode("[_description description='".$d['description']."']");
+
                 break;
             case 'image':
                 $same_height = $same_height_images ? 'same_height="true"' : 'same_height="false"';
