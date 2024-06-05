1bfa8cb0edb15c883ad1d5890f9b275d4ea37f64
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 13:54:46 +0800

    Update page-modules.php

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -55,7 +55,7 @@ $modules = get__post_meta('modules');
                         $background_overlay_type = $style['background_overlay_type'];
                         if ($background_overlay_type == 'image') {
                             $background_overlay_image = $style['background_overlay_image'];
-                            if ($style['background_overlay_image_opacity']) {
+                            if ($style['background_overlay_image_opacity'] || $style['background_overlay_image_opacity'] == 0) {
                                 $style_attribute .= '--background-image-opacity: ' . $style['background_overlay_image_opacity'];
                             }
                             $background_image_class = 'no-overlay';
