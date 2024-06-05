c51317acf58bb6286091d7c59887d162c485210c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 12:00:41 +0800

    css

--- template-parts/archive/_archive-post-no-ajax.php
+++ template-parts/archive/_archive-post-no-ajax.php
@@ -10,9 +10,9 @@
                 <?php while (have_posts()) {
                     the_post() ?>
                     <div class="col-lg-4">
-                        <div class="post-grid h-100 rounded-corner p-5 post-<?= get_the_ID() ?>" style="--border-radius: 10px; --padding: 20%">
+                        <div class="post-grid h-100 rounded-corner p-5 post-<?= get_the_ID() ?>" style="--border-radius: 10px; --padding: 30%">
                             <div class="content-margin h-100">
-                                <?= do_shortcode('[_image class="image-absolute image-absolute-contain" id="' . get_post_thumbnail_id(get_the_ID()) . '"]'); ?>
+                                <?= do_shortcode('[_image class="image-absolute" id="' . get_post_thumbnail_id(get_the_ID()) . '"]'); ?>
                                 <?= do_shortcode('[_heading heading="' . get_the_title() . '" tag="h3"]') ?>
                                 <?= do_shortcode('[_description description="' . get_the_excerpt() . '" ]') ?>
                                 <div class="button-box button-accent">
