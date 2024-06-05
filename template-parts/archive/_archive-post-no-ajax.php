b3436a0287c6d77a6b579407952b879f29dd120f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 12:01:01 +0800

    Update _archive-post-no-ajax.php

--- template-parts/archive/_archive-post-no-ajax.php
+++ template-parts/archive/_archive-post-no-ajax.php
@@ -14,7 +14,7 @@
                             <div class="content-margin h-100">
                                 <?= do_shortcode('[_image class="image-absolute" id="' . get_post_thumbnail_id(get_the_ID()) . '"]'); ?>
                                 <?= do_shortcode('[_heading heading="' . get_the_title() . '" tag="h3"]') ?>
-                                <?= do_shortcode('[_description description="' . get_the_excerpt() . '" ]') ?>
+                                <?= do_shortcode('[_description description="xxxx" ]') ?>
                                 <div class="button-box button-accent">
                                     <a href="<?= get_the_permalink() ?>">
                                         LEARN MORE
