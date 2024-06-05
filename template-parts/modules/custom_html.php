6cc6f16c09daef323c9a5e53f5fa45f2bffc6253
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 16:44:00 +0800

    Update custom_html.php

--- template-parts/modules/custom_html.php
+++ template-parts/modules/custom_html.php
@@ -1,4 +1,11 @@
-<section class="custom-html position-relative <?= $classes ?>" id="<?= $module_id ?>">
+<section class="custom-html position-relative <?= $classes ?>" id="<?= $module_id ?>" style="<?= $style_attribute . $container_width_style_attribute ?>">
+  <?php
+  _background_image(array(
+    'baground_image' => $baground_image,
+    'background_image_class' => $background_image_class,
+    'background_overlay_image' => $background_overlay_image,
+  ));
+  ?>
   <?php
   _background_image(array(
     'baground_image' => $baground_image,
