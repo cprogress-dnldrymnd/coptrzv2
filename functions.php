e31e8d11f921448b644d85a84d2614905337a685
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 12:06:55 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -211,10 +211,12 @@ function get_time_diff($post_id)
 }
 function custom_excerpt_length($content, $length = 50)
 {
-	// get the first 80 words from the content and added to the $abstract variable
-	preg_match('/^([^.!?\s]*[\.!?\s]+){0,' . $length . '}/', strip_tags($content), $abstract);
-	// pregmatch will return an array and the first 80 chars will be in the first element 
-	return $abstract[0] . '...';
+	if ($content) {
+		// get the first 80 words from the content and added to the $abstract variable
+		preg_match('/^([^.!?\s]*[\.!?\s]+){0,' . $length . '}/', strip_tags($content), $abstract);
+		// pregmatch will return an array and the first 80 chars will be in the first element 
+		return $abstract[0] . '...';
+	}
 }
 add_filter('excerpt_length', 'custom_excerpt_length', 999);
 
@@ -329,7 +331,7 @@ function action_admin_footer()
 			} else if ($value == 'product') {
 				$selector.html('<?= $select_product ?>');
 			} else if ($value == 'guides') {
-				$selector.html('<?= $select_guide?>');
+				$selector.html('<?= $select_guide ?>');
 			} else if ($value == 'casestudies') {
 				$selector.html('<?= $select_casestudies ?>');
 			} else if ($value == 'solutions') {
