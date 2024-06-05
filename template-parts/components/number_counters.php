13e6b52a4cdf2f0214fedadd12738775e873ef8b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 20:01:31 +0800

    cs

--- template-parts/components/number_counters.php
+++ template-parts/components/number_counters.php
@@ -6,7 +6,16 @@
                     <?= do_shortcode('[_image class="col-auto" size="medium" id="' . $number_counter['icon'] . '"]') ?>
                     <div class="number-counter col">
                         <div class="counter">
-                            <?= $number_counter['number'] ?>
+                            <?php if ($number_counter['prefix']) { ?>
+                                <span class="prefix"><?= $number_counter['prefix'] ?></span>
+                            <?php } ?>
+
+                            <span class="counter-number">
+                                <?= $number_counter['number'] ?>
+                            </span>
+                            <?php if ($number_counter['suffix']) { ?>
+                                <span class="suffix"><?= $number_counter['suffix'] ?></span>
+                            <?php } ?>
                         </div>
                         <?= do_shortcode("[_description description='" . _format_text($number_counter['description']) . "']"); ?>
                     </div>
