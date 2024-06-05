ccfbf845cf1fd021ef4537d911ff6609f1fdfe1a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 14:23:09 +0800

    cs

--- template-parts/components/heading.php
+++ template-parts/components/heading.php
@@ -1,17 +1,19 @@
 <?php
 $GetData = new GetData;
-$heading_small = isset($heading_small) ? $heading_small : false;
 ?>
 <?php
 if (isset($heading) && $heading != '') {
 	$tag = isset($tag) && $tag ? $tag : 'h2';
 ?>
 	<div class="heading-box <?= $class ?>" <?= $GetData->get_data_aos($data_aos) ?>>
-		<?php if ($heading_small) { ?>
-			<span class="prefix"><?= $heading_small ?></span>
+		<?php if ($heading_prefix) { ?>
+			<span class="prefix"><?= $heading_prefix ?></span>
 		<?php } ?>
 		<<?= $tag ?>>
 			<?= $heading ?>
 		</<?= $tag ?>>
+		<?php if ($heading_suffix) { ?>
+			<span class="prefix"><?= $heading_suffix ?></span>
+		<?php } ?>
 	</div>
 <?php } ?>
