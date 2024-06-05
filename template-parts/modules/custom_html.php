0c6b1b77f753f6498bf76bedaf68966dfc5c236f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 16:47:41 +0800

    cs

--- template-parts/modules/custom_html.php
+++ template-parts/modules/custom_html.php
@@ -5,12 +5,15 @@
     'background_image_class' => $background_image_class,
     'background_overlay_image' => $background_overlay_image,
   ));
-  ?>
-  <?php
-  _background_image(array(
-    'baground_image' => $baground_image,
-    'background_image_class' => $background_image_class,
-    'background_overlay_image' => $background_overlay_image,
+  _section_heading_description(array(
+    'heading' => $module['heading'],
+    'description' => $module['description'],
+    'text_align' => $module['text_align'],
+    'heading_prefix' => $module['heading_prefix'],
+    'heading_suffix' => $module['heading_suffix'],
+    'tag' => $module['tag'],
+    'size' => $module['size'],
+    'heading_with_line' => $module['heading_with_line'],
   ));
   ?>
   <div class="custom-html-holder position-relative">
