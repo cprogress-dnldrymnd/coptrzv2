5d7d4f7e29759a31a139a759bbd26d739f8009af
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 16:45:07 +0800

    Update wysiwyg.php

--- template-parts/modules/wysiwyg.php
+++ template-parts/modules/wysiwyg.php
@@ -5,6 +5,17 @@
     'background_image_class' => $background_image_class,
     'background_overlay_image' => $background_overlay_image,
   ));
+
+  _section_heading_description(array(
+    'heading' => $module['heading'],
+    'description' => $module['description'],
+    'text_align' => $module['text_align'],
+    'heading_prefix' => $module['heading_prefix'],
+    'heading_suffix' => $module['heading_suffix'],
+    'tag' => $module['tag'],
+    'size' => $module['size'],
+    'heading_with_line' => $module['heading_with_line'],
+  ));
   ?>
   <div class="container position-relative <?= $container_width_class ?>">
     <?= do_shortcode(wpautop($module['wysiwyg'])) ?>
