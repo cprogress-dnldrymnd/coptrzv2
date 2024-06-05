95392eb72fcf3e6056f6c0c4e3b195b605a2b047
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 14:00:59 +0800

    cs

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -57,11 +57,11 @@ $modules = get__post_meta('modules');
                             $background_overlay_image = $style['background_overlay_image'];
                             if ($style['background_overlay_image_opacity'] || $style['background_overlay_image_opacity'] == 0) {
                                 $style_attribute .= '--background-image-opacity: ' . $style['background_overlay_image_opacity'];
-                            } else if ($background_overlay_type == 'custom') {
-                                $style_attribute = '--background-overlay-custom: ' . $style['background_overlay_custom'];
-                                $background_image_class = 'custom-overlay';
                             }
                             $background_image_class = 'no-overlay';
+                        } else if ($background_overlay_type == 'custom') {
+                            $style_attribute = '--background-overlay-custom: ' . $style['background_overlay_custom'];
+                            $background_image_class = 'custom-overlay';
                         }
 
 
