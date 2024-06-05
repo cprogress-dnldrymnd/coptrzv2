45ad0b3042795d0efe56564f5c7ae24546692f1b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 19:37:39 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -18,8 +18,7 @@ jQuery(document).ready(function ($) {
 });
 
 function input_fields() {
-	jQuery('select option[value="--empty-option--"]').attr('value', '').text('Select Option');
-	jQuery('select option[value="Select Sector"]').attr('value', '');
+	jQuery('.remove-first-option-value select option[value="--empty-option--"]').attr('value', '');
 
 	jQuery(".wpforms-field input, .wpforms-field select, .wpforms-field textarea").on("blur input focus", function () {
 		if (this.value) {
