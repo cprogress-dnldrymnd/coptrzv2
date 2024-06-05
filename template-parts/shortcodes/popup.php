04da5d750f16185e9b39e091eaed2343f51c34a0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 20:35:25 +0800

    Update popup.php

--- template-parts/shortcodes/popup.php
+++ template-parts/shortcodes/popup.php
@@ -4,18 +4,20 @@
         <div class="modal-content background-white">
             <div class="modal-body p-0">
                 <div class="row g-0">
-                    <div class="col-lg-6">
+                    <div class="<?= get_the_post_thumbnail_url($id) ? 'col-lg-6 ': 'col-12' ?>">
                         <div class="form-holder px-4 py-5 h-100 d-flex align-items-center">
                             <div class="form-inner w-100">
                                 <?= do_shortcode(get_the_content(NULL, false, $id)) ?>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6 bg-image">
-                        <div class="position-relative h-100">
-                            <img src="<?= get_the_post_thumbnail_url($id, 'large') ?>" alt="<?= get_the_title($id) ?>">
+                    <?php if (get_the_post_thumbnail_url($id)) { ?>
+                        <div class="<?= get_the_post_thumbnail_url($id) ? 'col-lg-6 ': 'col-12' ?> bg-image">
+                            <div class="position-relative h-100">
+                                <img src="<?= get_the_post_thumbnail_url($id, 'large') ?>" alt="<?= get_the_title($id) ?>">
+                            </div>
                         </div>
-                    </div>
+                    <?php } ?>
                 </div>
             </div>
         </div>
