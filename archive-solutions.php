314e0c83c17b65bdd5f2ec7f6924fc0fcd342c36
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 12:01:31 +0800

    Update archive-solutions.php

--- archive-solutions.php
+++ archive-solutions.php
@@ -9,7 +9,7 @@ get_header(); // This fxn gets the header.php file and renders it
 ?>
 
 <?php
-get_template_part('template-parts/section/content-breadcrumbs');
+//get_template_part('template-parts/section/content-breadcrumbs');
 get_template_part('template-parts/archive/_archive-post-no-ajax');
 
 ?>
