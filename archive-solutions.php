301ff10bf59319b12300a95e2c2d7156cc1dc145
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 11:58:22 +0800

    cs

--- archive-solutions.php
+++ archive-solutions.php
@@ -10,38 +10,9 @@ get_header(); // This fxn gets the header.php file and renders it
 
 <?php
 get_template_part('template-parts/section/content-breadcrumbs');
+get_template_part('template-parts/archive/_archive-post-no-ajax');
+
 ?>
-<section class="archive-grid-v2 md-padding">
-    <div class="container">
-        <div class="section-heading-description content-margin mb-5 text-center">
-            <?= do_shortcode('[_heading heading="Explore our Industry Solutions"]') ?>
-            <?= do_shortcode('[_description description="Before we can help, you need to tell us a bit about you. Which sector below suits your business needs the most? Then we’ll introduce you to your industry expert." ]') ?>
-        </div>
 
-        <?php if (have_posts()) { ?>
-            <div class="row g-4">
-                <?php while (have_posts()) {
-                    the_post() ?>
-                    <div class="col-lg-4">
-                        <div class="post-grid h-100 rounded-corner p-5 post-<?= get_the_ID() ?>" style="--border-radius: 10px; --padding: 20%">
-                            <div class="content-margin h-100">
-                                <?= get_post_thumbnail_id(get_the_ID()) ?>
-                                <?= do_shortcode('[_image class="image-absolute image-absolute-contain" id="' . get_post_thumbnail_id(get_the_ID()) . '"]'); ?>
-                                <?= do_shortcode('[_heading heading="' . get_the_title() . '" tag="h3"]') ?>
-                                <?= do_shortcode('[_description description="' . get_the_excerpt() . '" ]') ?>
-                                <div class="button-box button-accent">
-                                    <a href="<?= get_the_permalink() ?>">
-                                        LEARN MORE
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
-
-        <?php } ?>
-    </div>
-</section>
 
 <?php get_footer(); ?>
