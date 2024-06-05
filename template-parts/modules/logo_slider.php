03caeb326d3b1c9b2486ff1e760f46e363919fdb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 16:40:00 +0800

    Update logo_slider.php

--- template-parts/modules/logo_slider.php
+++ template-parts/modules/logo_slider.php
@@ -1,4 +1,4 @@
-<section class="logo-slider position-relative <?= $classes ?>" id="<?= $module_id ?>" style="<?= $style_attribute ?>">
+<section class="logo-slider position-relative <?= $classes ?>" id="<?= $module_id ?>" style="<?= $style_attribute . $container_width_style_attribute ?>">
     <?php
     _background_image(array(
         'baground_image' => $baground_image,
