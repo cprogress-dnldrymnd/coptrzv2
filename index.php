58186d81f02c4b58d35e32129d76be2259599fcf
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-05 11:41:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,19 +13,9 @@ get_header(); // This fxn gets the header.php file and renders it
 if (is_search()) {
 	get_template_part('template-parts/archive/archive', 'search');
 } else {
-	$location = 'template-parts/archive/archive-'.get_post_type();
-	if(locate_template( $location.'.php' )) {
-		echo 'exists';
-	} else {
-		echo 'not-exist';
+	if(!get_template_part('template-parts/archive/archive', get_post_type())) {
+		get_template_part('template-parts/archive/_archive-post-ajax');
 	}
-
-	if(get_template_part('template-parts/archive/archive', get_post_type())) {
-		echo 'exists';
-	} else {
-		echo 'not-exist';
-	}
-	get_template_part('template-parts/archive/archive', get_post_type());
 }
 ?>
 
