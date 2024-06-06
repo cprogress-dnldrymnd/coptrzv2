98022cff7b3ba6f9df64cf126def758d6b7a440c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 18:14:32 +0800

    css

--- template-parts/modules/testimonials.php
+++ template-parts/modules/testimonials.php
@@ -85,7 +85,7 @@ $testimonial_rating = get__theme_option('testimonial_rating');
 
         <div class="review-holder review-holder-case-study">
             <div class="swiper  mySwiper-Reviews mySwiper-ReviewsCaseStudy">
-                <div class="swiper-wrapper align-items-center">
+                <div class="swiper-wrapper ">
                     <?php foreach ($testimonials_arr as $testimonial) { ?>
                         <div class="swiper-slide">
                             <div class="review-box case-study background-primary d-flex">
