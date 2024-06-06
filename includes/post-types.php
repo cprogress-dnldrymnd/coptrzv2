c5a432453083d68639bd5dbbe2de2be2ebab83e3
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 16:29:25 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -157,6 +157,40 @@ $Testimonials->publicly_queryable = false;
 $Testimonials->show_in_admin_bar = false;
 $Testimonials->has_archive = false;
 
+
+$Testimonial_Category = new newTaxonomy();
+$Testimonial_Category->taxonomy = 'testimonial_category';
+$Testimonial_Category->post_type = 'testimonials';
+$Testimonial_Category->args = array(
+    'label'        => 'Testimonial Categories',
+    'labels' => array(
+        'name'                       => _x('Testimonial Categories', 'Taxonomy General Name', 'text_domain'),
+        'singular_name'              => _x('Testimonial Category', 'Taxonomy Singular Name', 'text_domain'),
+        'menu_name'                  => __('Testimonial Category', 'text_domain'),
+        'all_items'                  => __('All Items', 'text_domain'),
+        'parent_item'                => __('Parent Item', 'text_domain'),
+        'parent_item_colon'          => __('Parent Item:', 'text_domain'),
+        'new_item_name'              => __('New Item Name', 'text_domain'),
+        'add_new_item'               => __('Add New Item', 'text_domain'),
+        'edit_item'                  => __('Edit Item', 'text_domain'),
+        'update_item'                => __('Update Item', 'text_domain'),
+        'view_item'                  => __('View Item', 'text_domain'),
+        'separate_items_with_commas' => __('Separate items with commas', 'text_domain'),
+        'add_or_remove_items'        => __('Add or remove items', 'text_domain'),
+        'choose_from_most_used'      => __('Choose from the most used', 'text_domain'),
+        'popular_items'              => __('Popular Items', 'text_domain'),
+        'search_items'               => __('Search Items', 'text_domain'),
+        'not_found'                  => __('Not Found', 'text_domain'),
+        'no_terms'                   => __('No items', 'text_domain'),
+        'items_list'                 => __('Items list', 'text_domain'),
+        'items_list_navigation'      => __('Items list navigation', 'text_domain'),
+    ),
+    'hierarchical' => true,
+    'query_var'    => true,
+    'show_in_rest' => true,
+
+);
+
 $Events = new newPostType();
 $Events->name = 'Events';
 $Events->singular_name = 'Event';
