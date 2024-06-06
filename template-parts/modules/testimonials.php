75afa4b083a192a02c3787dced9134cbadca54ca
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 18:08:20 +0800

    Update testimonials.php

--- template-parts/modules/testimonials.php
+++ template-parts/modules/testimonials.php
@@ -83,7 +83,7 @@ $testimonial_rating = get__theme_option('testimonial_rating');
         ));
         ?>
 
-        <div class="review-holder <?= $type == 'case-study' ? 'review-holder-case-study' : '' ?>">
+        <div class="review-holder review-holder-case-study">
             <div class="swiper  mySwiper-Reviews mySwiper-ReviewsCaseStudy">
                 <div class="swiper-wrapper align-items-center">
                     <?php foreach ($testimonials_arr as $testimonial) { ?>
