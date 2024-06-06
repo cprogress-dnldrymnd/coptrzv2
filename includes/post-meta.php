59edb32e1fe8b90404817e704cf52026452b389d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 16:20:15 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1897,7 +1897,7 @@ Container::make('post_meta', 'Modules')
 									)
 								)
 							),
-						Field::make('text', 'image_size_ratio', __('Image Size Ratio'))->set_width(33)
+						Field::make('text', 'image_size_ratio', __('Image Size Ratio'))->set_width(100)
 							->set_help_text('Default is 30%')
 							->set_conditional_logic(
 								array(
