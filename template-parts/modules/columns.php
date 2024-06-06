6301656dadbdbb99de6be61e5dc877be9fb31cd5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 16:22:21 +0800

    Update columns.php

--- template-parts/modules/columns.php
+++ template-parts/modules/columns.php
@@ -1,4 +1,4 @@
-<section class="columns position-relative <?= $classes ?> " id="<?= $module_id ?>" style="<?= $style_attribute . $container_width_style_attribute ?><?= $image_size_ratio ? 'padding: ' . $image_size_ratio : '' ?>">
+<section class="columns position-relative <?= $classes ?> " id="<?= $module_id ?>" style="<?= $style_attribute . $container_width_style_attribute ?><?= $module['image_size_ratio'] ? 'padding: ' . $module['image_size_ratio'] : '' ?>">
     <?php
     _background_image(array(
         'baground_image' => $baground_image,
