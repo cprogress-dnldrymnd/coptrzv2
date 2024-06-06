b958973f77df66bc19129670719ed2cf930a712c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 14:04:35 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -4049,7 +4049,8 @@ Container::make('post_meta', 'Modules')
 							->set_options(
 								array(
 									''      => 'Custom',
-									'faqs'      => 'FAQs',
+									'faqs'      => 'FAQs Select Manually',
+									'faqs_category'      => 'FAQs by Category',
 								)
 							),
 						Field::make('complex', 'accordion', __('Accordion'))
@@ -4090,6 +4091,24 @@ Container::make('post_meta', 'Modules')
 									array(
 										'field' => 'accordion_source',
 										'value' => 'faqs',
+										'comapre' => '='
+									)
+								)
+							),
+						Field::make('association', 'faqs_category', 'Select FAQs Category')
+							->set_types(
+								array(
+									array(
+										'type'      => 'term',
+										'taxonomy' => 'faqs_category',
+									)
+								)
+							)
+							->set_conditional_logic(
+								array(
+									array(
+										'field' => 'accordion_source',
+										'value' => 'faqs_category',
 										'comapre' => '='
 									)
 								)
