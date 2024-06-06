587c8e84df9ce2278ff594b14722c0ac9f2d67ba
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 19:11:08 +0800

    Update icon.php

--- template-parts/components/icon.php
+++ template-parts/components/icon.php
@@ -1,6 +1,7 @@
 <?php
 $url = wp_get_attachment_url($id);
-echo $url;
+$content = file_get_contents($url);
+echo $content;
 ?>
 <div class="icon-box <?= $class ?>">
     <?= output_svg_from_url($url) ?>
