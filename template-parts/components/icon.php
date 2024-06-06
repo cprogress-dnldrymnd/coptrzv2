802820fa254fdfdbc2482eab06736b0a75a171c3
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 19:33:50 +0800

    Update icon.php

--- template-parts/components/icon.php
+++ template-parts/components/icon.php
@@ -1,6 +1,6 @@
 <?php
 $url = wp_get_original_image_path($id);
 ?>
-<div class="icon-box <?= $class ?>">
+<div class="icon-box <?= $class ?>" style="--width: <?= $icon_width ?>; --height: <?= $icon_height ?>; <?= $icon_color_custom ? 'color: ' . $icon_color_custom : '' ?>">
     <?= output_svg_from_url($url) ?>
 </div>
