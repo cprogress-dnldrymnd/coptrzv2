0efec11641d7f4c26049c95eecbdd5c7c8e26d2b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-06 12:32:58 +0800

    Update post_grid.php

--- template-parts/shortcodes/post_grid.php
+++ template-parts/shortcodes/post_grid.php
@@ -8,7 +8,7 @@
             $captured_by = get__post_meta_by_id($id, 'captured_by');
             ?>
             <div class="button-box button-accent">
-                <button>
+                <button description="<?= get_the_content(null, false, $id) ?>" title="<?= get_the_title($id) ?>">
                     View Model
                 </button>
             </div>
