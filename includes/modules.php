39c04c2fc05173dadda0b1297c3e6e4ad600c1f5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-07 21:03:38 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -22,7 +22,6 @@ function _elements($data, $module_id, $same_height_images)
             case 'heading':
                 $class = $d['size'];
                 $class .= $d['text_color'];
-                echo $d['text_color'];
                 echo do_shortcode('[_heading tag="' . $d['tag'] . '" heading="' . $d['heading'] . '" class="' . $class . '"]');
                 break;
             case 'description':
