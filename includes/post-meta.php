bd3fbeddc779218fdc750a64e1392e0206a47ccf
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-08 01:10:07 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -4986,8 +4986,8 @@ Container::make('post_meta', 'Modules')
 									'col-lg-4'                => 3,
 									'col-lg-3'                => 4,
 								)
-									->set_default_value(4)
-							),
+							)
+							->set_default_value(4),
 						Field::make('complex', 'post_elements', 'Post Elements')
 							->set_duplicate_groups_allowed(false)
 							->add_fields(
