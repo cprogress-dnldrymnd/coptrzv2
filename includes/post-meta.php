b66ff34736a9fb586088a71c091b6e368bd6edea
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-07 21:01:01 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2320,6 +2320,7 @@ Container::make('post_meta', 'Modules')
 												Field::make('select', 'text_color', 'Text Color')
 													->set_options(
 														array(
+															''   => 'Default',
 															'text-primary'   => 'Primary',
 															'text-secondary' => 'Secondary',
 															'text-accent'    => 'Accent',
