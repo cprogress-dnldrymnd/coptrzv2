1603caadcc791cb2c444fb0d8de394b35eb9a8a6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-08 01:15:11 +0800

    Update post_grid.php

--- template-parts/modules/post_grid.php
+++ template-parts/modules/post_grid.php
@@ -53,13 +53,11 @@ foreach ($posts as $post) {
 
         <?php if ($posts_lists) { ?>
             <div class="row g-4">
-                <?php
-                $post_elements = $module['post_elements'];
-                ?>
                 <?php foreach ($posts_lists as $post) { ?>
                     <div class="<?= $number_of_columns ? $number_of_columns : 'col-lg-4' ?>">
                         <?php
                         $id = $post->ID;
+                        $post_elements = $module['post_elements'];
                         include locate_template('template-parts/components/post_grid.php');
                         ?>
                     </div>
