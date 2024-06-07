c09c0692f20e806d4f2608f5b511ccb121ba1152
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-07 20:50:18 +0800

    Update description.php

--- template-parts/components/description.php
+++ template-parts/components/description.php
@@ -1,8 +1,11 @@
 <?php
 $GetData = new GetData;
+
+$text = str_replace("'", "&#39;", $text);
+$text = str_replace('"', "&quot;", $text);
 ?>
 <?php if ($description) { ?>
 	<div class="description-box <?= $class ?>" <?= $GetData->get_data_aos($data_aos) ?>>
-		<?= do_shortcode(wpautop($description)) ?>
+		<?= do_shortcode(wpautop(html_entity_decode($description))) ?>
 	</div>
 <?php } ?>
