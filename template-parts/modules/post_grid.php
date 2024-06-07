e144cba95bce0b51460c07fb838bf5fb9616c3da
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-07 23:31:36 +0800

    Update post_grid.php

--- template-parts/modules/post_grid.php
+++ template-parts/modules/post_grid.php
@@ -2,8 +2,27 @@
 $source = $module['source'];
 echo $source;
 ?>
-<section class="post-grid">
+<section class="post-grid position-relative <?= $classes ?>" id="<?= $module_id ?>" style="<?= $style_attribute . $container_width_style_attribute ?>">
+    <?php
+    _background_image(array(
+        'baground_image' => $baground_image,
+        'background_image_class' => $background_image_class,
+        'background_overlay_image' => $background_overlay_image,
+    ));
+    ?>
     <div class="container">
+        <?php
+        _section_heading_description(array(
+            'heading' => $module['heading'],
+            'description' => $module['description'],
+            'text_align' => $module['text_align'],
+            'heading_prefix' => $module['heading_prefix'],
+            'heading_suffix' => $module['heading_suffix'],
+            'tag' => $module['tag'],
+            'size' => $module['size'],
+            'heading_with_line' => $module['heading_with_line'],
+        ));
+        ?>
         <?php
         // Build the args
         $args = array(
