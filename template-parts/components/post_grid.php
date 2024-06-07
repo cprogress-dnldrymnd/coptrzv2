e294e6a76c643e133383d4e25436c012812bf2de
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-08 01:38:58 +0800

    Update post_grid.php

--- template-parts/components/post_grid.php
+++ template-parts/components/post_grid.php
@@ -1,5 +1,5 @@
 <div class="post-grid post-<?= $id ?> <?= $classes ?>" style="<?= $style_attribute ?>;--padding: 40%">
-    <div class="inner content-margin <?= $style_attribute_inner ?>">
+    <div class="inner content-margin" style="<?= $style_attribute_inner ?>">
         <?php
         foreach ($post_elements as $el) {
             $type = $el['_type'];
