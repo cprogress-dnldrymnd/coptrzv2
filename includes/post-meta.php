417b8fd8d651b4228db41d25834ceb25de5ef750
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-08 02:08:39 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -5313,6 +5313,7 @@ Container::make('post_meta', 'Modules')
 								array(
 									Field::make('text', 'custom_field_key', __('Custom Field Key')),
 									Field::make('text', 'custom_field_type', __('Custom Field Type')),
+									Field::make('text', 'custom_field_class', __('Wrapper Class')),
 								)
 							)
 							->set_header_template('Custom Field: <%- custom_field_key  %>')
@@ -5321,6 +5322,7 @@ Container::make('post_meta', 'Modules')
 								array(
 									Field::make('text', 'custom_field_key', __('Custom Field Key')),
 									Field::make('text', 'custom_field_type', __('Custom Field Type')),
+									Field::make('text', 'custom_field_class', __('Wrapper Class')),
 								)
 							)
 							->set_header_template('Custom Field: <%- custom_field_key  %>')
@@ -5329,6 +5331,7 @@ Container::make('post_meta', 'Modules')
 								array(
 									Field::make('text', 'custom_field_key', __('Custom Field Key')),
 									Field::make('text', 'custom_field_type', __('Custom Field Type')),
+									Field::make('text', 'custom_field_class', __('Wrapper Class')),
 								)
 							)
 							->set_header_template('Custom Field: <%- custom_field_key  %>')
@@ -5337,6 +5340,7 @@ Container::make('post_meta', 'Modules')
 								array(
 									Field::make('text', 'custom_field_key', __('Custom Field Key')),
 									Field::make('text', 'custom_field_type', __('Custom Field Type')),
+									Field::make('text', 'custom_field_class', __('Wrapper Class')),
 								)
 							)
 							->set_header_template('Custom Field: <%- custom_field_key  %>')
