0ed8aa8ca207f89e118188e0790aa483773f5b80
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 16:18:27 +0800

    Update post_grid.php

--- template-parts/components/post_grid.php
+++ template-parts/components/post_grid.php
@@ -33,7 +33,7 @@ $arr = array_column($post_elements, '_type');
                     }
                     break;
                 case 'permalink':
-                    echo do_shortcode('[_button class="' . $el['button_style'] . '" id="' . $id . '"  button_type="' . get_post_type($id) . '" button_text=" ' . $el['button_text'] ? $el['button_text'] : 'Read More' . '" ]');
+                    echo do_shortcode('[_button class="' . $el['button_style'] . '" id="' . $id . '"  button_type="' . get_post_type($id) . '" button_text=" ' . ($el['button_text']) ? $el['button_text'] : 'Read More' . '" ]');
                     break;
                 case 'custom_field_1':
                 case 'custom_field_2':
