8df9649d3da06698c680b54b28171250ddadda1b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 19:19:05 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -11368,9 +11368,8 @@ Block::make(__('Modules'))
 	function generate_carbon_field_block_content($block_name, $field_values) {
 		ob_start(); // Start output buffering
 	
-		
 		// Render Carbon Fields block template
-		Carbon_Fields\Block::render($block_name, $field_values);
+		Carbon_Fields\Block_Template::render($block_name, $field_values);
 	
 		return ob_get_clean(); // Return buffered content
 	}
