d64badd01f0d3d41885a86786ccda7c37a86501a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 15:07:11 +0800

    Update wysiwyg.php

--- template-parts/modules/wysiwyg.php
+++ template-parts/modules/wysiwyg.php
@@ -8,7 +8,7 @@
 
   ?>
   <div class="container position-relative <?= $container_width_class ?> ">
-    <div class="inner <?= $inner_class ?>">
+    <div class="inner content-margin <?= $inner_class ?>">
       <?php
       _section_heading_description(array(
         'heading' => $module['heading'],
