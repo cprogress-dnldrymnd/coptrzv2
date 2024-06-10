04e5edd685f916cc3e6a7a872ffa41d3b19414d0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 16:09:18 +0800

    Update post_grid.php

--- template-parts/components/post_grid.php
+++ template-parts/components/post_grid.php
@@ -2,6 +2,9 @@
 $key = array_search('permalink', $post_elements);
 echo $key;
 echo 'test';
+echo '<pre>';
+var_dump($post_elements);
+echo '</pre>';
 ?>
 <div class="post-grid post-grid-style-2 h-100 d-flex post-<?= $id ?> <?= $classes ?>" style="<?= $style_attribute_post ?>--padding: 40%">
     <div class="inner position-relative content-margin-small w-100" style="<?= $style_attribute_inner ?>">
