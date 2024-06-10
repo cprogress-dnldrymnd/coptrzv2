7082a6c9dc00751b3278a356cfb353049e39e207
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 18:29:55 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -157,7 +157,7 @@ function action_admin_enqueue_scripts($hook)
 
 	wp_enqueue_script('select_2_js', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js', array(), '1.0');
 
-	//wp_enqueue_script('admin_js', get_template_directory_uri() . '/admin/js/admin-js.js', array(), '1.3');
+	wp_enqueue_script('admin_js', get_template_directory_uri() . '/admin/js/admin-js.js', array(), '1.3');
 }
 add_action('admin_enqueue_scripts', 'action_admin_enqueue_scripts');
 
@@ -359,7 +359,7 @@ function action_admin_footer()
 <?php
 }
 
-//add_action('admin_footer', 'action_admin_footer');
+add_action('admin_footer', 'action_admin_footer');
 
 function get__posts($post_type)
 {
