c5138218847013ac1cb9b729f14390353fc23273
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 21:23:04 +0800

    Update templates.php

--- template-parts/modules/templates.php
+++ template-parts/modules/templates.php
@@ -1,5 +1,7 @@
 <?php
+echo 'tesmplate';
 foreach ($module['templates'] as $template) {
     $modules = get__post_meta_by_id($template['id'], 'modules');
     echo modules($modules);
+    var_dump($modules);
 }
