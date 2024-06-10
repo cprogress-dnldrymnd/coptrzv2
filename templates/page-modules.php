ac71249f22af4a24e058feb62b8253eb0ae1ae16
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 19:11:46 +0800

    cs

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -6,6 +6,18 @@
 <?php get_header(); ?>
 <?php
 $modules = get__post_meta('modules');
+// Example block data (replace with your actual block names and field values)
+$blocks_data = array(
+    array(
+        'name' => 'modules',
+        'fields' => array(
+            'modules' => $modules,
+        )
+    ),
+);
+
+// Insert the blocks into a post with ID 123
+insert_carbon_field_blocks_into_post(get_the_ID(), $blocks_data);
 ?>
 <div class="modules">
     <?php
