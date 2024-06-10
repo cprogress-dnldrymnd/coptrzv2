a008aeeabcccc99dea148436c6dc2b72fade2f88
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-10 19:31:22 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -6099,7 +6099,7 @@ Container::make('term_meta', __('Category Properties'))
 /*-----------------------------------------------------------------------------------*/
 /* Gutenberg Blocks
 /*-----------------------------------------------------------------------------------*/
-Block::make(__('Modules'))
+Block::make(__('My Shiny Gutenberg Block'))
 	->add_fields(array(
 		Field::make('complex', 'modules', __('Modules'))
 			->setup_labels(
@@ -11270,5 +11270,21 @@ Block::make(__('Modules'))
 			->set_collapsed(true)
 	))
 	->set_render_callback(function ($fields, $attributes, $inner_blocks) {
-		echo 'sdsdsds';
+?>
+dsdsds
+	<div class="block">
+		<div class="block__heading">
+			<h1><?php echo esc_html($fields['heading']); ?></h1>
+		</div><!-- /.block__heading -->
+
+		<div class="block__image">
+			<?php echo wp_get_attachment_image($fields['image'], 'full'); ?>
+		</div><!-- /.block__image -->
+
+		<div class="block__content">
+			<?php echo apply_filters('the_content', $fields['content']); ?>
+		</div><!-- /.block__content -->
+	</div><!-- /.block -->
+
+<?php
 	});
