4c45880f4ac4f83db08757bf6b4d4d88fd9ba64f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-12 14:33:13 +0800

    Update content.php

--- template-parts/single/single-post/content.php
+++ template-parts/single/single-post/content.php
@@ -1,6 +1,6 @@
 <section class="post-details">
     <div class="container">
-        
+        <?= do_shortcode('[_image id="' . get_post_thumbnail_id(get_the_ID()) . '"]') ?>
     </div>
 </section>
 
