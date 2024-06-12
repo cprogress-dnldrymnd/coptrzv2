01b928fff9448a4b5c804dd3c94465cc438527e7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-12 17:12:29 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -718,12 +718,11 @@ class Shortcodes
 		global $post;
 		$url = get_permalink($post->ID);
 		$title = str_replace(' ', '%20', get_the_title($post->ID));
-		
+
 		$social_buttons = '';
 		$social_buttons .= '<div class="social-share-buttons">';
-		$social_buttons .= '<a href="https://www.facebook.com/sharer.php?u=' . $url . '&t=' . $title . '" target="_blank">Share on Facebook</a>';
-		$social_buttons .= '<a href="https://twitter.com/share?url=' . $url . '&text=' . $title . '" target="_blank">Share on Twitter</a>';
-		// Add more social networks here (e.g., LinkedIn, Pinterest, etc.)
+		$social_buttons .= '<a href="#" onclick="window.open(\'https://www.facebook.com/sharer.php?u=' . $url . '&t=' . $title . '\', \'_blank\', \'width=600,height=400\'); return false;">Share on Facebook</a>';
+		$social_buttons .= '<a href="#" onclick="window.open(\'https://twitter.com/share?url=' . $url . '&text=' . $title . '\', \'_blank\', \'width=600,height=400\'); return false;">Share on Twitter</a>';
 		$social_buttons .= '</div>';
 		return $social_buttons;
 	}
