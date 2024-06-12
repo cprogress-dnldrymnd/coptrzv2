739f8eebfe2803b0d2b66d56bbc7ff1f4995f477
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-12 14:46:22 +0800

    Update content.php

--- template-parts/single/single-post/content.php
+++ template-parts/single/single-post/content.php
@@ -18,25 +18,24 @@
     </div>
 </section>
 
-<section class="post-content background-secondary scrolling-section-v2">
-    <div class="container-fluid p-0">
-        <div class="row gx-0 gy-4">
-            <div class="col-description col-left <?= get_the_post_thumbnail() ? 'col-lg-6' : 'col-12' ?>">
-                <div class="column-holder lg-padding">
-                    <div class="description-box content-margin">
-                        <?php the_content() ?>
-                    </div>
+<section class="post-content-v2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="column-holder">
+
                 </div>
             </div>
-            <?php if (get_the_post_thumbnail()) { ?>
-                <div class="col-lg-6 col-image col-right">
-                    <div class="column-holder ">
-                        <div class="image-box">
-                            <?php the_post_thumbnail('full') ?>
-                        </div>
-                    </div>
+            <div class="col-lg-6">
+                <div class="column-holder the-content">
+                    <?php the_content() ?>
                 </div>
-            <?php } ?>
+            </div>
+            <div class="col-lg-3">
+                <div class="column-holder">
+
+                </div>
+            </div>
         </div>
     </div>
 </section>
