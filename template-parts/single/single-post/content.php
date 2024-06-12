4b52b836ef5773726705d30f9786a020ecd05069
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-12 14:41:07 +0800

    Update content.php

--- template-parts/single/single-post/content.php
+++ template-parts/single/single-post/content.php
@@ -1,7 +1,7 @@
 <section class="post-details">
     <div class="container content-margin">
         <?= do_shortcode('[_image class="blog-image image-absolute rounded-corner overflow-hidden" id="' . get_post_thumbnail_id(get_the_ID()) . '"]') ?>
-        <?= do_shortcode('[_description description="' . _format_text(get_the_excerpt()) . '" ]') ?>
+        <?= do_shortcode('[_description description="' . _format_text(custom_excerpt_length(get_the_excerpt()), 200) . '" ]') ?>
 
         <div class="row g-4 justify-content-between">
             <div class="col-auto">
