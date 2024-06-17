33d8f08eac40dd4cc17d28a2b074dbc7d2726b0f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-17 21:14:41 +0800

    Update checkout.js

--- assets/javascripts/checkout.js
+++ assets/javascripts/checkout.js
@@ -180,7 +180,7 @@ function coupon_ajax() {
 
     } else {
         jQuery('.coupon-message').html('<span class="failed">Please enter a valid coupon code.</span>');
-        jQuery('.td-coupon .blockUI').removeClass('active');
+        jQuery('.blockUI.coupon-ui').removeClass('active');
 
     }
 }
