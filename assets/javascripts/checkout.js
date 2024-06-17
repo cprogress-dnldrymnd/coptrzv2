a7319b3abb0a91ab2f098c637d64d86461e2d407
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-17 21:39:11 +0800

    Update checkout.js

--- assets/javascripts/checkout.js
+++ assets/javascripts/checkout.js
@@ -101,7 +101,18 @@ function validate() {
         jQuery('.nav-box ul li.active').removeClass('active').next().addClass('active');
         $step = jQuery('.step-box.active').attr('step');
         previous_step($step);
-    } 
+    } else {
+        // lock scroll position, but retain settings for later
+        var scrollPosition = [
+            self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+            self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+        ];
+        var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
+        html.data('scroll-position', scrollPosition);
+        html.data('previous-overflow', html.css('overflow'));
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+    }
 }
 
 
