004bb3ba38b2e663c92a82182fc799d034d74809
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-26 17:58:15 +0800

    cs

---
+++ includes/post-meta.php
@@ -0,0 +1,27 @@
+<?php
+
+use Carbon_Fields\Block;
+use Carbon_Fields\Container;
+use Carbon_Fields\Complex_Container;
+use Carbon_Fields\Field;
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Modules
+/*-----------------------------------------------------------------------------------*/
+
+Container::make('post_meta', __('Book Data'))
+    ->where('post_template', '=', 'templates/page-modules.php')
+    ->add_fields(array(
+        Field::make('complex', 'Module')
+            ->add_fields('Section', array(
+                Field::make('image', 'image'),
+                Field::make('text', 'caption'),
+            ))
+            ->add_fields('movie', array(
+                Field::make('file', 'video'),
+                Field::make('text', 'title'),
+                Field::make('text', 'length'),
+            ))
+
+    ));
