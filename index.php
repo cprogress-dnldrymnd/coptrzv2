cc8f2ac03049c474dd16d89d8b367620fe8a7759
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-26 17:20:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * The template for displaying the home/index page.
+ * This template will also be called in any case where the Wordpress engine 
+ * doesn't know which template to use (e.g. 404 error)
+ */
+get_header(); // This fxn gets the header.php file and renders it 
+?>
+
+<?php get_template_part('template-parts/section/content-breadcrumbs'); ?>
+<?php
+if (is_search()) {
+	get_template_part('template-parts/archive/archive', 'search');
+} else {
+	get_template_part('template-parts/archive/_archive-post-ajax');
+}
+?>
+
+<?php get_footer(); ?>
