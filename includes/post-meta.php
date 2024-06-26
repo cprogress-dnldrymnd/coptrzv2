941f195159a3a42cef4e50e53603a0f4afe24df5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-26 18:22:35 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -80,8 +80,16 @@ Container::make('post_meta', __('Modules'))
                             'singular_name' => 'Column',
                         )
                     )
+
                     ->add_fields('column', array(
                         Field::make('complex', 'items', __('Items'))
+                        ->set_layout('tabbed-vertical')
+                            ->setup_labels(
+                                array(
+                                    'plural_name'   => 'Items',
+                                    'singular_name' => 'Item',
+                                )
+                            ->set_collapsed(true)
                             ->add_fields('heading', $heading_fields)
                             ->add_fields(
                                 'description',
@@ -229,13 +237,7 @@ Container::make('post_meta', __('Modules'))
                                     Field::make('textarea', 'custom_html', __('Custom HTML')),
                                 )
                             )
-                            ->set_layout('tabbed-vertical')
-                            ->setup_labels(
-                                array(
-                                    'plural_name'   => 'Items',
-                                    'singular_name' => 'Item',
-                                )
-                            )
+                            
                     ))
 
             ))
