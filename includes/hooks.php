81a3541b975855395aa962a0cf377997cbeab9b1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-29 04:53:36 +0800

    css

--- includes/hooks.php
+++ includes/hooks.php
@@ -5,3 +5,74 @@ function action_wp_head()
 }
 
 add_action('wp_head', 'action_wp_head');
+
+
+function action_admin_head()
+{
+?>
+	<style>
+		.columns>.cf-field__body>.cf-complex__groups {
+			display: flex
+		}
+
+		.columns>.cf-field__body>.cf-complex__groups>div {
+			flex: 1;
+			padding: 5px;
+		}
+
+		.cb-label.cb-label.cb-label {
+			background-color: var(--wp-admin-theme-color);
+			color: #fff;
+			font-family: Courier;
+			font-size: 16px;
+			padding: 5px;
+			font-weight: bold;
+			text-transform: uppercase;
+		}
+
+		.cb-label-end.cb-label-end.cb-label-end {
+			background-color: var(--wp-admin-theme-color);
+			padding: 5px;
+		}
+
+		.inline-field.inline-field {
+			display: flex;
+			flex-wrap: wrap;
+		}
+
+		.inline-field.inline-field .cf-field__head {
+			flex: 0 0 10%;
+		}
+
+		.inline-field.inline-field .cf-field__body {
+			flex: 0 0 90%;
+		}
+
+		.inline-field.inline-field .cf-field__help {
+			margin-left: 10%;
+		}
+
+		.postbox-header {
+			background-color: lightblue;
+		}
+
+		.edit-post-meta-boxes-area .postbox {
+			margin-bottom: 10px;
+		}
+
+		<?php if (_is_module()) { ?>.wp-block-post-content {
+			display: none !important
+		}
+
+		.edit-post-header__toolbar {
+			display: none;
+		}
+
+		<?php } ?>
+	</style>
+
+<?php
+
+}
+
+add_action('admin_head', 'action_admin_head');
