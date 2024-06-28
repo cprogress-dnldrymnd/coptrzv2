bfe9cecf36f9f9aed537b0530e31651c7de31868
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-28 20:41:22 +0800

    Create shortcodes.php

---
+++ includes/shortcodes.php
@@ -0,0 +1,24 @@
+<?php
+
+class Shortcodes
+{
+    function __heading($atts)
+    {
+        extract(
+            shortcode_atts(
+                array(
+                    'heading' => '',
+                    'tag' => 'h2',
+                    'class' => '',
+                ),
+                $atts
+            )
+        );
+
+        if ($heading) {
+            return "<$tag class='$class'>$heading</$tag>";
+        }
+    }
+}
+
+add_shortcode('__heading', array($Shortcodes, '__heading'));
