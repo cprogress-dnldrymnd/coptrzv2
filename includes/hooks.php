8372970cf0a9f42e36de65d635f9214d3a8bfade
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-28 22:24:25 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -5,7 +5,7 @@ function action_wp_head()
     if (!$hero_hidden) {
         $hero_background = get__post_meta('hero_background');
         $hero_background_url = wp_get_attachment_image_url($hero_background, 'full');
-        $style = ".hero{ background-image: $hero_background_url}";
+        $style = ".hero{ background-image: url($hero_background_url)}";
         echo '<style id="hero-styles">';
         echo $style;
         echo '</style>';
