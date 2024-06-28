9e1f6da8ddfcabeaa6a30e7450fcc5109d53d11e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-28 22:38:27 +0800

    cs

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -13,8 +13,6 @@ $modules = get__post_meta('modules');
 <div class="modules">
     <?php
     the_content();
-    echo do_shortcode('[__heading heading="test"]');
-    // echo modules($modules);
     ?>
 </div>
 
