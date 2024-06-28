07ca951e3bf368c3067fb09a36b05402037c3f82
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-28 20:29:43 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1,11 +1,19 @@
 <?php
-function action_update_zip_url()
+function action_module_content()
 {
     // Check if a post was updated (add your specific conditions here)
     if (did_action('post_updated') && is_single()) {
         // Check if this is an autosave
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
 
+        // Update post 37
+        $my_post = array(
+            'ID'           => get_the_ID(),
+            'post_content' => 'This is the updated content.',
+        );
+
+        // Update the post into the database
+        wp_update_post($my_post);
     }
 }
-add_action('shutdown', 'action_update_zip_url');
+add_action('shutdown', 'action_module_content');
