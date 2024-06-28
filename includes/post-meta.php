d2e912c17500a244d89b2cf69a503432845429bb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-29 05:06:47 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -10,7 +10,7 @@ use Carbon_Fields\Field;
 
 Container::make('post_meta', __('Preview'))
     ->add_fields(array(
-        Field::make('html', 'preview')->set_html(get_the_content(NULL, false, $_GET['post']))
+        Field::make('html', 'preview')->set_html('<iframe src="' . get_permalink($_GET['post']) . '"></iframe>')
     ));
 Container::make('post_meta', __('Hero'))
     ->add_fields(array(
