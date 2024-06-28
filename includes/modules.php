184ae634ad1d7f5d5b1e901809387cb9e0c4c7a7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-06-28 22:38:54 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -33,7 +33,7 @@ function ___hero()
     $hero_heading_val = $hero_heading ? $hero_heading : get_the_title();
     if (!$hero_hidden) {
         $hero = "<section class='hero pb-50px background-image text-center rounded-10px bg-primary text-white d-flex align-items-end mx-20px'><div class='container'>";
-        $hero .= do_shortcode("[__heading tag='h1' heading='$hero_heading_val']");
+        $hero .= do_shortcode("[__heading class='large-heading' tag='h1' heading='$hero_heading_val']");
         $hero .= do_shortcode("[__description description='$hero_description']");
         $hero .= "</div></section>";
         return $hero;
