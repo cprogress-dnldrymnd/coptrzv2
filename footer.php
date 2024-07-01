33b024b5511113edacc2bb32f1623b6143047a4d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 23:49:58 +0800

    cs

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 $hide_footer = get__post_meta('hide_footer');
 if (!$hide_footer) {
 ?>
-    <footer id="footer" class="bs-black">
+    <footer id="footer" class="bs-black text-white">
         <div class="container">
             <div class="row g-4">
                 <div class="col-lg col-md-12">
