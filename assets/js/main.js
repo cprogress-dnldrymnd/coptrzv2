2310db09eca40d73931f0f90e7026798b8b4fd7b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 22:51:56 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,6 @@ function swipers() {
         $number_of_slides = jQuery(this).attr('number_of_slides');
         $number_of_slides_tablet = jQuery(this).attr('number_of_slides_tablet');
         $number_of_slides_mobile = jQuery(this).attr('number_of_slides_mobile');
-        console.log($id);
 
         var logoSwiper = new Swiper($id, {
             loop: true,
