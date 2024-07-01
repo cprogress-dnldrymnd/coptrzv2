9bc0aa5b828b9529d8377da1037b68219dbf31f9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 11:00:59 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -42,11 +42,11 @@ Container::make('post_meta', __('Sections'))
                     )
                     ->set_duplicate_groups_allowed(false)
                     ->add_fields('heading', array(
-                        Field::make('html', 'html_1')->set_html('<label>Heading Options</label>')->set_classes('cb-label'),
+                        Field::make('html', 'html_1')->set_html('<label>Section Heading Options</label>')->set_classes('cb-label'),
                         Field::make('checkbox', 'has_prefix', __('Heading Has Prefix'))->set_width(20),
                         Field::make('checkbox', 'has_suffix', __('Heading Has Suffix'))->set_width(20),
                         Field::make('checkbox', 'has_custom_heading_settings', __('Custom Heading Settings'))->set_width(50),
-                        Field::make('html', 'html_2')->set_html('<label>Heading Settings</label>')->set_classes('cb-label'),
+                        Field::make('html', 'html_2')->set_html('<label>Section Heading Settings</label>')->set_classes('cb-label'),
                         Field::make('text', 'heading', __('Heading'))->set_classes('inline-field'),
                         Field::make('text', 'prefix', __('Prefix'))->set_classes('inline-field')
                             ->set_conditional_logic(
@@ -134,12 +134,14 @@ Container::make('post_meta', __('Sections'))
                                 )
                             ),
                     ))
-                    
+
                     ->add_fields('description',  array(
-                        Field::make('html', 'html_4')->set_html('<label>Column Settings</label>')->set_classes('cb-label'),
+                        Field::make('html', 'html_4')->set_html('<label>Section Description Settings</label>')->set_classes('cb-label'),
                         Field::make('textarea', 'description', __('Description'))->set_classes('activate-tinymce'),
                     ))
                     ->add_fields('columns', array(
+                        Field::make('html', 'html_4')->set_html('<label>Section Column Settings</label>')->set_classes('cb-label'),
+
                         Field::make('complex', 'columns', __(''))
                             ->setup_labels(
                                 array(
