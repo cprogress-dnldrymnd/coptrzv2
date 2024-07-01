96d675f74269c81e7212a53aeebba11fab8f5ea0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 23:59:00 +0800

    css

--- footer.php
+++ footer.php
@@ -5,21 +5,33 @@ if (!$hide_footer) {
 ?>
     <footer id="footer" class="bg-black text-white md-padding-top">
         <div class="container">
-            <div class="row g-4">
-                <div class="col-lg col-md-12">
-                    <?php dynamic_sidebar('footer_column_1') ?>
+            <div class="footer-columns">
+                <div class="row g-4">
+                    <div class="col-lg col-md-12">
+                        <?php dynamic_sidebar('footer_column_1') ?>
+                    </div>
+                    <div class="col-lg col-md-6">
+                        <?php dynamic_sidebar('footer_column_2') ?>
+                    </div>
+                    <div class="col-lg col-md-6">
+                        <?php dynamic_sidebar('footer_column_3') ?>
+                    </div>
+                    <div class="col-lg col-md-6">
+                        <?php dynamic_sidebar('footer_column_4') ?>
+                    </div>
+                    <div class="col-lg col-md-6">
+                        <?php dynamic_sidebar('footer_column_5') ?>
+                    </div>
                 </div>
-                <div class="col-lg col-md-6">
-                    <?php dynamic_sidebar('footer_column_2') ?>
-                </div>
-                <div class="col-lg col-md-6">
-                    <?php dynamic_sidebar('footer_column_3') ?>
-                </div>
-                <div class="col-lg col-md-6">
-                    <?php dynamic_sidebar('footer_column_4') ?>
-                </div>
-                <div class="col-lg col-md-6">
-                    <?php dynamic_sidebar('footer_column_5') ?>
+            </div>
+            <div class="footer-bottom">
+                <div class="row g-4">
+                    <div class="col-lg-6">
+                        <?php dynamic_sidebar('footer_bottom_left') ?>
+                    </div>
+                    <div class="col-lg-6">
+                        <?php dynamic_sidebar('footer_bottom_right') ?>
+                    </div>
                 </div>
             </div>
         </div>
