afad94900674f784f464415e62d96934cc6626ae
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 11:38:52 +0800

    Update elements.php

--- includes/elements.php
+++ includes/elements.php
@@ -8,7 +8,7 @@ function __heading($data, $html = '')
     $suffix = isset($data['prefix']) ? $data['suffix'] : false;
     $attributes_args = [];
     if ($class) {
-        $attributes_args[] = array('class' $class);
+        $attributes_args[] = array('class', $class);
     }
     if ($prefix || $suffix) {
         $attributes_args[] = array('class', 'heading-box');
