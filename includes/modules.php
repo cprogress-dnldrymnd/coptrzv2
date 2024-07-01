271a6b95ae610fc7ae9c93bc8eede507fd5f0e6f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 13:10:52 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -121,6 +121,7 @@ function ____columns_modules($columns)
                     $html .= ____heading_modules($item);
                     break;
                 case 'icon':
+                    $html .= 'ico';
                     $html .= ____icon_modules($item);
                     break;
             }
