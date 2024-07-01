9d299b74d6f05d8d476fbe066d59d3a448e41cc7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 21:38:07 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -315,7 +315,7 @@ function ____post_grid($data)
     $html .= "<div class='row g-4'>";
     foreach ($posts_lists as $post) {
         $html .= "<div $column_attribute>";
-        $html .= "<div $post_attribute";
+        $html .= "<div $post_attribute>";
         foreach ($post_elements as $item) {
             $type = $item['_type'];
             switch ($type) {
