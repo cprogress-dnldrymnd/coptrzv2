68a580423684a75cb390d60645d018549261da9e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 13:38:35 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -431,7 +431,7 @@ Container::make('post_meta', __('Sections'))
                                             Field::make('textarea', 'custom_html', __('Custom HTML')),
                                         )
                                     ),
-                                Field::make('complex', 'column_styles', __('Styles'))
+                                Field::make('complex', 'column_styles', __('Column Styles'))
                                     ->set_duplicate_groups_allowed(false)
                                     ->add_fields(
                                         'background_color',
