8c20d17b867c75c426b5d16be09dda173c06e8c9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 13:53:58 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -112,8 +112,11 @@ function ___sections()
 
             $id = _attribute('id', array($section_id_val));
             $classes_attr = _attribute('class', $classes);
+            if ($styles) {
+                $icon_data['styles'] = _attribute('style', $styles, ';');
+            }
 
-            $section_attribute = _attributes(array($classes_attr, $id));
+            $section_attribute = _attributes(array($classes_attr, $id, $styles));
 
             $sections_var .= "<section $section_attribute>";
             $sections_var .= "<div class='container'>";
