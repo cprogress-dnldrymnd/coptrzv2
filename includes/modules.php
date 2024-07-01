d67e253fc21b66e6998657b0f18f2a99887ae1a1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 20:43:21 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -291,32 +291,7 @@ function ____post_grid($data)
     foreach ($posts_lists as $post) {
         $html .= "<div class='col'>";
         $html .= "<div class='column-holder'>";
-        foreach ($post_elements as $items) {
-            foreach ($items as $item) {
-                $type = $item['_type'];
-                switch ($type) {
-                    case 'post_title':
-                        $html .= ____heading_modules($item);
-                        break;
-                    case 'excerpt':
-                        $html .= __description(array(
-                            'description' => $item['description'],
-                            'class' => _attribute('class', array('description-box'))
-                        ));
-                        break;
-                    case 'permalink':
-                        $html .= $html .= __button(array(
-                            'button_type' => get_post_type(),
-                            'button_text' => $item['button_text'],
-                            'button_url' => $post->ID,
-                            'button_url_custom' => $item['button_url_custom'],
-                            'button_style' => $item['button_style'],
-                            'button_target' => $item['button_target'],
-                        ));
-                        break;
-                }
-            }
-        }
+       
         $html .= "</div>";
         $html .= "</div>";
     }
