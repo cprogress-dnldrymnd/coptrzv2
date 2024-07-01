4bc5633099162efd7cbc6c88d355736282c550a7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-02 00:05:25 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -31,7 +31,7 @@ if (!$hide_footer) {
                     <div class="col-lg-6">
                         <?php dynamic_sidebar('footer_bottom_left') ?>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-lg-6 footer-right">
                         <?php dynamic_sidebar('footer_bottom_right') ?>
                     </div>
                 </div>
