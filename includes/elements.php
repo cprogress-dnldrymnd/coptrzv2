405def2f808a7be2ec3745b75081f83f25377184
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 11:35:22 +0800

    Update elements.php

--- includes/elements.php
+++ includes/elements.php
@@ -20,7 +20,7 @@ function __heading($data, $html = '')
 
     if ($heading) {
         if ($prefix || $suffix) {
-            $html .= "<div>";
+            $html .= "<div $_attributes>";
             if ($prefix) {
                 $html .= "<span>$prefix</span>";
             }
