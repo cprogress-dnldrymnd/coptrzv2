41a08f64cfe6e89d26b480e5a73da2c68c924bae
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 12:13:04 +0800

    Update elements.php

--- includes/elements.php
+++ includes/elements.php
@@ -53,19 +53,12 @@ function __description($data)
     }
 }
 
-function __image($atts)
+function __image($data)
 {
-    extract(
-        shortcode_atts(
-            array(
-                'featured_image' => false,
-                'image_id' => '',
-                'size' => '',
-                'class' => '',
-            ),
-            $atts
-        )
-    );
+    $featured_image = isset($data['featured_image']) ? true : false;
+    $image_id = isset($data['image_id']) ? $data['image_id'] : false;
+    $size = isset($data['size']) ? $data['size'] : false;
+    $class = isset($data['size']) ? $data['class'] : false;
 
     if ($featured_image) {
         $image = get_the_post_thumbnail(get_the_ID(), $size);
@@ -73,10 +66,11 @@ function __image($atts)
         $image = wp_get_attachment_image($image_id, $size);
     }
     if ($image) {
-        $_attributes = _attributes(array(
-            array('class', $class),
-            array('class', 'image-box'),
-        ));
+        $attributes_args = [];
+        if ($class) {
+            $attributes_args[] = $class;
+        }
+        $_attributes = _attributes($attributes_args);
 
         return "<div $_attributes>$image</div>";
     }
@@ -88,7 +82,6 @@ function __video($data)
 
     if ($video_url) {
         $class = isset($data['class']) ? $data['class'] : false;
-
         $attributes_args = [];
         if ($class) {
             $attributes_args[] = $class;
