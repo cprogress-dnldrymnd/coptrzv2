0af5d0b1a9c7c5ae0b63f476036a73db524146b9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 15:41:50 +0800

    css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,26 +3,42 @@ jQuery(document).ready(function () {
 });
 
 function swipers() {
-    var logoSwiper = new Swiper(".logo-slider", {
-        loop: true,
-        freeMode: true,
-        centeredSlides: true,
-        speed: 5000,
-        autoplay: {
-            delay: 0,
-            disableOnInteraction: false
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 3,
+    jQuery('.mySwiper-logoSwiper-Module').each(function (index, element) {
+        $id = '#' + jQuery(this).attr('id');
+        $number_of_slides = jQuery(this).attr('number_of_slides');
+        $number_of_slides_tablet = jQuery(this).attr('number_of_slides_tablet');
+        $number_of_slides_mobile = jQuery(this).attr('number_of_slides_mobile');
+        console.log($id);
+
+        var logoSwiper = new Swiper($id, {
+            loop: true,
+            freeMode: true,
+            centeredSlides: true,
+            speed: 5000,
+            autoplay: {
+                delay: 0,
+                disableOnInteraction: false
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: $number_of_slides_mobile,
+                },
+
+                768: {
+                    slidesPerView: $number_of_slides_tablet,
+                },
+
+
+                992: {
+                    slidesPerView: $number_of_slides,
+                },
+
+
+
             },
 
-            992: {
-                slidesPerView: 4,
-            },
+        });
 
-
-        },
 
     });
 }
