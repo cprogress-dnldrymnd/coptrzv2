3ba4c0f5b8dc356029fc501cee601eef4533790f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-01 22:32:13 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -159,7 +159,13 @@ Container::make('post_meta', __('Sections'))
                     ->add_fields('columns', array(
                         Field::make('html', 'html_4')->set_html('<label>Section Columns Options</label>')->set_classes('cb-label'),
                         Field::make('checkbox', 'individual_column_settings', __('Individual Column Settings'))->set_width(20),
-                        Field::make('checkbox', 'is_slider', __('Is Slider'))->set_width(80),
+                        Field::make('checkbox', 'is_slider', __('Is Slider'))->set_width(20),
+                        Field::make('select', 'slider_style', __('Tag'))->set_width(60)
+                            ->set_options(
+                                array(
+                                    'style-1' => 'Style 1',
+                                )
+                            ),
                         Field::make('html', 'html_42')->set_html('<label>Section Columns Settings</label>')->set_classes('cb-label'),
                         Field::make('complex', 'columns', __(''))
                             ->setup_labels(
