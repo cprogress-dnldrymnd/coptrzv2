a1909e134a955697222c468debdede59b4aeb3af
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-02 11:04:37 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,4 +55,16 @@ function swipers() {
         });
 
     });
+
+    var swiper_sliders = new Swiper('.swiper-full-width', {
+        loop: true,
+        autoplay: false,
+        slidesPerView: 'auto',
+        slidesPerView: 1,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    
 }
