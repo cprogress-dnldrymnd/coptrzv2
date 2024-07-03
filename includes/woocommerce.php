9da286867b7e94b546ba1c74517cfcf88582d63a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 02:25:12 +0800

    cs

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -13,12 +13,24 @@ function action_woocommerce_before_shop_loop()
 {
     echo '<section class="product-archive-loop">';
     echo '<div class="container">';
+    echo '<div class="row">';
+    echo '<div class="col-lg-4">';
+    /**
+     * Hook: woocommerce_sidebar.
+     *
+     * @hooked woocommerce_get_sidebar - 10
+     */
+    do_action('woocommerce_sidebar');
+    echo '</div>';
+    echo '<div class="col-lg-8">';
 }
 
 add_action('woocommerce_before_shop_loop', 'action_woocommerce_before_shop_loop');
 
 function action_woocommerce_after_shop_loop()
 {
+    echo '</div>';
+    echo '</div>';
     echo '</div>';
     echo '</section>';
 }
