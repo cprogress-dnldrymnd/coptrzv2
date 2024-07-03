d2798e78161949fa658469dc16a29d3b53dc4cdb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 16:06:33 +0800

    Update menus.php

--- includes/menus.php
+++ includes/menus.php
@@ -26,7 +26,11 @@ function header_menu()
 	);
 	$header_menu = wp_get_nav_menu_items($menuID, $args); // Get the array
 
-	
+	if (current_user_can('administrator')) {
+		echo '<pre>';
+		var_dump($header_menu);
+		echo '</pre>';
+	}
 
 	$html = '<nav class="navbar text-white p-0">';
 	$html .= '<ul class="navbar-nav flex-row me-auto mb-2 mb-lg-0">';
@@ -34,9 +38,14 @@ function header_menu()
 	foreach ($header_menu as $menu) {
 		$title = $menu->title;
 		$url = $menu->url;
-		$html .= '<li class="nav-item">';
-		$html .= "<a class='nav-link text-white' href='$url'>$title</a>";
-		$html .= '</li>';
+		$menu_item_parent = $menu->menu_item_parent;
+		if ($menu_item_parent == 0) {
+			$html_submenu = '';
+			$html .= '<li class="nav-item">';
+			$html .= "<a class='nav-link text-white' href='$url'>$title</a>";
+			$html .= '</li>';
+		} else {
+		}
 	}
 
 	$html .= '</ul>';
