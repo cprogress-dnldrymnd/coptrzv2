9d27942bfde7096cb121c44dfe444d5fd2b767c8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 16:07:09 +0800

    Update menus.php

--- includes/menus.php
+++ includes/menus.php
@@ -44,8 +44,7 @@ function header_menu()
 			$html .= '<li class="nav-item">';
 			$html .= "<a class='nav-link text-white' href='$url'>$title</a>";
 			$html .= '</li>';
-		} else {
-		}
+		} 
 	}
 
 	$html .= '</ul>';
