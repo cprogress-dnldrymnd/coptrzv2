906fb17980a0dd62dd74c0a60123a91de959e2c2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 14:46:35 +0800

    Update header.php

--- header.php
+++ header.php
@@ -72,6 +72,9 @@ $SVG = new SVG;
 								<div class="mini-cart-wrapper">
 									<a href="#" id="mini-cart-button" class="header-icon cart-icon text-white d-flex align-items-center">
 										<?= $SVG->cart(); ?>
+										<div class="cart-number">
+											<?= WC()->cart->get_cart_contents_count(); ?>
+										</div>
 									</a>
 									<div class="mini-cart-holder bg-white rounded-10px mt-10px">
 										<?php woocommerce_mini_cart() ?>
@@ -79,7 +82,7 @@ $SVG = new SVG;
 								</div>
 							</div>
 							<div class="col-auto d-flex align-items-center button-box button-accent button-small">
-								<a href="#"  href="#" >Enquire Now</a>
+								<a href="#" href="#">Enquire Now</a>
 							</div>
 						</div>
 
