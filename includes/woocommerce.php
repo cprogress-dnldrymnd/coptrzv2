ac41248844ab67e57c6835191ac00bc930240e21
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 00:49:11 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -1,4 +1,11 @@
 <?php
+// Remove breadcrumbs from shop & categories
+add_filter('woocommerce_before_main_content', 'remove_breadcrumbs');
+function remove_breadcrumbs()
+{
+    remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
+}
+
 function action_woocommerce_before_main_content()
 {
     echo ___hero();
@@ -7,14 +14,16 @@ function action_woocommerce_before_main_content()
 
 add_action('woocommerce_before_main_content', 'action_woocommerce_before_main_content');
 
-function action_woocommerce_before_shop_loop() {
+function action_woocommerce_before_shop_loop()
+{
     echo '<section class="product-archive-loop">';
     echo '<div class="container">';
 }
 
 add_action('woocommerce_before_shop_loop', 'action_woocommerce_before_shop_loop');
 
-function action_woocommerce_after_shop_loop() {
+function action_woocommerce_after_shop_loop()
+{
     echo '</div>';
     echo '</section>';
 }
