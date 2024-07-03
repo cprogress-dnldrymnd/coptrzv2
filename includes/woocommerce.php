08e3944709b55f7a2c3cc5cbc13fcf4f809e4dcc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 00:44:55 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -6,3 +6,9 @@ function action_woocommerce_before_main_content()
 
 
 add_action('woocommerce_before_main_content', 'action_woocommerce_before_main_content');
+
+function action_woocommerce_before_shop_loop() {
+    echo 'xsdsds';
+}
+
+add_action('woocommerce_before_shop_loop', 'action_woocommerce_before_shop_loop');
