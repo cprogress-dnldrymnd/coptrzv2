24c4505001de939d741e9db97e98a6381b4935c2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 00:02:58 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -1,5 +1,13 @@
 <?php
-function action_woocommerce_before_main_content() {
+add_filter('woocommerce_show_page_title', 'bbloomer_hide_shop_page_title');
+function bbloomer_hide_shop_page_title($title)
+{
+    if (is_shop()) $title = false;
+    return $title;
+}
+
+function action_woocommerce_before_main_content()
+{
     echo ___hero();
 }
 
