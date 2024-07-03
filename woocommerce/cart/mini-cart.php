1d8a55a32b5141a63a8beffa9c90f56313c98fa1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 14:33:07 +0800

    Update mini-cart.php

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -59,15 +59,17 @@ do_action('woocommerce_before_mini_cart'); ?>
 						$cart_item_key
 					);
 					?>
-					<?php if (empty($product_permalink)) : ?>
-						<?php echo $thumbnail . wp_kses_post($product_name); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-						?>
-					<?php else : ?>
-						<a href="<?php echo esc_url($product_permalink); ?>">
-							<?php echo $thumbnail; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+					<div class="product-thumbnail">
+						<?php if (empty($product_permalink)) : ?>
+							<?php echo $thumbnail . wp_kses_post($product_name); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
 							?>
-						</a>
-					<?php endif; ?>
+						<?php else : ?>
+							<a href="<?php echo esc_url($product_permalink); ?>">
+								<?php echo $thumbnail; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+								?>
+							</a>
+						<?php endif; ?>
+					</div>
 					<div class="product-title-quantity">
 						<?
 						echo wp_kses_post($product_name);
