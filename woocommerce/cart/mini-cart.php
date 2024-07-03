9e6a801b4e070fe258b2a75c4acdeadad1cd677b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 14:34:58 +0800

    cs

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -71,8 +71,10 @@ do_action('woocommerce_before_mini_cart'); ?>
 						<?php endif; ?>
 					</div>
 					<div class="product-title-quantity">
+						<div class="product-title">
+							<?php echo wp_kses_post($product_name); ?>
+						</div>
 						<?
-						echo wp_kses_post($product_name);
 						echo wc_get_formatted_cart_item_data($cart_item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
 						echo apply_filters('woocommerce_widget_cart_item_quantity', '<span class="quantity">' . sprintf('%s &times; %s', $cart_item['quantity'], $product_price) . '</span>', $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
 						?>
