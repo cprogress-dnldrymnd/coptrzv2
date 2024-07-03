047ca85fbf1a747c06e07748d6eaa3002b7f6697
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 15:27:50 +0800

    cssss

--- header.php
+++ header.php
@@ -35,41 +35,31 @@ $SVG = new SVG;
 						</a>
 					</div>
 					<div class="col-auto">
-						<div class="offcanvas offcanvas-start" tabindex="-1" id="offCanvasMenu" aria-labelledby="offcanvasLabel">
-							<div class="offcanvas-header">
-								<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-							</div>
-							<div class="offcanvas-body">
-								<nav class="navbar text-white p-0">
-
-									<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-										<li class="nav-item">
-											<a class="nav-link text-white active" aria-current="page" href="#">Products</a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link text-white" href="#">Industry</a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link text-white" href="#">Capabilities</a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link text-white" href="#">Training</a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link text-white" href="#">Repairs</a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link text-white" href="#">Resources</a>
-										</li>
-										<li class="nav-item">
-											<a class="nav-link text-white" href="#">Defence</a>
-										</li>
-									</ul>
-								</nav>
-
-							</div>
-						</div>
-
+						<nav class="navbar text-white p-0">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+								<li class="nav-item">
+									<a class="nav-link text-white active" aria-current="page" href="#">Products</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white" href="#">Industry</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white" href="#">Capabilities</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white" href="#">Training</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white" href="#">Repairs</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white" href="#">Resources</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white" href="#">Defence</a>
+								</li>
+							</ul>
+						</nav>
 					</div>
 					<div class="col-auto d-flex align-items-center">
 						<div class="row g-4 header-right">
@@ -109,5 +99,39 @@ $SVG = new SVG;
 			</div>
 		</div>
 	</header>
+	<div class="offcanvas offcanvas-start" tabindex="-1" id="offCanvasMenu" aria-labelledby="offcanvasLabel">
+		<div class="offcanvas-header">
+			<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+		</div>
+		<div class="offcanvas-body">
+			<nav class="navbar text-white p-0">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link text-white active" aria-current="page" href="#">Products</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Industry</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Capabilities</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Training</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Repairs</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Resources</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-white" href="#">Defence</a>
+					</li>
+				</ul>
+			</nav>
+
+		</div>
+	</div>
+
 	<?php wp_body_open(); ?>
 	<main class="mt-20px">
