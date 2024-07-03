709c0674831451eb358e37e028b73901bf32c503
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 15:25:28 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ define('coptz_version', 4.4);
 define('theme_dir', get_template_directory_uri() . '/');
 define('assets_dir', theme_dir . 'assets/');
 define('image_dir', assets_dir . 'images/');
-define('vendor_dir', assets_dir . 'coptrz_vendors/');
+define('vendor_dir', theme_dir . 'vendor/');
 /*-----------------------------------------------------------------------------------*/
 /* After Theme Setup
 /*-----------------------------------------------------------------------------------*/
@@ -64,6 +64,7 @@ function enqueue_scripts()
 {
 	wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css');
 	wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js');
+	wp_enqueue_script('bootstrap', vendor_dir . '/twbs/boostrap/dist/js/bootstrap.min.js');
 
 	wp_enqueue_style('coptz-style', theme_dir . 'style.css', NULL, coptz_version);
 	wp_enqueue_script('coptz-main', assets_dir . 'js/main.js', NULL, coptz_version);
