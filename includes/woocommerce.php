778bde9ac62fc432890652868773a600066d7922
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 02:53:28 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -68,7 +68,7 @@ function action_woocommerce_before_shop_loop_item()
 
 
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
-add_action('woocommerce_after_shop_loop_item', 'product_loop_button');
+add_action('woocommerce_template_loop_add_to_cart', 'product_loop_button');
 
 function product_loop_button()
 {
