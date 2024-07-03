e5bd7ccfc402f63159f2c2a82885a36776010224
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 13:56:16 +0800

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ $SVG = new SVG;
 		<div class="container">
 			<div class="header-inner mt-20px rounded-10px">
 				<div class="row justify-content-between">
-					<div class="col-auto">
+					<div class="col-auto d-flex">
 						<a class="site-logo" href="#">
 							<svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="256" height="26.026" viewBox="0 0 256 26.026" class="svg-image replaced-svg">
 								<g id="Group_170" data-name="Group 170" transform="translate(-99.825 -87.108)">
@@ -61,7 +61,7 @@ $SVG = new SVG;
 							</ul>
 						</nav>
 					</div>
-					<div class="col-auto">
+					<div class="col-auto d-flex">
 						<div class="row g-4 header-right align-items-center">
 							<div class="col-auto">
 								<a href="<?= get_permalink(get_option('woocommerce_myaccount_page_id')); ?>" class="account-icon text-white">
