442d320d96540c6a9b5cce09284cdbe257e1ad09
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-03 17:57:23 +0800

    csss

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function () {
 
 function header_menu() {
     jQuery('.has-children.nav-link').click(function (e) { 
+        jQuery(this).toggleClass('active');
         jQuery(this).next().toggleClass('active');
         e.preventDefault();
     });
