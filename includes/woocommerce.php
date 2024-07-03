8a0a353e7a8fae342e9201eeba2f63bd3cb2dde4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 02:42:27 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -38,12 +38,13 @@ function action_woocommerce_after_shop_loop()
 add_action('woocommerce_after_shop_loop', 'action_woocommerce_after_shop_loop');
 
 
-remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
-add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
-
 /**
 * WooCommerce Loop Product Thumbs
 **/
+
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+
 if ( ! function_exists( 'woocommerce_template_loop_product_thumbnail' ) ) {
     function woocommerce_template_loop_product_thumbnail() {
         echo "<div class='wc-img-wrapper'>";
