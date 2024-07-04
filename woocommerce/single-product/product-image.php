8073a41672032a54976e06839c0b403d45e9634e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 00:02:16 +0800

    css

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -32,51 +32,53 @@ $wrapper_classes   = apply_filters(
 	'woocommerce_single_product_image_gallery_classes',
 	array(
 		'woocommerce-product-gallery',
-		'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
-		'woocommerce-product-gallery--columns-' . absint( $columns ),
+		'woocommerce-product-gallery--' . ($post_thumbnail_id ? 'with-images' : 'without-images'),
+		'woocommerce-product-gallery--columns-' . absint($columns),
 		'images',
 	)
 );
 ?>
 <div class="images">
 	<div class="woocommerce-product-gallery__wrapper">
-		<div class="swiper product-main-image border-default rounded-corner overflow-hidden">
-			<div class="swiper-wrapper">
-				<div class="swiper-slide">
-					<?php
+		<div class="product-main-image-holder">
+			<div class="swiper product-main-image border-default rounded-corner overflow-hidden">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						<?php
 
-					if ($post_thumbnail_id) {
-						echo __image(array(
-							'image_id' => $post_thumbnail_id,
-							'class' => _attribute('class', array('product-image')),
-							'size' => 'large'
-						));
-					} else {
-						$html  = '<div class="product-image woocommerce-product-gallery__image--placeholder">';
-						$html .= sprintf('<img src="%s" alt="%s" class="wp-post-image" />', esc_url(wc_placeholder_img_src('woocommerce_single')), esc_html__('Awaiting product image', 'woocommerce'));
-						$html .= '</div>';
-						echo $html;
-					}
-
-					?>
-				</div>
-				<?php
-				if ($attachment_ids && $product->get_image_id()) {
-					foreach ($attachment_ids as $attachment_id) {
-				?>
-						<div class="swiper-slide">
-							<?php
+						if ($post_thumbnail_id) {
 							echo __image(array(
-								'image_id' => $attachment_id,
-								'class' => 'product-image',
+								'image_id' => $post_thumbnail_id,
+								'class' => _attribute('class', array('product-image')),
 								'size' => 'large'
 							));
-							?>
-						</div>
-				<?php
+						} else {
+							$html  = '<div class="product-image woocommerce-product-gallery__image--placeholder">';
+							$html .= sprintf('<img src="%s" alt="%s" class="wp-post-image" />', esc_url(wc_placeholder_img_src('woocommerce_single')), esc_html__('Awaiting product image', 'woocommerce'));
+							$html .= '</div>';
+							echo $html;
+						}
+
+						?>
+					</div>
+					<?php
+					if ($attachment_ids && $product->get_image_id()) {
+						foreach ($attachment_ids as $attachment_id) {
+					?>
+							<div class="swiper-slide">
+								<?php
+								echo __image(array(
+									'image_id' => $attachment_id,
+									'class' => 'product-image',
+									'size' => 'large'
+								));
+								?>
+							</div>
+					<?php
+						}
 					}
-				}
-				?>
+					?>
+				</div>
 			</div>
 		</div>
 		<?php
