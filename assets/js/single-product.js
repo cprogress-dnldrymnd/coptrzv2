e1791313f70c7e5bf676384f2acd30304e9816b6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 23:44:41 +0800

    Update single-product.js

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -3,6 +3,17 @@ jQuery(document).ready(function () {
 });
 
 function product_gallery() {
+    var product_main_image = new Swiper('.product-main-image', {
+        loop: true,
+        autoplay: false,
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
     var product_thumb = new Swiper('.product-thumb', {
         loop: true,
         autoplay: false,
