3a732a4f9e9df324365e640f0baea7d7694fae06
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 23:51:04 +0800

    Update product-image.php

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -28,8 +28,17 @@ global $product;
 $columns           = apply_filters('woocommerce_product_thumbnails_columns', 4);
 $post_thumbnail_id = $product->get_image_id();
 $attachment_ids = $product->get_gallery_image_ids();
+$wrapper_classes   = apply_filters(
+	'woocommerce_single_product_image_gallery_classes',
+	array(
+		'woocommerce-product-gallery',
+		'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
+		'woocommerce-product-gallery--columns-' . absint( $columns ),
+		'images',
+	)
+);
 ?>
-<div class="product-main">
+<div class="<?php echo esc_attr(implode(' ', array_map('sanitize_html_class', $wrapper_classes))); ?>" data-columns="<?php echo esc_attr($columns); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
 	<div class="woocommerce-product-gallery__wrapper">
 		<div class="swiper product-main-image">
 			<div class="swiper-wrapper">
