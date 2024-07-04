f3f4a53e50ad7ad1b7008bb8bc2574c1afc6a59f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 23:51:49 +0800

    Update product-image.php

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -38,7 +38,7 @@ $wrapper_classes   = apply_filters(
 	)
 );
 ?>
-<div class="<?php echo esc_attr(implode(' ', array_map('sanitize_html_class', $wrapper_classes))); ?>" data-columns="<?php echo esc_attr($columns); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
+<div class="images">
 	<div class="woocommerce-product-gallery__wrapper">
 		<div class="swiper product-main-image">
 			<div class="swiper-wrapper">
