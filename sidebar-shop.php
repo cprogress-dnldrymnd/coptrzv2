efe99b76653eed372cc243f922447eedf411986d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 20:27:31 +0800

    cs

--- sidebar-shop.php
+++ sidebar-shop.php
@@ -1,3 +1,5 @@
-<div class="shop-sidebar">
-    <?php dynamic_sidebar('woocommerce_sidebar') ?>
-</div>
+<?php if (is_product_taxonomy()) { ?>
+    <div class="shop-sidebar">
+        <?php dynamic_sidebar('woocommerce_sidebar') ?>
+    </div>
+<?php } ?>
