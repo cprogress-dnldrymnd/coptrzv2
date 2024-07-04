f110449cb21334bdf8e1455678b40eb0316221f6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 17:19:10 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -386,6 +386,24 @@ Container::make('post_meta', __('Sections'))
                                     ->set_header_template('<%- heading %>')
                                     ->add_fields('description',  array(
                                         Field::make('textarea', 'description', __('Description'))->set_classes('activate-tinymce'),
+                                        Field::make('text', 'description_width', __('Description Custom Width'))->set_classes('activate-tinymce'),
+                                        Field::make('select', 'size', __('Description Size'))->set_width(20)
+                                            ->set_options(
+                                                array(
+                                                    '' => 'Default',
+                                                    'small-text' => 'Small Text',
+                                                    'medium-text' => 'Medium Text',
+                                                    'large-text' => 'Large Text',
+                                                )
+                                            )
+                                            ->set_conditional_logic(
+                                                array(
+                                                    array(
+                                                        'field' => 'has_custom_heading_settings',
+                                                        'value' => true,
+                                                    )
+                                                )
+                                            ),
                                     ))
 
                                     ->add_fields('image', array(
