4ab7368c94bad566d6ff37f286726207f762f48d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 19:56:32 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -303,8 +303,8 @@ Container::make('post_meta', __('Sections'))
                         Field::make('complex', 'columns', __(''))
                             ->setup_labels(
                                 array(
-                                    'plural_name'   => 'Row',
-                                    'singular_name' => 'row',
+                                    'plural_name'   => 'Columns',
+                                    'singular_name' => 'Columnn',
                                 )
                             )
                             ->set_classes('columns')
