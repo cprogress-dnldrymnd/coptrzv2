b38dd1cbb8f76c1ff0fa7b220a933316883a4d6b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 21:58:09 +0800

    Update page-modules.php

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -9,7 +9,7 @@
 $modules = get__post_meta('modules');
 ?>
 
-
+<iframe src="https://www.youtube.com/embed/b74ehKejCLs?si=f55i9YxclcAiOMI7?autoplay=1&amp;controls=0&amp;showinfo=0&amp;mute=1&amp;loop=1"></iframe>
 <div class="modules">
     <?php
     the_content();
