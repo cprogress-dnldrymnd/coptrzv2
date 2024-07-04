024820ced579962805904461467532e6e012f15f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 19:16:21 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -707,6 +707,7 @@ function ____columns_modules($items, $id, $html = '')
     }
 
 
+
     if ($is_slider) {
         $html .= "<div class='swiper-wrapper'>"; //swiper-wrapper
 
@@ -818,18 +819,17 @@ function ____columns_modules($items, $id, $html = '')
 
         $column_class_val = _attribute('class', $column_class, ' ');
         $column_class_attr = _attributes(array($column_class_val));
-
         if ($styles) {
             $styles_val = _attribute('style', $styles, ';');
         }
-    
+
         if ($classes) {
             $classes_val = _attribute('class', $classes, ' ');
-    
-    
-    
+        }
+
+
+
         $column_attributes = _attributes(array($classes_val, $styles_val));
-        
         if ($is_slider) {
             $html .= '<div class="swiper-slide">'; //swiper-slide
         } else {
