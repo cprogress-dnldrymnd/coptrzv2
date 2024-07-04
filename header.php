86e600422a8d31f7298fa8184aeee8faf0b00da2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 22:12:48 +0800

    Update header.php

--- header.php
+++ header.php
@@ -2,8 +2,18 @@
 <html <?php language_attributes(); ?> class="html">
 
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="author" content="">
+	<meta name="format-detection" content="telephone=no">
+	<title>
+		<?php bloginfo('name'); // show the blog name, from settings 
+		?> |
+		<?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page 
+		?>
+	</title>
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 	<?php wp_head(); ?>
 </head>
@@ -75,7 +85,7 @@ $SVG = new SVG;
 			</div>
 		</div>
 	</header>
-	
-	
+
+
 	<?php wp_body_open(); ?>
 	<main class="mt-20px">
