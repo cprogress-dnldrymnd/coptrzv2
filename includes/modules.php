bfaa33ff8261c2d5d550db785274ddea7a9fac29
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 18:33:48 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -137,8 +137,6 @@ function ___sections()
 {
     $sections = get__post_meta('sections');
     $html = '';
-    $classes = array();
-    $styles = array();
     foreach ($sections as $key => $section) {
         $disable_section = $section['disable_section'];
         if (!$disable_section) {
@@ -146,7 +144,8 @@ function ___sections()
             $section_items = $section['section_items'];
             $section_styles = $section['section_styles'];
             $section_id_val  = $section_id ? $section_id : 'section-' . $key;
-       
+            $classes = array();
+            $styles = array();
             $container_styles = array();
             $classes[] = 'section';
             $classes[] = 'section-' . $key;
