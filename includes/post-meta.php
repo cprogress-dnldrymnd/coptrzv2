49f354e95eaf2979068ea464bcb6aebbde5b11d5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 17:10:16 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -226,6 +226,7 @@ Container::make('post_meta', __('Sections'))
                                 )
                             ),
                         Field::make('text', 'image_padding', __('Image Padding'))->set_width(25)
+                            ->set_help_text('Default is 40%')
                             ->set_conditional_logic(
                                 array(
                                     array(
