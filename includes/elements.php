b89b2b66dca68841d1e2c4ac3f0bdcb312a46f1e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 22:22:15 +0800

    Update elements.php

--- includes/elements.php
+++ includes/elements.php
@@ -150,7 +150,7 @@ function _background($hero_background, $is_youtube = false)
             ));
         }
     } else {
-        return "<div class='background-image background-overlay'><iframe src='$hero_background?autoplay=1&mute=1&controls=0&loop=1'></iframe></div>";
+        return "<div class='background-image background-overlay'><iframe src='$hero_background?controls=0&rel=0&playsinline=1&enablejsapi=1&autoplay=1&mute=1&controls=0&loop=1'></iframe></div>";
     }
 }
 
