f5a1dfb9ecfc1991746353e5e3b9d0708456e413
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-04 21:22:24 +0800

    css

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ function action_after_setup_theme()
 
 	require_once('vendor/autoload.php');
 	\Carbon_Fields\Carbon_Fields::boot();
-	
+
 
 	global $popups_id, $styles;
 
@@ -68,8 +68,12 @@ function enqueue_scripts()
 	wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js');
 	wp_enqueue_script('bootstrap', vendor_dir . '/twbs/bootstrap/dist/js/bootstrap.min.js');
 
-	wp_enqueue_style('coptz-style', theme_dir . 'style.css', NULL, coptz_version);
-	wp_enqueue_script('coptz-main', assets_dir . 'js/main.js', NULL, coptz_version);
+	wp_enqueue_style('style', theme_dir . 'style.css', NULL, coptz_version);
+	wp_enqueue_script('main', assets_dir . 'js/main.js', NULL, coptz_version);
+
+	if (is_product()) {
+		wp_enqueue_script('single-product', assets_dir . 'js/main.js', NULL, coptz_version);
+	}
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_scripts', 99999); // Register this fxn and allow Wordpress to call it automatcally in the header
