ee841087015f88148162eafb3cefd6eb28530633
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 15:29:26 +0800

    Update _required_files.php

--- includes/_required_files.php
+++ includes/_required_files.php
@@ -1,2 +1,9 @@
-<?php
+<?php 
+require_once('post-types.php');
+require_once('elements.php');
+require_once('shortcodes.php');
+require_once('modules.php');
+require_once('hooks.php');
+require_once('theme-widgets.php');
 require_once('svg.php');
+require_once('menus.php');
