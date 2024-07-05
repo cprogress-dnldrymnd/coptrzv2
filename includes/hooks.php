af7d9255918b1e46f663620135b67baee1ffcb40
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 11:16:39 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -1,4 +1,10 @@
 <?php
+function add_svg_support($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+  add_filter('upload_mimes', 'add_svg_support');
+  
 function action_wp_head()
 {
 ?>
