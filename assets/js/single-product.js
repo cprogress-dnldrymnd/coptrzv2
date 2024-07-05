d88e8f42a47000cbad35509a1acba567bb11ad01
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 14:46:10 +0800

    Update single-product.js

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -10,6 +10,8 @@ function product_variation() {
         $variations = JSON.parse($data_variations);
 
         jQuery.each($variations, function (variation_name, variation_val) {
+            jQuery('#' + variation_name).val($variation_val);
+            jQuery('#' + variation_name).trigger('change');
             console.log(variation_name, variation_val);
         });
         e.preventDefault();
