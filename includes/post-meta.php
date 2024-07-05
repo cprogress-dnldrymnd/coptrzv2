1667faada9b11c5226b2af3d1e19b7a49cc7c743
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 22:30:34 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2680,7 +2680,7 @@ Container::make('post_meta', __('Product Settings'))
     ->where('post_type', '=', 'product')
     ->add_fields(array(
         Field::make('hidden', 'single_product_content', __('')),
-        Field::make('hidden', 'single_product_content_after', __(''))
+        Field::make('textarea', 'single_product_content_after', __(''))
     ));
 
 
