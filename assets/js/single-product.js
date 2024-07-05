c7952403a0ae77ab5cbde161fdb4f2797dc05d54
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 12:51:13 +0800

    Update single-product.js

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -13,9 +13,7 @@ function product_variation() {
                     const variationName = radio.getAttribute('data-variation-name');
                     const selectBox = document.querySelector('select[name=' + variationName + ']');
                     selectBox.value = radio.getAttribute('data-value');
-                    jQuery('.single_add_to_cart_button').removeAttr('disabled ');
-                    jQuery('input[name="variation_id"]').val(selectBox.value);
-                    $(selectBox).trigger('change');
+                    jQuery(selectBox).trigger('change');
                 });
             });
         });
