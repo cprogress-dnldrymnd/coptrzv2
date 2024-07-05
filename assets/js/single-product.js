65e06b29071f2fe4f56e904b88b4464a68103d34
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 14:42:28 +0800

    Update single-product.js

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -7,7 +7,7 @@ function product_variation() {
 
     jQuery('input[name="variation-radio"]').change(function (e) { 
         $data_variations = jQuery(this).attr('data_variations');
-        console.log(parseJSON($data_variations));
+        console.log(JSON.parse($data_variations));
         e.preventDefault();
     });
 
