384dac8fb74db53c2e4470341bb658bbc0924e9b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 13:58:37 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -183,8 +183,8 @@ function custom_product_variation()
         $price = $variation->get_price_html();
         $html .= '<div class="col-12">';
         $html .= "<input stock='$stock_status_variation' type='radio' id='variation-$child' $variations value='$child'  name='variation-radio'>";
-        $html .= "<label for='variation-$child' class='variation-label w-100 status-$stock_status_variation'>";
-        $html .= '<div class="inner d-flex w-100 p-20px rounded-corner">';
+        $html .= "<label for='variation-$child' class='variation-label w-100'>";
+        $html .= "<div class='inner d-flex w-100 p-20px rounded-corner status-$stock_status_variation'>";
         $html .= "<div class='col-auto'>";
         $html .= __image(array(
             'image_id' => $thumbnail,
