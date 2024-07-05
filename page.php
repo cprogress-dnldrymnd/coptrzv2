69332145a7fb143733f5b8eeef46618af4c3c62d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-05 15:18:03 +0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+/*-----------------------------------------------------------------------------------*/
+/* Template Name: Modules 
+/* Template Post Type: page, coptrztemplates
+/*-----------------------------------------------------------------------------------*/
+?>
+<?php get_header(); ?>
+
+
+<div class="modules">
+    <?php
+    the_content();
+    ?>
+</div>
+
+<?php get_footer(); ?>
