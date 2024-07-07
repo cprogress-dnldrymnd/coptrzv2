908827ce39e4e37d0f4540e41e35ee1e75687671
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 06:40:05 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1249,7 +1249,7 @@ function __post_box($id)
 
     $image_args['featured_image'] = $id;
     $image_args['size'] = 'large';
-    $image_args['class'] = _attribute('class', array('image-box same-image-height'));
+    $image_args['class'] = _attribute('class', array('image-box', 'same-image-height'));
 
     $html = "<div class='post-box-inner content-margin'>";
     $html .= __image($image_args);
