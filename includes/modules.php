773b0b4392cf247cda53eeefa76044f64dd0bb48
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 06:35:44 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1263,7 +1263,7 @@ function __post_box($id)
     $html .= __button(array(
         'button_type' => get_post_type($id),
         'button_text' => 'Read Guide',
-        'button_url' => get_the_permalink($id),
+        'button_url' => $id,
         'button_url_custom' => false,
         'button_style' => 'button-bordered',
     ));
