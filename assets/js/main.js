8ed6885e80bc7f401694e5c9d2c4cb1add559149
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 06:56:21 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@ jQuery(document).ready(function () {
 
 function __fixed_heading_position() {
     jQuery('.section.extend-right, .section.extend-left').each(function (index, element) {
-        $this = jQuery(this);
-        jQuery(this).find('> .container > h2').addClass('px-20px').preprendTo($this);
+        $id = '#' + jQuery(this).attr('id');
+        jQuery(this).find('> .container > h2').addClass('px-20px').preprendTo($id);
     });
 }
 
