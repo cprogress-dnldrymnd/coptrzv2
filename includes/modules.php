bd373b9d3012e238e390f7526b2d8d2f0ad9ec80
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 23:03:25 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -35,6 +35,7 @@ function action_module_content()
             update_post_meta(get_the_ID(), '_single_product_content', $single_product_content);
             update_post_meta(get_the_ID(), '_single_product_content_after', $single_product_content_after);
         }
+        echo '<script> console.log("test") </script>';
     }
 }
 add_action('shutdown', 'action_module_content');
