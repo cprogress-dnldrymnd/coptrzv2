3b83399269bc4c6327d3077474698a0612ccc3ea
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 16:34:19 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -32,7 +32,7 @@ class Shortcodes
         }
         $html .= "</div>";
 
-        $html .= "<div class='row g-3 same-image-height'>";
+        $html .= "<div class='row g-3 same-image-height' style='--object-fit-contain'>";
         foreach ($terms as $term) {
             $logo = get___term_meta($term->term_id, 'image');
             $image_args['image_id'] = $logo;
