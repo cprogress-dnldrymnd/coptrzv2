9c6ac2c979981c8ee1e60f5fc7b8171140742fe2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 16:24:49 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -2,103 +2,35 @@
 
 class Shortcodes
 {
-    function __heading($atts)
+    function taxonomy_terms($atts)
     {
         extract(
             shortcode_atts(
                 array(
                     'heading' => '',
-                    'tag' => 'h2',
-                    'class' => '',
+                    'taxonomy' => '',
+                    'search_filter' => false,
+                    'items_per_page' => 16,
                 ),
                 $atts
             )
         );
-        $_attributes = _attributes(array(
-            array('class', $class)
-        ));
+
+        $html = '';
+        $html .= "<div class='taxonomy-terms'>";
+        $html .= "<div class='row g-4 justify-content-between'>";
+
         if ($heading) {
-            return "<$tag $_attributes>$heading</$tag>";
+            $html .= "<div class='col'>";
+            $html .= "<h2>$heading</h2>";
+            $html .= "</div>";
         }
-    }
 
-    function __description($atts)
-    {
-        extract(
-            shortcode_atts(
-                array(
-                    'description' => '',
-                    'class' => '',
-                ),
-                $atts
-            )
-        );
-        $_attributes = _attributes(array(
-            array('class', $class),
-            array('class', 'description-box'),
-        ));
+        $html .= "</div>";
+        $html .= "</div>";
 
-        $description_val = do_shortcode(wpautop(html_entity_decode($description)));
-        if ($description) {
-            return "<div $_attributes>$description_val</div>";
-        }
-    }
-
-    function __image($atts)
-    {
-        extract(
-            shortcode_atts(
-                array(
-                    'featured_image' => false,
-                    'image_id' => '',
-                    'size' => '',
-                    'class' => '',
-                ),
-                $atts
-            )
-        );
-
-        if ($featured_image) {
-            $image = get_the_post_thumbnail(get_the_ID(), $size);
-        } else {
-            $image = wp_get_attachment_image($image_id, $size);
-        }
-        if ($image) {
-            $_attributes = _attributes(array(
-                array('class', $class),
-                array('class', 'image-box'),
-            ));
-
-            return "<div $_attributes>$image</div>";
-        }
-    }
-
-    function __video($atts)
-    {
-        extract(
-            shortcode_atts(
-                array(
-                    'video_id' => '',
-                    'class' => '',
-                ),
-                $atts
-            )
-        );
-
-        $video_url = wp_get_attachment_url($video_id);
-
-        if ($video_url) {
-            $_attributes = _attributes(array(
-                array('class', $class),
-                array('class', 'video-box'),
-            ));
-
-            return "<div $_attributes><video autoplay loop muted src='$video_url'></video></div>";
-        }
+        return $html;
     }
 }
 $Shortcodes = new Shortcodes;
-add_shortcode('__heading', array($Shortcodes, '__heading'));
-add_shortcode('__description', array($Shortcodes, '__description'));
-add_shortcode('__image', array($Shortcodes, '__image'));
-add_shortcode('__video', array($Shortcodes, '__video'));
+add_shortcode('taxonomy_terms', array($Shortcodes, 'taxonomy_terms'));
