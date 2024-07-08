16eb548da8791cad1b9a9adcf0815d5cd9104d5c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 19:10:51 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -460,7 +460,7 @@ function ___sections($id = 'sections')
                                 )
                             );
                             $products = get_posts($args);
-                            $html .= __linked_products($products, $button_text, $button_url, 'swiper-' . $section_id_val, $heading, false, false);
+                            $html .= __linked_products($products, $button_text, $button_url, 'swiper-' . $section_id_val, $heading, true, false);
                         }
 
 
