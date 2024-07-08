8d36600ac3be79fc2fe58f4d21a3613356291deb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 18:40:23 +0800

    css

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -3,45 +3,9 @@ jQuery(document).ready(function () {
     __product_variation();
     __move_paypal();
     __quantity();
-    __swiper_sliders();
 });
 
-function __swiper_sliders() {
-    jQuery('.swiper-linked-products').each(function (index, element) {
-        $id = jQuery(this).attr('id');
 
-        var swiper_linked_products = new Swiper('#' + $id, {
-            loop: true,
-            spaceBetween: 20,
-            autoplay: false,
-            breakpoints: {
-                0: {
-                    slidesPerView: 2,
-                },
-
-                768: {
-                    slidesPerView: 3,
-                },
-
-
-                992: {
-                    slidesPerView: 4,
-                },
-
-
-                1200: {
-                    slidesPerView: 4.5
-                },
-            },
-            navigation: {
-                nextEl: "#swiper-next-" + $id,
-                prevEl: "#swiper-prev-" + $id
-            },
-        });
-
-    });
-
-}
 function __quantity() {
     jQuery('form.cart').on('click', 'button.plus, button.minus', function () {
         var qty = jQuery(this).closest('form.cart').find('.qty');
