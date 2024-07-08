cdb5f9ab8cfb421050601224b2c689c5580662cf
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 18:06:12 +0800

    Create single-productcategorypages.php

---
+++ single-productcategorypages.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+
+<section class="default-page">
+    <div class="container">
+        <?php
+        the_content();
+        ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
