912a2e577fdcb7c1b669c763bbedac7644ead7b5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 19:13:08 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,41 +170,39 @@ function __swipers() {
             prevEl: ".swiper-button-prev",
         },
     });
-    setTimeout(function () {
-        jQuery('.swiper-linked-products').each(function (index, element) {
-            $id = jQuery(this).attr('id');
-            var swiper_linked_products = new Swiper('#' + $id, {
-                loop: true,
-                spaceBetween: 20,
-                autoplay: false,
-                breakpoints: {
-                    0: {
-                        slidesPerView: 2,
-                    },
-    
-                    768: {
-                        slidesPerView: 3,
-                    },
-    
-    
-                    992: {
-                        slidesPerView: 4,
-                    },
-    
-    
-                    1200: {
-                        slidesPerView: 4.5
-                    },
-                },
-                navigation: {
-                    nextEl: "#swiper-next-" + $id,
-                    prevEl: "#swiper-prev-" + $id
-                },
-            });
-    
-        });
-    }, 1000);
 
-  
+
+    jQuery('.swiper-linked-products').each(function (index, element) {
+        $id = jQuery(this).attr('id');
+        var swiper_linked_products = new Swiper('#' + $id, {
+            loop: true,
+            spaceBetween: 20,
+            autoplay: false,
+            breakpoints: {
+                0: {
+                    slidesPerView: 2,
+                },
+
+                768: {
+                    slidesPerView: 3,
+                },
+
+
+                992: {
+                    slidesPerView: 4,
+                },
+
+
+                1200: {
+                    slidesPerView: 4.5
+                },
+            },
+            navigation: {
+                nextEl: "#swiper-next-" + $id,
+                prevEl: "#swiper-prev-" + $id
+            },
+        });
+
+    });
 
 }
