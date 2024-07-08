56b17aa89fce51053c4213ba283362d69355aafc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 19:51:45 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -5,7 +5,7 @@ function action_module_content()
     if (did_action('post_updated')) {
         // Check if this is an autosave
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-        if (_is_module() || get_post_type() == 'layouts') {
+        if (_is_module() || get_post_type() == 'layouts' || get_post_type() == 'productcategorypages') {
             $post_content = '<!-- wp:html -->';
 
             if (get_post_type() != 'layouts') {
