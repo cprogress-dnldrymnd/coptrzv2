a014147a9dc483e0f25cee7507ce918654018ae0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 23:01:03 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -88,6 +88,7 @@ class Shortcodes
                 if ($product_cat) {
                     foreach ($product_cat as $cat) {
                         $cat_name = $cat->name;
+                        $parent = $cat->parent;
                         $cat_link = get_term_link($cat->term_id);
                         $html .= "<li><a class='item text-white' href='$cat_link'>$cat_name</a></li>";
                     }
