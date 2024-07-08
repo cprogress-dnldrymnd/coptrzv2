c092927ca3a7c389ddaed4b5a0d7f55d64a3eb9a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 19:49:59 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2683,6 +2683,7 @@ Container::make('post_meta', __('Sections'))
     ->where('post_template', '=', 'templates/page-modules.php')
     ->or_where('post_type', '=', 'product')
     ->or_where('post_type', '=', 'layouts')
+    ->or_where('post_type', '=', 'productcategorypages')
     ->add_fields(__section_fields());
 
 
