36e8ae75fa8b59cc3baca8b98cfcb81170937acb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-08 20:57:35 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -4,10 +4,7 @@ remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0
 function action_woocommerce_before_main_content()
 {
     if (is_product_taxonomy()) {
-        $product_category_page = __get_product_category_page(get_queried_object()->term_id);
-        if (!$product_category_page) {
-            echo do_shortcode(___hero_product_taxonomy());
-        }
+        echo do_shortcode(___hero_product_taxonomy());
     } else if (is_product()) {
         $single_product_content = get__post_meta('single_product_content');
         echo do_shortcode($single_product_content);
