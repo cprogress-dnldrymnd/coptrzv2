0b7e53c2f7009819b1179890279e863102085f05
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-09 16:22:24 +0800

    cs
    cs

---
+++ header-landingpages.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="html">
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="author" content="">
+	<meta name="format-detection" content="telephone=no">
+	<title>
+		<?php bloginfo('name'); // show the blog name, from settings 
+		?> |
+		<?php is_front_page() ? bloginfo('description') : wp_title(''); // if we're on the home page, show the description, from the site's settings - otherwise, show the title of the post or page 
+		?>
+	</title>
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<?php wp_head(); ?>
+</head>
+
+
+<body <?php body_class(); ?>>
+	<header class="header small-text">
+		<div class="container">
+			<div class="header-inner mt-20px rounded-10px">
+				<div class="row justify-content-between">
+					<?php get_template_part('template-parts/header/header-left') ?>
+					<?php get_template_part('template-parts/header/header-menu') ?>
+					<?php get_template_part('template-parts/header/header-right-landing') ?>
+				</div>
+			</div>
+		</div>
+	</header>
+	<?php wp_body_open(); ?>
+	<main class="mt-20px">
