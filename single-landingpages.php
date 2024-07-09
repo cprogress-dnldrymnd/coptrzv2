6dd96b82e5db300943530e1041c5d01dac2a4234
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-09 16:38:11 +0800

    Update single-landingpages.php

--- single-landingpages.php
+++ single-landingpages.php
@@ -1,7 +1,7 @@
 <?php get_header('landingpages'); ?>
 <section class="landing-page bg-black xl-padding-top md-padding-bottom rounded-corner mx-20px">
     <div class="container">
-        <div class="row">
+        <div class="row g-4 align-items-center">
             <div class="col-lg-6 text-white">
                 <h2> <?php the_title() ?></h2>
                 <div class="description-box">
