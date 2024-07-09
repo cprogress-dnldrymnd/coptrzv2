b00dee01fea082259eee6bd6a0ba03195df90eca
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-09 16:30:19 +0800

    cs

---
+++ single-landingpages.php
@@ -0,0 +1,3 @@
+<?php get_header('landingpages'); ?>
+<?php get_template_part('template-parts/single/single', get_post_type()) ?>
+<?php get_footer('landingpages'); ?>
--- template-parts/single/single-landingpages.php
+++
@@ -1,5 +0,0 @@
-<section class="single-wrapper md-padding-top md-padding-bottom">
-    <div class="container">
-        <?php the_content() ?>
-    </div>
-</section>
