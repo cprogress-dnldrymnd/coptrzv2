5d113800976c500460bd7cbd4a7c438cd537f4c4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-09 22:03:18 +0800

    Update page-landing.php

--- templates/page-landing.php
+++ templates/page-landing.php
@@ -23,7 +23,8 @@ $form = get__post_meta('form');
     ?>
     <div class="container">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-6 text-white">
+                <?php the_title() ?>
                 <?php the_content() ?>
             </div>
             <div class="col-lg-6">
