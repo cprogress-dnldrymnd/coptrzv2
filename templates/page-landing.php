a6f0180758e4eb7e73eb64bed302c851681e053a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-09 22:31:56 +0800

    csss

--- templates/page-landing.php
+++ templates/page-landing.php
@@ -60,7 +60,7 @@ $description_args['class'] =  _attribute('class', array('description-box'));
                             </div>
                         </div>
                     </div>
-                    <div class="form-box p-20px">
+                    <div class="form-box p-20px small-text fw-light">
                         <?= do_shortcode($form) ?>
                     </div>
                 </div>
