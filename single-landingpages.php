797baa98be6e0b5234b523a051bc1ecad1df5bdf
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-09 16:32:01 +0800

    Update single-landingpages.php

--- single-landingpages.php
+++ single-landingpages.php
@@ -1,3 +1,14 @@
 <?php get_header('landingpages'); ?>
-<?php get_template_part('template-parts/single/single', get_post_type()) ?>
+<section class="landing-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php the_content() ?>
+            </div>
+            <div class="col-lg-6">
+                
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer('landingpages'); ?>
