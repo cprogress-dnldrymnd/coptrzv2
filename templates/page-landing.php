9df52be603756df9f5ebd4aa2c2abf453bcfc531
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-09 22:02:28 +0800

    Update page-landing.php

--- templates/page-landing.php
+++ templates/page-landing.php
@@ -7,11 +7,20 @@
 <?php get_header('landing'); ?>
 
 <?php
+$background_type = get__post_meta('background_type');
 $background = get__post_meta('background');
+$background_youtube = get__post_meta('background_youtube');
 $form = get__post_meta('form');
 ?>
 
 <section class="landing-page xl-padding-top md-padding-bottom rounded-corner mx-20px">
+    <?php
+    if ($background_youtube && $background_type == 'youtube') {
+        echo _background($background_youtube, true);
+    } else if ($background) {
+        echo  _background($background);
+    }
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
