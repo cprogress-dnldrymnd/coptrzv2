f570d4107e277afeabaa443970b7e901c63102f0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 17:28:58 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1450,7 +1450,7 @@ function __post_box_blog($id, $col = false, $featured = false)
     } else {
         $html .= __image(array(
             'image_id' => $image,
-            'class' => _attribute('class', array('image-box'))
+            'class' => _attribute('class', array('image-box rounded-corner overflow-hidden'))
         ));
         $html .= __post_category($id, 'category');
     }
