4c14baec372e4f28c0bfaa7129f31d96a7d5ecff
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 04:43:53 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1748,7 +1748,7 @@ function __layouts($args, $return = '')
 
     if ($layouts) {
         foreach ($layouts as $layout) {
-            $return .=  "[layout id='$layout->ID']";
+            $return .=  "[layouts id='$layout->ID']";
         }
         return $return;
     }
