39fcaaf51b5e57831a2eb099434122520078fa07
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 23:08:06 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1598,7 +1598,8 @@ function ___posts_header($key, $title, $taxonomy, $class='')
     ));
 
     $html = "<div class='post-archive-header'>";
-    $html .= "<div class='container $class'>";
+    $html .= "<div class='container'>";
+    $html .= "<div class='inner $class'>";
     $html .= "<div class='row g-3 justify-content-between'>";
     if ($title) {
         $html .= "<div class='col-auto'>";
@@ -1674,6 +1675,7 @@ function ___posts_header($key, $title, $taxonomy, $class='')
     $html .= "</div>";
     $html .= "</div>";
 
+    $html .= "</div>";
     $html .= "</div>";
     $html .= "</div>";
     $html .= "</div>";
