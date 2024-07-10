097ee6e71f9ef14202c834b7f5b7ed3969c97080
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 18:06:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,6 @@ echo do_shortcode(___hero_archive($key));
 if($has_featured) {
     echo ___featured($key);
 }
-echo $title;
 ?>
 
 <section class="archive-posts md-padding-top md-padding-bottom border-top-default">
