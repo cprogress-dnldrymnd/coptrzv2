6bf05e37c19c737dbe129f97e46ac6b634c2a6ee
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 17:45:58 +0800

    Update elements.php

--- includes/elements.php
+++ includes/elements.php
@@ -103,22 +103,25 @@ function __image($data)
     } else {
         $image = wp_get_attachment_image($image_id, $size);
     }
-
-    if ($class) {
-        $attributes_args[] = $class;
-    }
-    if ($style) {
-        $attributes_args[] = $style;
-    }
     if ($image) {
+        if ($class) {
+            $attributes_args[] = $class;
+        }
+        if ($style) {
+            $attributes_args[] = $style;
+        }
         $_attributes = _attributes($attributes_args);
+
         return "<div $_attributes>$image</div>";
     } else {
         if ($placeholder) {
             $image = wp_get_attachment_image(67781, $size);
-            $class = _attribute('class', array('is-placeholder'));
+
+            $class = _attribute('class', array('is-placeholder image-box'));
             $attributes_args[] = $class;
+
             $_attributes = _attributes($attributes_args);
+
             return "<div $_attributes>$image</div>";
         }
     }
