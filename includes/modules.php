fbb724c331354e64744c5000337720e9930a9a77
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 15:50:46 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1507,7 +1507,7 @@ function ___featured($key)
 {
     $featured = get__theme_option($key . 'featured');
     if ($featured) {
-        $html = "<section class='featured-posts md-padding-top sm-padding-nottom'>";
+        $html = "<section class='featured-posts md-padding-top sm-padding-bottom'>";
         $html .= "<div class='container'>";
         $html .= __heading(array(
             'heading' => 'Featured Articles',
