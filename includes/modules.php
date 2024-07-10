6fcb769557b161aca0b67766f48614eb0ef49823
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 17:31:53 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1443,7 +1443,7 @@ function __post_box_blog($id, $col = false, $featured = false)
     if ($col == true) {
         $html = "<div class='col-lg-4 col-sm-6'>";
     }
-    $html .= "<div class='post-box post-box-blog column-holder position-relative xs-padding rounded-10px overflow-hidden $featured h-100'>";
+    $html .= "<div class='post-box post-box-blog column-holder position-relative xs-padding rounded-10px overflow-hidden $class h-100'>";
     if ($featured) {
         $html .= __background($image);
         $html .= __post_category($id, 'category', 'text-white');
