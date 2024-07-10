b81a0e184e8c01968acb028d50606873da8cd075
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 22:57:06 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1617,6 +1617,8 @@ function ___posts_header($key, $title, $taxonomy)
 
         $html .= "<div class='col-auto'>";
 
+        $html .= "<div class='event-filter'>";
+        $html .= "<p class='fw-medium text-medium'>Select event type:</p>";
         $html .= "<div class='row event-filter'>";
         $html .= "<input name='events_category' value='' type='radio' id='term-all'>";
         $html .= "<label for='term-all'>All</label>";
@@ -1629,6 +1631,7 @@ function ___posts_header($key, $title, $taxonomy)
             $html .= "<label for='term-$term_id'>$term_name</label>";
             $html .= "</div>";
         }
+        $html .= "</div>";
         $html .= "</div>";
 
         $html .= "</div>";
