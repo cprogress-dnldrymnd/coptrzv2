2f133b3bbab8fb67eecc5faa5af2ea107267035c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 02:46:07 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -3556,7 +3556,9 @@ Container::make('post_meta', 'Case Study Settings')
             Field::make('complex', 'feature', __('Feature'))
                 ->add_fields(array(
                     Field::make('text', 'feature_text', __('Feature Text'))
-                )),
+                ))
+                ->set_layout('tabbed-vertical')
+                ->set_header_template('Feature Text: <%- feature_text %>'),
             Field::make('image', 'logo', __('Logo'))
 
         )
