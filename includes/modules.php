1bcbb29be02b32521604ab22f07153501fdcb055
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 02:10:06 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1410,7 +1410,7 @@ function __post_box_blog($data)
         $content_box_class = 'px-20px pb-20px';
     }
     if($bg_image ) {
-        $class .= ' rounded-corner overflow-hidden';
+        $class .= ' rounded-corner overflow-hidden h1-100 bg-black';
     }
     $html = '';
     if ($col == true) {
