1f3530f3ab4a6c37c1e79f7d8f1795f56e59cd69
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 03:56:18 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -326,7 +326,7 @@ function ___sections($id = 'sections')
                 $type = $items['_type'];
                 switch ($type) {
                     case 'layouts':
-                        $layouts = $items['layouts'];
+                        $layouts = $items['post'];
                         foreach ($layouts as $layout) {
                             $layout_id = $layout['id'];
                             $html .= "[layouts id='$layout_id']";
