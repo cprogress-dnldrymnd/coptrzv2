9c233aac9dd6831793909105bb6adfccdf0babb0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 22:38:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if ($has_featured) {
                 while (have_posts()) {
                     the_post();
                     $data['id'] = get_the_ID();
-                    if ($key == 'events') {
+                    if ($key == 'events_') {
                         $crb_event_start_date = get__post_meta_by_id(get_the_ID(), 'crb_event_start_time');
                         $crb_event_start_time = get__post_meta_by_id(get_the_ID(), 'crb_event_start_time');
                         $additional_content = '<ul class="meta-box list-inline text-small fw-medium">';
