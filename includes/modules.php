869572f1f35c4007426742866e2e364df144a22d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 21:44:55 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1455,6 +1455,8 @@ function __post_box_blog($data)
         }
         $html .= '</ul>';
     }
+    $html .= var_dump($metas);
+
     if (in_array('button', $elements)) {
         $html .= __button(array(
             'button_type' => get_post_type($id),
