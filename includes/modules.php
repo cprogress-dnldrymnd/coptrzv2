f4a3369151ee572d906b7219edc36897e7588b40
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-10 18:09:01 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1489,8 +1489,8 @@ function __post_box_blog($id, $col = false, $featured = false)
 function ___hero_archive($key)
 {
 
-    echo $key;
     $hero_heading = get__theme_option($key . 'archive_title');
+    echo $hero_heading;
     $hero_description = _format_text(get__theme_option($key . 'archive_description'));
     $hero_background = get__theme_option($key . 'archive_background');
     $hero_height = 'small-hero';
