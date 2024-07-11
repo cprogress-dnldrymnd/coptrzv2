e820473744de7817e03ee8687a633cc0c009055a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 15:32:10 +0800

    Update single-post.php

--- template-parts/single/single-post.php
+++ template-parts/single/single-post.php
@@ -15,8 +15,8 @@
                     </div>
                 </div>
             </div>
+            <div class="col"></div>
         </div>
-        <div class="col"></div>
     </div>
 </section>
 
