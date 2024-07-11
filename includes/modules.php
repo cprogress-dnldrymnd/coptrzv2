18ec15b6a8dfea069b5e6b3c5c956b55e2a36e8b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 06:21:51 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -281,7 +281,7 @@ function ___sections($id = 'sections', $post_id = '')
                             $background_image_class[] = 'no-overlay';
                         } else if ($background_overlay_type == 'custom') {
                             $styles_section[]  = '--background-overlay-custom: ' . $section_style['background_overlay_custom'];
-                            $background_image_class[]  = 'custom-overlay';
+                            $background_image_class[] = 'custom-overlay';
                         }
                         $classes[] = $section_style['background_attachment'];
                         $classes[] = $section_style['background_size'];
