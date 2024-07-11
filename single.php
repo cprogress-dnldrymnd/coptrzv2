b4508317b5de2a67ff092a44a893c254adce4ce1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 20:28:46 +0800

    cs

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php
-echo ___hero_modules('text-start', 'small-hero');
 if (false === get_template_part('template-parts/single/single', get_post_type())) {
+    echo ___hero_modules('text-start', 'small-hero');
     echo do_shortcode(___sections('sections', get_the_ID()));
 }
 ?>
