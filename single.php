91e42d0dc0ac731e4b8fe536d0f2f922029fd284
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 15:19:23 +0800

    Update single.php

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php
-echo ___hero_modules('text-left', 'small-hero');
+echo ___hero_modules('text-start', 'small-hero');
 if (false === get_template_part('template-parts/single/single', get_post_type())) {
     the_content();
 }
