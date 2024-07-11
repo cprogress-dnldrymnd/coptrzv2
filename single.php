6c6e3092ce5b902fbcb2d07ad56486260348e0ff
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 20:27:43 +0800

    Update single.php

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 <?php
 echo ___hero_modules('text-start', 'small-hero');
 if (false === get_template_part('template-parts/single/single', get_post_type())) {
-    the_content();
+    echo do_shortcode(___sections('sections', get_the_ID()));
 }
 ?>
 <?php get_footer(); ?>
