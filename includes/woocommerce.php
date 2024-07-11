e380567df96613b3689772c4390c954228b4aad3
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 14:31:17 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -680,8 +680,8 @@ function custom_product_variation_training()
         $html .= '</pre>';
 
         $html .= "<input stock='$stock_status_variation' type='radio'  id='variation-$child' data_variations='$json' value='$child'  name='variation-radio'>";
-        $html .= "<label for='variation-$child' class='variation-label status-style-2 w-100'>"; //label
-        $html .= "<div class='inner product-inner w-100 p-20px rounded-corner content-margin'>"; //inner
+        $html .= "<label for='variation-$child' class='variation-label status-style-2 w-100 h-100'>"; //label
+        $html .= "<div class='inner product-inner w-100 p-20px rounded-corner content-margin h-100'>"; //inner
 
 
         $html .= "<div class='row g-3 justify-content-between align-items-center'>";
