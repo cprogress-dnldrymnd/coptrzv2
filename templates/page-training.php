dad585d5368601ff698b436427473d5184c210f5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 15:03:40 +0800

    Update page-training.php

--- templates/page-training.php
+++ templates/page-training.php
@@ -73,6 +73,29 @@
 <div class="modules">
     <?php
     echo do_shortcode(___sections('sections_after_main', get_the_ID()));
+
+    $sections = get__post_meta('sections');
     ?>
+
+
+    <?php
+    $new_sections = array();
+    foreach ($sections as $section) {
+        $new_sections['_type'] = 'section';
+        $new_sections['sec_1'] = $section['sec_1'];
+        $new_sections['title'] = $section['title'];
+        $new_sections['section_id'] = $section['section_id'];
+        $new_sections['section_class'] = $section['section_class'];
+        $new_sections['disable_section'] = $section['disable_section'];
+        $new_sections['section_items'] = $section['section_items'];
+        $new_sections['html_3'] = $section['html_3'];
+        $new_sections['section_styles'] = $section['section_styles'];
+    }
+
+    update_post_meta('carbon_set_post_meta', 'sections', $sections);
+    ?>
+    <pre>
+        <?php var_dump($sections); ?>
+    </pre>
 </div>
 <?php get_footer(); ?>
