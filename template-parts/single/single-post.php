e15614c3adca0e6062f660f8ee6eaa5ca36aba5a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 05:15:32 +0800

    Update single-post.php

--- template-parts/single/single-post.php
+++ template-parts/single/single-post.php
@@ -50,7 +50,7 @@ echo ___hero_modules('text-start', 'small-hero');
     </div>
 </section>
 
-<div class="row justify-content-center">
+<div class="row justify-content-center sm-padding-top sm-padding-bottom border-top-default">
     <div class="col-lg-7">
         <div class="author">
             <div class="text-accent fw-semibold medium-text">Written by:</div>
