374dd3698ac4222afd3a0ac6846fcc0d349b0620
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 14:31:53 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -709,7 +709,7 @@ function custom_product_variation_training()
         ));
         $html .= $price;
 
-        $html .= "<div class='button-box button-box button-bordered'>";
+        $html .= "<div class='button-box button-bordered mt-3'>";
         $html .= "<a href='?add-to-cart=$child' data-quantity='1' class='button ajax_add_to_cart' data-product_id='$child' data-product_sku='$sku' rel='nofollow'>Add to basket</a>";
         $html .= '</div>';
 
