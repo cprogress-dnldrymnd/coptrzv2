2a1aa6bc32af83c7259fa0e2a7856c48cad6489f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-11 21:30:38 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -11,8 +11,8 @@ function action_woocommerce_before_main_content()
             echo do_shortcode(___sections('sections', $product_category_page));
         }
     } else if (is_product()) {
-echo ___hero_modules();
-
+        echo ___hero_modules();
+        echo _product_specifications();
         echo do_shortcode(___sections('sections', get_the_ID()));
     }
 }
