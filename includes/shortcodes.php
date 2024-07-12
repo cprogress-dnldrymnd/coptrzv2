b21cf62ef21d1f3cbcbee1ad91b19a69bcf13c46
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 22:52:10 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -383,6 +383,9 @@ class Shortcodes
                 if ($button_text) {
                     $data['button_text'] = $button_text;
                 }
+                $html .= '<pre>';
+                $html.= var_dump($data);
+                $html .= '</pre>';
                 $html .= __post_box($data);
             }
 
