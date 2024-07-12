fe781e9664e74e5a551edd6f299de7ee80196b1f
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 23:31:35 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -41,7 +41,7 @@ function action_module_content()
 add_action('shutdown', 'action_module_content');
 */
 
-function _date_format($date_input, $format = 'jS F')
+function _date_format($date_input)
 {
     $date = strtotime($date_input);
     $day = date('jS', $date);
