924d1a7eb41b8ceb8b2e0c9a9bbf6aa807e5aeb2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 22:22:31 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -24,8 +24,24 @@ Container::make('theme_options', __('Theme Settings'))
                                 'bg-white' => 'Background White',
                             )
                         ),
-                    Field::make('checkbox', 'is_new', __('Is New')->set_help_text('Add new label tag')),
-
+                    Field::make('checkbox', 'is_new', __('Is New'),
+                    Field::make('association', 'post', 'Select Post')
+                        ->set_types(
+                            array(
+                                array(
+                                    'type'      => 'post',
+                                    'post_type' => 'post',
+                                ),
+                                array(
+                                    'type'      => 'post',
+                                    'post_type' => 'product',
+                                ),
+                                array(
+                                    'type'      => 'post',
+                                    'post_type' => 'guides',
+                                )
+                            )
+                        )
                 ))
         )
     );
