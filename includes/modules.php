e4291e2e62587bcb1a80d74f9ae8466c49a4860d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 22:00:10 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -116,6 +116,13 @@ function ___hero_modules($hero_alignment_args = false, $hero_height_args = false
             'class' => _attribute('class', $heading_class),
             ''
         ));
+
+        $hero .= __heading(array(
+            'heading' => $hero_description,
+            'tag' => 'p',
+            'class' => _attribute('class', $heading_class),
+            ''
+        ));
         $hero .= __description(array(
             'description' => $hero_description,
             'class' => _attribute('class', array('description-box medium-text small-width mx-auto mb-4')),
