3d81a856d3737bab173db181aa65c9290aaf03c4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 21:52:44 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -730,7 +730,7 @@ function __hero_fields()
         Field::make('checkbox', 'hero_hidden', __('Hide Hero'))->set_width(20),
         Field::make('checkbox', 'breadcrumbs_hidden', __('Hide Breadcrumbs'))->set_width(80),
         Field::make('text', 'hero_heading', __('Heading'))->set_help_text('')->set_classes('inline-field')->set_attribute('placeholder', 'Defaults to page title'),
-        Field::make('rich_text', 'hero_description', __('Description'))->set_classes('inline-field'),
+        Field::make('textarea', 'hero_description', __('Description'))->set_classes('inline-field'),
         Field::make('select', 'hero_background_type', __('Background Type'))->set_classes('inline-field')
             ->set_options(
                 array(
