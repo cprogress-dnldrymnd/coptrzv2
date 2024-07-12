14b2367118cf31b780dba59768ff3895ed0775b9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-13 00:07:27 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -666,7 +666,7 @@ $Global_Post_Boxes->name = 'Global Post Boxes';
 $Global_Post_Boxes->singular_name = 'Global Post Box';
 $Global_Post_Boxes->icon = 'dashicons-portfolio';
 $Global_Post_Boxes->supports = array('title', 'revisions', 'editor', 'thumbnail', 'page-attributes');
-$Global_Post_Boxes->show_in_rest = false;
+$Global_Post_Boxes->show_in_rest = true;
 
 
 $Global_Post_Boxes = new newTaxonomy();
