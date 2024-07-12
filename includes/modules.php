fdc21e69f3617fd9fab136666c8c5c4af84f5a7c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-12 21:55:10 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -117,7 +117,7 @@ function ___hero_modules($hero_alignment_args = false, $hero_height_args = false
             ''
         ));
         $hero .= __description(array(
-            'description' => _format_text($hero_description),
+            'description' => $hero_description,
             'class' => _attribute('class', array('description-box medium-text small-width mx-auto mb-4')),
         ));
 
