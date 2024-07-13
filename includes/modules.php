b6a51520a00c68313815b8dd757a50dc6c39c683
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 02:35:48 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -395,10 +395,11 @@ function ___sections($id = 'sections', $post_id = '')
                     case 'border':
                         $border_radius = $section_style['border_radius'];
                         if ($border_radius) {
-                            $classes[] = $border_radius;
-                            if($border_radius == 'custom') {
+                            if ($border_radius == 'custom') {
                                 $border_radius_custom = $section_style['border_radius_custom'];
                                 $container_styles[] = "--border-radius: $border_radius_custom";
+                            } else {
+                                $classes[] = $border_radius;
                             }
                         }
 
