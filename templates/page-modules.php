36bfa69c99406fd93f717d9cb1dd40d3d2176b9a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 00:03:03 +0800

    Update page-modules.php

--- templates/page-modules.php
+++ templates/page-modules.php
@@ -1,7 +1,7 @@
 <?php
 /*-----------------------------------------------------------------------------------*/
 /* Template Name: Modules 
-/* Template Post Type: page, coptrztemplates
+/* Template Post Type: page, guides
 /*-----------------------------------------------------------------------------------*/
 ?>
 <?php get_header(); ?>
