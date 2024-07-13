740c653a40a8aa9ff79aab3d9215cd76c2a8588c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 06:32:47 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1222,7 +1222,10 @@ function __section_fields($name = 'sections')
 
                     ->add_fields('description',  array(
                         Field::make('html', 'html_4')->set_html('<label>Section Description Settings</label>')->set_classes('cb-label'),
-                        Field::make('textarea', 'description', __('Description')),
+                        Field::make('textarea', 'description', __('Description'))->set_width(80)->set_classes('editor-field'),
+                        Field::make('html', 'activate_wysiwyg')->set_width(20)
+                            ->set_html('<button class="button button-primary button-large" id="Wysiwyg-Editor-Trigger">Wysiwyg Editor</button>'),
+                        Field::make('text', 'description_width', __('Description Custom Width')),
                         Field::make('text', 'description_width', __('Description Custom Width')),
                         Field::make('select', 'description_alignment', __('Description Alignment'))
                             ->set_options(
