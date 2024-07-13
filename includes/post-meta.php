907f947bc30df9945aaf60ac618758eb0da978e2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 06:31:56 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1222,7 +1222,7 @@ function __section_fields($name = 'sections')
 
                     ->add_fields('description',  array(
                         Field::make('html', 'html_4')->set_html('<label>Section Description Settings</label>')->set_classes('cb-label'),
-                        Field::make('textarea', 'description', __('Description'))->set_classes('activate-tinymce'),
+                        Field::make('textarea', 'description', __('Description')),
                         Field::make('text', 'description_width', __('Description Custom Width')),
                         Field::make('select', 'description_alignment', __('Description Alignment'))
                             ->set_options(
@@ -1601,7 +1601,9 @@ function __section_fields($name = 'sections')
                                     ))
                                     ->set_header_template('<%- heading %>')
                                     ->add_fields('description',  array(
-                                        Field::make('textarea', 'description', __('Description'))->set_classes('activate-tinymce'),
+                                        Field::make('textarea', 'description', __('Description'))->set_width(80)->set_classes('editor-field'),
+                                        Field::make('html', 'activate_wysiwyg')->set_width(20)
+                                            ->set_html('<button class="button button-primary button-large" id="Wysiwyg-Editor-Trigger">Wysiwyg Editor</button>'),
                                         Field::make('text', 'description_width', __('Description Custom Width')),
                                         Field::make('select', 'description_alignment', __('Description Alignment'))
                                             ->set_options(
