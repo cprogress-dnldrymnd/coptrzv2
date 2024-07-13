8e33e37d49fd84cc286eca59db056d977f507cdd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-13 23:50:00 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -985,6 +985,9 @@ function __section_fields($name = 'sections')
                                 Field::make('html', 'html')->set_html('<h3>This will display latest from coptrz section </h3>'),
                             ))
                     ))
+                    ->add_fields('relate_post',  array(
+                        Field::make('html', 'html')->set_html('<h3>This will display related post base on current post type </h3>'),
+                    ))
                     ->add_fields('global_post_box_selection',  array(
                         Field::make('association', 'post', 'Select Items')
                             ->set_types(
