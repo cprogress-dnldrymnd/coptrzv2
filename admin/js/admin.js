a9f71f97910290b50674234c42cc63b76bfd3e34
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 07:10:33 +0800

    Update admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -17,10 +17,9 @@ function codemirror() {
                 statusbar: false,
                 branding: false
             });
-            
+
             jQuery(document).on("click", '.submit-wysiwyg-trigger', function (event) {
                 jQuery('#wysiwyg-editor').removeClass('active');
-                var textareaId = jQuery('wysiwyg-editor-field');
                 jQuery(this).parent().parent().parent().prev().find('textarea').val(tinymce.get(textareaId).getContent());
             });
 
