73567c454007148e9f2413031697f71437e856fd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 06:47:39 +0800

    cs

---
+++ admin/js/admin.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+    codemirror();
+});
+
+
+function codemirror() {
+    setTimeout(function () {
+        if (typeof tinymce !== 'undefined') {
+            // Loop through each textarea
+            var textareaId = 'wysiwyg-editor-field';
+            // TinyMCE settings to mimic classic editor
+            tinymce.init({
+                selector: '#' + textareaId,  // Use the unique ID
+                plugins: 'lists link charmap paste textcolor',
+                toolbar: 'formatselect | bold italic | bullist numlist | link | forecolor | charmap | pastetext | removeformat',
+                block_formats: 'Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Preformatted=pre',
+                toolbar_location: 'top',
+                menubar: false,
+                statusbar: false,
+                branding: false
+            });
+            /*
+            // Save content on form submission (adjust if your form has a different ID)
+            jQuery('form').submit(function (e) {
+                var textareaId = jQuery('#wysiwyg-editor-field');
+                jQuery(this).val(tinymce.get(textareaId).getContent());
+            });*/
+        } else {
+            console.error('TinyMCE is not loaded.');
+        }
+    }, 1000);
+}
