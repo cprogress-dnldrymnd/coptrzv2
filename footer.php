2def1f02009a4686ff1053b6e2be95f7cadbfcb1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 01:03:21 +0800

    css

--- footer.php
+++ footer.php
@@ -14,60 +14,7 @@ $layouts = get_posts($args);
 foreach ($layouts as $layout) {
     echo do_shortcode("[layouts id='$layout']");
 }
-?>
 
-<section class="reviews bg-black lg-padding-top pb-20px">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="column-holder bg-secondary xs-padding rounded-10px text-white">
-                    <div class="review-box d-flex justify-content-between">
-                        <div class="review-text">
-                            4.9
-                        </div>
-                        <div class="review-stars">
-                            <div class="stars d-flex">
-                                <?php
-                                $i = 1;
-                                while ($i < 6) {
-                                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16"> <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> </svg>';
-                                    $i++;
-                                }
-                                ?>
-                            </div>
-                            <div class="small-text">571 reviews</div>
-                        </div>
-                    </div>
-                    <div class="logo-box text-center mt-4">
-                        <img src="https://dev.coptrz.com/wp-content/uploads/2024/07/reviewsio-logo.png" alt="">
-                    </div>
-                </div>
-            </div>
-           
-        </div>
-    </div>
-</section>
-<div class="footer-cta bg-black">
-    <div class="container">
-        <div class="inner position-relative overflow-hidden rounded-10px sm-padding">
-            <div class="background-image background-overlay">
-                <video src="https://dev.coptrz.com/wp-content/uploads/2023/09/mavic-3-clip-optimized.mp4" autoplay loop muted></video>
-            </div>
-            <div class="position-relative row g-4 align-items-end">
-                <div class="col-lg-8 text-white">
-                    <h2 class="mb-0">
-                        Unlock your business potential with drone technology
-                    </h2>
-                </div>
-                <div class="col-lg-4 text-end">
-                    <div class="button-accent button-box"><a href="#" target="_self" href="#" rel="noopener">Enquire Now</a></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<?php
 
 $hide_footer = get__post_meta('hide_footer');
 if (!$hide_footer) {
