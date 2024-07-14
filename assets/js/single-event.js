910553812ec9b270316b60997f9d531888685888
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 07:26:38 +0800

    Update single-event.js

--- assets/js/single-event.js
+++ assets/js/single-event.js
@@ -1,29 +1,31 @@
-date = jQuery('.event-countdown').attr('date');
-console.log(date);
-var countDownDate = new Date(date).getTime();
+jQuery(document).ready(function () {
+    date = jQuery('.event-countdown').attr('date');
+    console.log(date);
+    var countDownDate = new Date(date).getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
+    // Update the count down every 1 second
+    var x = setInterval(function () {
 
-    // Get today's date and time
-    var now = new Date().getTime();
+        // Get today's date and time
+        var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-    // Display the result in the element with id="demo"
-    jQuery('.countdown-days').html(days);
-    jQuery('.countdown-hours').html(hours);
-    jQuery('.countdown-minutes').html(minutes);
+        // Display the result in the element with id="demo"
+        jQuery('.countdown-days').html(days);
+        jQuery('.countdown-hours').html(hours);
+        jQuery('.countdown-minutes').html(minutes);
 
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        jQuery('.event-countdown-holder').html('EXPIRED');
-    }
-}, 1000);
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            jQuery('.event-countdown-holder').html('EXPIRED');
+        }
+    }, 1000);
+});
