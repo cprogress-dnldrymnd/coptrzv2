e6e3a23bb429e6b2a79ab01b5b654d78bcb2a01e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 07:49:20 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -2255,7 +2255,7 @@ function __post_box($data, $class = [], $content_box_class = [])
     $class[] = 'post-box column-holder position-relative overflow-hidden h-100';
     $content_box_class[] = 'content-box content-margin ';
     if ($featured) {
-        $class[] = 'featured-box text-white d-flex flex-column justify-content-end p-20px rounded-10px';
+        $class[] = 'featured-box text-white d-flex flex-column justify-content-between p-20px rounded-10px';
     } else {
         $class[] = 'content-margin';
     }
@@ -2283,7 +2283,7 @@ function __post_box($data, $class = [], $content_box_class = [])
     $html .= "<div $class_attribute>"; //inner
     if ($featured) {
         $html .= __background($image);
-        $html .= __post_category($id, $category, 'text-white');
+        $html .= __post_category($id, $taxonomy, 'text-white');
     } else {
         if ($bg_image) {
             $html .= __background($image);
