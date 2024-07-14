9e23f015ce040703af7584da462df9b1a043e303
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 07:22:24 +0800

    Update single-event.js

--- assets/js/single-event.js
+++ assets/js/single-event.js
@@ -22,6 +22,6 @@ var x = setInterval(function () {
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        jQuery('.event-countdown-holder').html('EXPIRED');
     }
 }, 1000);
