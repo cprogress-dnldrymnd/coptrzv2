0bfa4fcdbf69e494f6bec5073e6a14127d31a023
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 07:42:36 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -102,7 +102,9 @@ class Shortcodes
             } else {
                 $post_type = get_post_type();
                 $link = get_post_type_archive_link($post_type);
-                $html .= "<li><a class='item text-white' href='$link'>$post_type</a></li>";
+                $post_type_obj = get_post_type_object($post_type);
+                $name = $post_type_obj->labels->singular_name;
+                $html .= "<li><a class='item text-white' href='$link'>$name</a></li>";
             }
             $html .= "<li><span class='item text-white'  >$title</span></li>";
         } else if ($type == 'term') {
