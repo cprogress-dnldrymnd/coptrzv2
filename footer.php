1cc2ad9b321c18857cd83309fc0c8f2e3e46ea38
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 04:09:21 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,7 +17,6 @@ $args = array(
 $layouts = get_posts($args);
 foreach ($layouts as $layout) {
     echo do_shortcode("[layouts id='$layout']");
-    $layouts_global[] = $layout;
 }
 
 var_dump($layouts_global);
