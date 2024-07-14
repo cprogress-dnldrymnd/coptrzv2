9e8caf3219cab48f1b619b9e92759001375b6006
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 02:14:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ if (is_home()) {
     $has_featured = false;
     $has_filter = true;
     $archive_title = 'Blog';
-    $category = 'case_study_category';
+    $category = 'casestudies_category';
     $data = array(
         'col' => true,
         'featured' => false,
