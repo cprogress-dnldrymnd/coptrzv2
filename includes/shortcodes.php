52f8794b7f21ab3abdea8f2a3d18ae933cba62c8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 07:37:49 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -522,7 +522,7 @@ class Shortcodes
         $html .= "<div class='col-auto'>";
         $html .= "<div class='col-minutes'>";
         $html .= "<div class='countdown-box countdown-seconds rounded-corner'></div>";
-        $html .= "<span>Minutes</span>";
+        $html .= "<span>Seconds</span>";
         $html .= "</div>";
         $html .= "</div>";
 
