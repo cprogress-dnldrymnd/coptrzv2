c79100dae96148fa00de8b636b159183cd9ca944
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 09:58:20 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -42,21 +42,40 @@ if (!$hide_footer) {
         </div>
         <div class="footer-columns sm-padding-top sm-padding-bottom">
             <div class="container">
-                <div class="row g-4">
-                    <div class="col-lg col-md-12">
-                        <?php dynamic_sidebar('footer_column_1') ?>
-                    </div>
-                    <div class="col-lg col-md-6">
-                        <?php dynamic_sidebar('footer_column_2') ?>
-                    </div>
-                    <div class="col-lg col-md-6">
-                        <?php dynamic_sidebar('footer_column_3') ?>
-                    </div>
-                    <div class="col-lg col-md-6">
-                        <?php dynamic_sidebar('footer_column_4') ?>
-                    </div>
-                    <div class="col-lg col-md-6">
-                        <?php dynamic_sidebar('footer_column_5') ?>
+                <div class="accordion accordionFooter">
+                    <div class="row g-4">
+                        <div class="col-lg col-md-6">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingTwo">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        About Us
+                                    </button>
+                                </h2>
+                                <?php dynamic_sidebar('footer_column_1') ?>
+                            </div>
+                        </div>
+                        <div class="col-lg col-md-6">
+                            <div class="accordion-item">
+                                
+                                <?php dynamic_sidebar('footer_column_2') ?>
+                            </div>
+
+                        </div>
+                        <div class="col-lg col-md-6">
+                            <div class="accordion-item">
+                                <?php dynamic_sidebar('footer_column_3') ?>
+                            </div>
+                        </div>
+                        <div class="col-lg col-md-6">
+                            <div class="accordion-item">
+                                <?php dynamic_sidebar('footer_column_4') ?>
+                            </div>
+                        </div>
+                        <div class="col-lg col-md-6">
+                            <div class="accordion-item">
+                                <?php dynamic_sidebar('footer_column_5') ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
