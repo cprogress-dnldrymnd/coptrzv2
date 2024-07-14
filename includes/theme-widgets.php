71a8fc4c89211b0becc501f16050821e491e1223
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 01:26:51 +0800

    css

--- includes/theme-widgets.php
+++ includes/theme-widgets.php
@@ -42,7 +42,7 @@ function action_widgets_init()
         $after_title .= "</h4>";
 
         $after_title .= "<div id='collapse$index' class='accordion-collapse collapse' aria-labelledby='footerCol$index' data-bs-parent='#accordionFooter'>";
-        $after_title .= " <div class='accordion-body p-0'>";
+        $after_title .= " <div class='accordion-body pt-0'>";
 
 
         $before_widget = "<div class='accordion-item'>";
