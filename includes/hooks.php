1ebb5f26230e5e4d62320a6e46750a43daec66e1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 04:03:32 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -200,7 +200,7 @@ add_action('wp_footer', 'action_popups');
  */
 function my_plugin_add_admin_bar_items($admin_bar)
 {
-    global $layouts_global;
+    global $popups_id;
     // Run admin bar code here. Will run on both frontend and backend.
     $admin_bar->add_menu(
         array(
