b84044f515f78dd3f19548862e91fc6e651ddd11
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 03:17:57 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -4898,7 +4898,7 @@ function __section_fields($name = 'sections')
                                 ),
 
                             Field::make('color', 'background_gradient_color_1', 'Background Gradient Color[1]')->set_width(50)
-                            
+
                                 ->set_conditional_logic(
                                     array(
                                         array(
@@ -4908,7 +4908,7 @@ function __section_fields($name = 'sections')
                                     )
                                 ),
                             Field::make('text', 'background_gradient_stop_1', 'Background Gradient Stop[1]')->set_width(50)
-                           
+                                ->set_default_value('0%')
                                 ->set_conditional_logic(
                                     array(
                                         array(
@@ -4918,7 +4918,7 @@ function __section_fields($name = 'sections')
                                     )
                                 ),
                             Field::make('color', 'background_gradient_color_2', 'Background Gradient Color[2]')->set_width(50)
-                             
+
                                 ->set_conditional_logic(
                                     array(
                                         array(
@@ -4928,7 +4928,7 @@ function __section_fields($name = 'sections')
                                     )
                                 ),
                             Field::make('text', 'background_gradient_stop_2', 'Background Gradient Stop[2]')->set_width(50)
-                           
+                                ->set_default_value('100%')
                                 ->set_conditional_logic(
                                     array(
                                         array(
