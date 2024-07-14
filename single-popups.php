5e8dbe96cec8adf4fa68cc50e4c6ec48dadc7878
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-14 09:09:57 +0800

    Update single-popups.php

--- single-popups.php
+++ single-popups.php
@@ -5,9 +5,11 @@
 <br>
 <?php while (have_posts()) : the_post(); ?>
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-<?= get_the_ID() ?>">
-        Launch Popup
-    </button>
+    <div class="button-box button-accent">
+        <button type="button" data-bs-toggle="modal" data-bs-target="#modal-<?= get_the_ID() ?>">
+            Launch Popup
+        </button>
+    </div>
     <?= do_shortcode('[popup id=' . get_the_ID() . ']') ?>
 <?php endwhile; ?>
 <?php get_footer() ?>
