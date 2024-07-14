a6ffd024fc04bea111d26b4eeadbe96901a359b8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 04:11:35 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -212,4 +212,4 @@ function my_plugin_add_admin_bar_items($admin_bar)
         )
     );
 }
-add_action('admin_bar_menu', 'my_plugin_add_admin_bar_items', -99);
+add_action('admin_bar_menu', 'my_plugin_add_admin_bar_items', 500);
