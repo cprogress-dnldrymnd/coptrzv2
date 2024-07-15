f46eddbad55b3d3839e6e33425ddb3f89c9b552e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 09:53:58 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
-<section class="not-found text-center md-padding-top md-padding-bottom text-white bg-primary">
-    <div class="container content-margin d-flex align-items-center justify-content-center">
+<section class="not-found text-center md-padding-top md-padding-bottom text-white bg-primary  d-flex align-items-center justify-content-center">
+    <div class="container content-margin">
         <div class="inner content-margin">
             <h1>404 - Page Not Found</h1>
             <div class="description-box">
