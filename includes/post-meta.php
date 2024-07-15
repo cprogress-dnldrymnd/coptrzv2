59bbf2012bc19916845a2a89146a5b54fc39365d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 10:00:39 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -5626,6 +5626,7 @@ Container::make('post_meta', __('Conditional Display'))
                 'product_cat' => __('Product Category'),
                 'after_header' => __('After Header'),
                 'before_footer' => __('Before Footer'),
+                '404' => __('404'),
             )),
 
         Field::make('select', 'display_location_archive', __('Select Archive'))
