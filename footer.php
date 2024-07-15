67d920eb93e3b88e9b859f4df6f06f8138d0ff21
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 13:04:58 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -25,7 +25,9 @@ foreach ($layouts as $layout) {
     if (is_404() && $do_not_display_on != '404') {
         echo do_shortcode("[layouts id='$layout']");
         $layouts_global[] = $layout;
-    } 
+    } else {
+       
+    }
 }
 $hide_footer = get__post_meta('hide_footer');
 if (!$hide_footer) {
