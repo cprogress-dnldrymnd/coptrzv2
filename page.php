c2e8f55853ee2dcc981b70f045c40f7f9124ba06
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 12:09:29 +0800

    css

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 <?php
 $container_width = get__post_meta('container_width');
 ?>
-<section class="default-page <?= $container_width ?> md-padding-top md-padding-bottom">
+<section class="default-page <?= $container_width ?> md-padding-top md-padding-bottom no-overflow">
     <div class="container">
         <?php
         the_content();
