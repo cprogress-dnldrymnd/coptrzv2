6f4e95e86bdd88d79ba88dd74c7d566b5d73e2c5
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 08:55:33 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -846,7 +846,6 @@ function ___sections($id = 'sections', $post_id = '')
                                 $posts[] = $post['id'];
                             }
 
-                            $taxonomy = $post_type . '_category';
 
                         } else {
                             $args['post_type'] = get_post_type();
