383c303406bb13aa59e2733bf4d35e04ff305270
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 12:42:42 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 $SVG = new SVG;
 global $layouts_global;
-$hidden_layouts = get_post_meta(get_the_ID(), '_hidden_layouts', true);
+$hidden_layouts = get_post_meta(get_the_ID(), '_hidden_layouts');
 var_dump($hidden_layouts);
 $args = array(
     'numberposts' => -1,
