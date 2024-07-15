f5dcdc876f3bc095b8fd05959e23998cc344de91
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 12:58:06 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@ $args = array(
         array(
             'key'   => '_do_not_display_on',
             'value' => '404',
-            'compare' => 'NOT EXISTS'
+            'compare' => 'NOT BETWEEN'
         ),
     ),
 );
