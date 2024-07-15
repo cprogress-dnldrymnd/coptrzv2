c3475ff9b646e92d08bd51e83fbc3abf1532c05d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 10:03:10 +0800

    css

--- 404.php
+++ 404.php
@@ -12,25 +12,4 @@ $args = array(
 echo do_shortcode(__layouts($args));
 ?>
 
-<section class="not-found text-center md-padding-top md-padding-bottom text-white bg-primary  d-flex align-items-center justify-content-center">
-    <?= __background(271282); ?>
-    <div class="container content-margin">
-        <div class="inner content-margin">
-            <h1>404 - Page Not Found</h1>
-            <div class="description-box">
-                <p>
-                    Oops! Looks like we’ve flown off course.
-
-                </p>
-                <p>
-                    Why not head back to the home page?
-                </p>
-            </div>
-            <div class="button-box button-bordered">
-                <a href="<?= get_site_url() ?>">Return To Home</a>
-            </div>
-        </div>
-    </div>
-</section>
-
 <?php get_footer(); ?>
