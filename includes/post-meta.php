a8b37d3449987fd50cd75e4b5295afd3eb22d564
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 11:22:48 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -5924,7 +5924,9 @@ Container::make('post_meta', 'Popup Settings')
 /* Default Page
 /*-----------------------------------------------------------------------------------*/
 Container::make('post_meta', 'Page Settings')
-    ->where('post_type', '=', 'page.php')
+    ->where('post_type', '=', 'page')
+    ->set_context('side')
+    ->or_where('post_template', '!=', 'templates/page-modules.php')
     ->add_fields(array(
         Field::make('select', 'container_width', 'Container Width')
             ->set_options(
