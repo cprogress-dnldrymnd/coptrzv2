4965e0ac598c998ac3a99cedb5ea15cb80cbb759
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 12:52:45 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -7,6 +7,11 @@ $args = array(
             'key'   => '_display_location',
             'value' => '404',
         ),
+        array(
+            'key'   => '_do_not_display_on',
+            'value' => '404',
+            'compare' => '!='
+        ),
     )
 );
 echo do_shortcode(__layouts($args));
