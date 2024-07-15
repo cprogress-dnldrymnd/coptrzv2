a48e7f932802c47094bb3618c03e834edc4fc413
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-15 10:35:09 +0800

    Update page-calculator.php

--- templates/page-calculator.php
+++ templates/page-calculator.php
@@ -7,8 +7,18 @@
 <div class="modules">
     <?php
     echo ___hero_modules();
-    echo do_shortcode(___sections('sections', get_the_ID()));
     ?>
+    <section class="calculator medium-container lg-padding-top lg-padding-bottom">
+        <div class="container">
+            <h1>Project Information</h1>
+            <div class="description-box">
+                <p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum
+                </p>
+            </div>
+            <hr>
+        </div>
+    </section>
 </div>
 
 <?php get_footer(); ?>
