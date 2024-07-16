185ab37048f7b538d0d7d9d94e9dd2d3a607eeb1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 17:06:38 +0800

    Update page-calculator.php

--- templates/page-calculator.php
+++ templates/page-calculator.php
@@ -153,10 +153,10 @@
                 $Total_Net_Profit_Over_3_Years = (($Net_Annual_Cash_Inflow * 3) - $Initial_Investment);
                 $ROI = ($Total_Net_Profit_Over_3_Years / $Initial_Investment) * 100;
 
-                console.log(yearsToYearsMonthsDays($Payback_Period));
+        
 
 
-                jQuery('#Payback_Period').text($Payback_Period.toFixed(2) + ' years');
+                jQuery('#Payback_Period').text(yearsToYearsMonthsDays($Payback_Period));
                 jQuery('#ROI').text(parseInt($ROI) + '%');
 
                 jQuery('#calculator').addClass('calculating');
@@ -183,7 +183,6 @@
                 var months = Math.floor((totalDays - (years * 365)) / 30);
                 var days = Math.floor(totalDays - (years * 365) - (months * 30));
                 var result = years + " years, " + months + " months, " + days + " days";
-                console.log(result);
             }
         });;
     </script>
