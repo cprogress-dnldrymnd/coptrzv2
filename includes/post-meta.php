84cb8c4f6123be85ebe32a7d5f329948e9054f05
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 10:52:08 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -92,7 +92,7 @@ function __servicing_fields()
         Field::make('text', 'servicing_heading', 'Heading')->set_classes('inline-field'),
         Field::make('textarea', 'servicing_description', 'Description')->set_classes('inline-field'),
         Field::make('complex', 'servicing_drones', 'Drone Services')->set_classes('inline-field')
-        ->set_header_template('Drone Service: <%- servicing_heading %>'),
+            ->set_header_template('Drone Service: <%- servicing_heading %>')
             ->add_fields(array(
                 Field::make('text', 'service_name', 'Service Name')->set_classes('inline-field'),
                 Field::make('text', 'service_subheading', 'Service Subheading')->set_classes('inline-field'),
