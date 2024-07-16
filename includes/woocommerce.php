99952e160f58dda74e440b56c6fa28b606d5edc4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 11:35:02 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -809,7 +809,7 @@ function __drone_servicing()
         $service_price = $drone['service_price'];
         $html .= "<div class='col-lg-3'>";
         $html .=
-            $html .= "<div class'service-box rounded-corner xs-padding d-flex justify-content-between flex-column $class'>";
+            $html .= "<div class='service-box rounded-corner xs-padding d-flex justify-content-between flex-column $class'>";
         $html .= __heading(array(
             'heading' => $service_name,
             'tag' => 'h3',
