aa2ea94353fc2cf1faa9f83809476f28d5bdc694
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 19:55:44 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -306,8 +306,16 @@ $Guides->rewrite = array(
 
 
 $Industry = new newPostType();
-$Industry->name = 'Industry';
+$Industry->name = 'Industries';
 $Industry->singular_name = 'Industries';
+$Industry->icon = 'dashicons-media-document';
+$Industry->supports = array('title', 'revisions', 'editor', 'thumbnail', 'excerpt', 'author');
+$Industry->show_in_rest = true;
+
+
+$Industry = new newPostType();
+$Industry->name = 'Industries';
+$Industry->singular_name = 'Industry';
 $Industry->icon = 'dashicons-media-document';
 $Industry->supports = array('title', 'revisions', 'editor', 'thumbnail', 'excerpt', 'author');
 $Industry->show_in_rest = true;
@@ -315,7 +323,6 @@ $Industry->rewrite = array(
     'with_front' => false,
     'slug' => 'industry-solutions'
 );
-
 
 
 $Capabilities = new newPostType();
