f567c97077e088caa68d2690b6b5b47216d6956a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 12:16:35 +0800

    css

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -833,36 +833,34 @@ function __drone_servicing()
                 $html .= "</div>"; //end-inner
                 $html .= "</div>"; //end-specs-row-col
 
-                foreach ($servicing_drones as $drone) {
-                    $spec_services = array();
-                    foreach ($drone['service_features'] as $service_feature) {
-                        $spec_services[$service_feature['_type']] = $service_feature['quantity'];
+                $spec_services = array();
+                foreach ($drone['service_features'] as $service_feature) {
+                    $spec_services[$service_feature['_type']] = $service_feature['quantity'];
+                }
+
+                $html .= "<div class='col'>";
+                $html .= "<div class='inner inner-specs-list  h-100 d-flex align-items-center justify-content-center'>"; //inner
+
+                if (array_key_exists($key, $spec_services)) {
+
+                    $quantity = $spec_services[$key];
+
+                    $html .= "<div class='active d-flex align-items-center'> ";
+                    $html .= $SVG->check();
+                    if ($quantity > 1) {
+                        $html .= "<span class='qty ms-2 text-primary d-flex align-items-center'> ";
+                        $html .= "<span class='smaller-text me-2'>x</span> <span class='fw-'medium'>$quantity</span>";
+                        $html .= "</span>";
                     }
-
-                    $html .= "<div class='col'>";
-                    $html .= "<div class='inner inner-specs-list  h-100 d-flex align-items-center justify-content-center'>"; //inner
-
-                    if (array_key_exists($key, $spec_services)) {
-
-                        $quantity = $spec_services[$key];
-
-                        $html .= "<div class='active d-flex align-items-center'> ";
-                        $html .= $SVG->check();
-                        if ($quantity > 1) {
-                            $html .= "<span class='qty ms-2 text-primary d-flex align-items-center'> ";
-                            $html .= "<span class='smaller-text me-2'>x</span> <span class='fw-'medium'>$quantity</span>";
-                            $html .= "</span>";
-                        }
-                        $html .= "</div>";
-                    } else {
-                        $html .= "<div class='not-active d-flex align-items-center'>";
-                        $html .= $SVG->xmark();
-                        $html .= "</div>";
-                    }
-
                     $html .= "</div>";
+                } else {
+                    $html .= "<div class='not-active d-flex align-items-center'>";
+                    $html .= $SVG->xmark();
                     $html .= "</div>";
                 }
+
+                $html .= "</div>";
+                $html .= "</div>";
             }
             $html .= "</div>";
         }
