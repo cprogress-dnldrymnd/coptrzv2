51e691e2f8328fe4ba15111492d7c1b9e5500c4a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 11:15:51 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -774,7 +774,8 @@ function __drone_servicing()
             $specs[$service_feature['_type']] = $service_feature['_type'];
         }
     }
-    $html = "<div class='product-compare'>";
+    $test = json_encode($specs);
+    $html = "<div class='product-compare' test='$test' >";
     $html .= "<div class='comparison products-specifications products-specifications-v2'>"; //products-specifications
     $html .= "<div class='row g-10px'>";
     $html .= "<div class='col-lg-3'>";
