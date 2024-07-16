8746159d05a091ce33f7653b1a226345406bda24
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 22:22:42 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -623,7 +623,6 @@ class Shortcodes
                     "_type" => "custom_field_1",
                     "custom_field_key" => "_testimonial_content",
                     "custom_field_type" => "p",
-                    "custom_field_class" => "big-text"
                 ),
                 array(
                     "_type" => "post_title",
