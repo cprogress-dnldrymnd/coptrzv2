375238b98bd366d74fb80406eccc53db1aa27a27
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 21:02:25 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -50,7 +50,7 @@ class Shortcodes
 
                 $html .= "<div class='col-lg-3'>";
                 $html .= "<div class='inner text-center h-100 border-default rounded-corner xs-padding'>";
-                $html .= "<a href='$link' class='text-white'>";
+                $html .= "<a href='$link' class='text-primary'>";
 
                 $html .= __image($image_args);
                 $html .= __heading(array(
