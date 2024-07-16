3967179a67d2ec111827b0e204c34b0271228480
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-16 23:04:04 +0800

    Update elements.php

--- includes/elements.php
+++ includes/elements.php
@@ -233,9 +233,9 @@ function __button($data)
         $popups_id[] = $button_url;
         $tag = 'button';
         $link = 'data-bs-toggle="modal" data-bs-target="#modal-' . $button_url . '"';
-    } else  {
+    } else {
         $tag = 'button';
-        $link = 'data-target="buy-now"';
+        $link = 'data-target="' . get_the_ID() . '"';
         $class = 'buy-now-trigger';
     }
 
