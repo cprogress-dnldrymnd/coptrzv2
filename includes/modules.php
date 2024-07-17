793f53572f953a2df6cef8d11d6277362bb002db
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-17 21:09:10 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -2577,7 +2577,7 @@ function ___posts_header($key, $title, $taxonomy, $class = '')
     $html .= "<div class='row g-3 align-items-center'>";
     $html .= "<div class='col-auto'>";
     $html .= "<select name='category' class='trigger-change-link'>";
-    $html .= "<option term_lin='$site_url' value=''>Category: All</option>";
+    $html .= "<option term_link='$site_url' value=''>Category: All</option>";
     foreach ($terms as $term) {
         $term_name = $term->name;
         $term_id = $term->term_id;
