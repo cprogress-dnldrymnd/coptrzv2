b229ecd7c9ed1e2fe643820a42509f5036901620
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-18 00:20:01 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -2010,7 +2010,7 @@ function __section_fields($name = 'sections')
                                                     array(
                                                         array(
                                                             'type'      => 'post',
-                                                            'post_type' => 'wpcf7',
+                                                            'post_type' => 'cf7',
                                                         )
                                                     )
                                                 )
