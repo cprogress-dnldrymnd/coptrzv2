4329102c106a0fa4def5a9396acb179cb79f14b1
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-17 16:56:35 +0800

    Update page-calculator.php

--- templates/page-calculator.php
+++ templates/page-calculator.php
@@ -24,28 +24,28 @@
                     <div class="col-lg-6">
                         <label for="" class="mb-2 fw-medium">Initial Investment</label>
                         <div class="input-box d-flex rounded-corner border-default">
-                            <div class="icon d-flex align-items-center justify-content-center fw-medium">$</div>
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
                             <input type="number" name="Initial_Investment" value="100000">
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <label for="" class="mb-2 fw-medium">Annual Revenue from Drone Survey Work</label>
                         <div class="input-box d-flex rounded-corner border-default">
-                            <div class="icon d-flex align-items-center justify-content-center fw-medium">$</div>
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
                             <input type="number" name="Annual_Revenue_from_Drone_Survey_Work" value="70000">
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <label for="" class="mb-2 fw-medium">Annual Operating Costs</label>
                         <div class="input-box d-flex rounded-corner border-default">
-                            <div class="icon d-flex align-items-center justify-content-center fw-medium">$</div>
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
                             <input type="number" name="Annual_Operating_Costs" value="30000">
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <label for="" class="mb-2 fw-medium">Average Savings per Day Using a Drone</label>
                         <div class="input-box d-flex rounded-corner border-default">
-                            <div class="icon d-flex align-items-center justify-content-center fw-medium">$</div>
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
                             <input type="number" name="Average_Savings_per_Day_Using_a_Drone" value="500">
                         </div>
                     </div>
@@ -141,23 +141,23 @@
                     scrollTop: jQuery("#results-position").offset().top
                 }, 100);
 
-                $Initial_Investment = __get_val('Initial_Investment');
-                $Annual_Revenue_from_Drone_Survey_Work = __get_val('Annual_Revenue_from_Drone_Survey_Work');
-                $Annual_Operating_Costs = __get_val('Annual_Operating_Costs');
-                $Average_Savings_per_Day_Using_a_Drone = __get_val('Average_Savings_per_Day_Using_a_Drone');
-                $Number_of_Days_Drone_Will_Be_Used_Per_Year = __get_val('Number_of_Days_Drone_Will_Be_Used_Per_Year');
+                £Initial_Investment = __get_val('Initial_Investment');
+                £Annual_Revenue_from_Drone_Survey_Work = __get_val('Annual_Revenue_from_Drone_Survey_Work');
+                £Annual_Operating_Costs = __get_val('Annual_Operating_Costs');
+                £Average_Savings_per_Day_Using_a_Drone = __get_val('Average_Savings_per_Day_Using_a_Drone');
+                £Number_of_Days_Drone_Will_Be_Used_Per_Year = __get_val('Number_of_Days_Drone_Will_Be_Used_Per_Year');
 
-                $Net_Annual_Cash_Inflow = ($Annual_Revenue_from_Drone_Survey_Work + ($Average_Savings_per_Day_Using_a_Drone * $Number_of_Days_Drone_Will_Be_Used_Per_Year)) - $Annual_Operating_Costs;
-                $Payback_Period = $Initial_Investment / $Net_Annual_Cash_Inflow;
+                £Net_Annual_Cash_Inflow = (£Annual_Revenue_from_Drone_Survey_Work + (£Average_Savings_per_Day_Using_a_Drone * £Number_of_Days_Drone_Will_Be_Used_Per_Year)) - £Annual_Operating_Costs;
+                £Payback_Period = £Initial_Investment / £Net_Annual_Cash_Inflow;
 
-                $Total_Net_Profit_Over_3_Years = (($Net_Annual_Cash_Inflow * 3) - $Initial_Investment);
-                $ROI = ($Total_Net_Profit_Over_3_Years / $Initial_Investment) * 100;
-
-
+                £Total_Net_Profit_Over_3_Years = ((£Net_Annual_Cash_Inflow * 3) - £Initial_Investment);
+                £ROI = (£Total_Net_Profit_Over_3_Years / £Initial_Investment) * 100;
 
 
-                jQuery('#Payback_Period').text(yearsToYearsMonthsDays($Payback_Period));
-                jQuery('#ROI').text(parseInt($ROI) + '%');
+
+
+                jQuery('#Payback_Period').text(yearsToYearsMonthsDays(£Payback_Period));
+                jQuery('#ROI').text(parseInt(£ROI) + '%');
 
                 jQuery('#calculator').addClass('calculating');
                 jQuery('#calculator').removeClass('calculated');
@@ -172,9 +172,9 @@
                 e.preventDefault();
             });
 
-            function __get_val($name) {
-                $val = jQuery('input[name="' + $name + '"]').val();
-                return parseFloat($val);
+            function __get_val(£name) {
+                £val = jQuery('input[name="' + £name + '"]').val();
+                return parseFloat(£val);
             }
 
             function yearsToYearsMonthsDays(value) {
@@ -182,26 +182,26 @@
                 var years = Math.floor(totalDays / 365);
                 var months = Math.floor((totalDays - (years * 365)) / 30);
                 var days = Math.floor(totalDays - (years * 365) - (months * 30));
-                $years = '';
+                £years = '';
                 if (years != 0) {
                     if (years > 1) {
-                        $years = years + " years, ";
+                        £years = years + " years, ";
                     } else {
-                        $years = years + " year, ";
+                        £years = years + " year, ";
                     }
                 }
                 if (months > 1) {
-                    $months = months + " months, ";
+                    £months = months + " months, ";
                 } else {
-                    $months = months + " month, ";
+                    £months = months + " month, ";
                 }
                 if (days > 1) {
-                    $days = days + " days";
+                    £days = days + " days";
                 } else {
-                    $days = days + " day";
+                    £days = days + " day";
                 }
 
-                return $years + $months + $days;
+                return £years + £months + £days;
             }
         });;
     </script>
