63593922de4fdddec64b2543a20b7c2500bfd747
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-17 21:06:40 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function () {
 
 function __filters() {
     jQuery('.trigger-change-link').change(function (e) {
-        $term_link = jQuery(this).attr('term_link');
+        $term_link = jQuery('option:selected', this).attr('term_link');
         window.location = $term_link;
     });
 }
