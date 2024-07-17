1cb945d43c883bc4645c5a975e1014a5b68d5032
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-17 21:04:07 +0800

    cs

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,7 @@ jQuery(document).ready(function () {
 function __filters() {
     jQuery('.trigger-change-link').change(function (e) {
         $term_link = jQuery(this).attr('term_link');
-        $term_link_url = $term_link.replace(/\/$/, '');
-        window.location = $term_link_url;
+        window.location = $term_link;
     });
 }
 
