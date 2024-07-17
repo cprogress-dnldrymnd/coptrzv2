918fd07440a71e5b62ee85ac2b3a70d09893e1a0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-17 20:56:18 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,20 +13,14 @@ jQuery(document).ready(function () {
 function __filters() {
     jQuery('.trigger-change-link').change(function (e) {
         $term_link = jQuery(this).attr('term_link');
-
-        jQuery.ajax({
-            url: $term_link,
-            type: "GET",
-            success: function () {
-                alert("done");
-                return false;
-            },
-            error: function () {
-                alert("testing error");
-            }
-        });
-        e.preventDefault();
+        window.location.href = $term_link;
     });
+}
+
+function processAjaxData(response, urlPath) {
+    document.getElementById("content").innerHTML = response.html;
+    document.title = response.pageTitle;
+    window.history.pushState({ "html": response.html, "pageTitle": response.pageTitle }, "", urlPath);
 }
 
 function __ajax_buy_now() {
