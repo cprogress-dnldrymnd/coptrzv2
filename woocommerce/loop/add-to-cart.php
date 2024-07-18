2c86e639d3ae7253c4f5cde0ad90ee7a5d687b18
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-19 00:17:56 +0800

    Update add-to-cart.php

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -26,6 +26,7 @@ $class = 'col-sm-12';
 if (is_product_taxonomy()) {
 	$product_category_page = __get_product_taxonomy_page(get_queried_object()->term_id);
 	$class = 'col-sm-6';
+	echo $product_category_page;
 }
 
 ?>
