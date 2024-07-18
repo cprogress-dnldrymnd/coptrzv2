e8ab900e6ba7d1511ddbcc322c36a98cf1c7566d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-19 00:07:11 +0800

    Update header-right-landing.php

--- template-parts/header/header-right-landing.php
+++ template-parts/header/header-right-landing.php
@@ -4,7 +4,7 @@ $SVG = new SVG;
 <div class="col-auto d-flex align-items-center">
     <div class="row g-4 header-right">
         <div class="col-auto button-box button-accent button-small d-none d-lg-block">
-            <a href="#" href="#enquire">Enquire Now</a>
+            <a  href="#enquire">Enquire Now</a>
             
         </div>
     </div>
