33a93f04e064e339561f3aedca0b6654c16c3b75
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-19 03:33:25 +0800

    Update page-training.php

--- templates/page-training.php
+++ templates/page-training.php
@@ -24,11 +24,11 @@
                                 <p class="fw-medium medium-text">Select delivery method:</p>
                                 <div class="filter-box bg-light rounded-corner">
                                     <div class="row">
-                                        <div class="col-auto"><input name="delivery_method" value="online-self-paced" type="radio" id="Online" checked>
-                                            <label class="rounded-corner trigger-training-ajax" for="Online">Online Self-paced</label>
+                                        <div class="col-auto"><input name="delivery_method" value="online-self-paced" type="radio" id="online" checked>
+                                            <label class="rounded-corner trigger-training-ajax" for="online">Online Self-paced</label>
                                         </div>
-                                        <div class="col-auto"><input name="delivery_method" value="classroom" type="radio" id="Classroom">
-                                            <label class="rounded-corner trigger-training-ajax" for="Classroom">Classroom</label>
+                                        <div class="col-auto"><input name="delivery_method" value="classroom" type="radio" id="classroom">
+                                            <label class="rounded-corner trigger-training-ajax" for="classroom">Classroom</label>
                                         </div>
                                     </div>
                                 </div>
