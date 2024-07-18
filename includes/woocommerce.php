7557e6f4a8b0e2ddbe415b61fce92123f4f9f84a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-19 02:59:50 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -740,7 +740,7 @@ function custom_product_variation_training($delivery_method = 'online-self-paced
         );
     }
 
-    usort($array, 'date_compare'); 
+    usort($child_array, 'date_compare'); 
 
     return var_dump($child_array);
 
