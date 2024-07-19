cfa4e2651bf9526864185fd8bdf90c77bb75172d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-19 14:17:49 +0800

    Update page-calculator.php

--- templates/page-calculator.php
+++ templates/page-calculator.php
@@ -8,131 +8,135 @@
     <?php
     echo ___hero_modules();
     ?>
-    <form>
-        <section class="calculator medium-container lg-padding-top md-padding-bottom">
-            <div class="container content-margin">
-                <div class="inner px-5 content-margin">
-                    <h1>Project Information</h1>
-                    <div class="description-box fw-light small-text">
-                        <p>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum
-                        </p>
+    <section class="calculator medium-container lg-padding-top md-padding-bottom">
+        <div class="container content-margin">
+            <div class="inner px-5 content-margin">
+                <h1>Project Information</h1>
+                <div class="description-box fw-light small-text">
+                    <p>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum
+                    </p>
+                </div>
+            </div>
+            <hr class="my-5">
+            <div class="inner px-5">
+                <div class="row g-4 form-groups">
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Initial Investment</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
+                            <input type="number" name="Initial_Investment" value="100000">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Annual Revenue from Drone Survey Work</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
+                            <input type="number" name="Annual_Revenue_from_Drone_Survey_Work" value="70000">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Annual Operating Costs</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
+                            <input type="number" name="Annual_Operating_Costs" value="30000">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Average Savings per Day Using a Drone</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
+                            <input type="number" name="Average_Savings_per_Day_Using_a_Drone" value="500">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Number of Days Drone Will Be Used Per Year</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <input type="number" name="Number_of_Days_Drone_Will_Be_Used_Per_Year" value="150">
+                        </div>
                     </div>
                 </div>
-                <hr class="my-5">
-                <div class="inner px-5">
-                    <div class="row g-4 form-groups">
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Initial Investment</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
-                                <input type="number" name="Initial_Investment" value="100000">
+            </div>
+        </div>
+    </section>
+    <section class="background-gradient-default medium-container md-padding-top md-padding-bottom">
+        <div class="container">
+            <div class="inner px-5 content-margin">
+                <h3>Who should we send your report to?</h3>
+                <div class="description-box fw-light small-text">
+                    <p>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum
+                    </p>
+                </div>
+                <div class="row g-4 form-groups">
+                <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">First Name</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <input type="text" name="First_Name">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Last Name</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <input type="text" name="Last_Name">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Company</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <input type="text" name="Company">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Email Address</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <input type="text" name="Email_Address">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="" class="mb-2 fw-medium">Telephone</label>
+                        <div class="input-box d-flex rounded-corner border-default">
+                            <input type="text" name="Telephone">
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="button-box button-accent">
+                            <button class="w-100" id="generate_report">Generate my report</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="inner your-result">
+                <div id="results-position" class="md-padding-top"></div>
+                <div class="results-holder sm-padding bg-primary rounded-corner text-white" id="results">
+                    <div id="results-animation">
+                        <div class="loader"></div>
+                    </div>
+                    <div id="results-box">
+                        <h2 class="mb-4">Your Results</h2>
+                        <hr class="mb-4">
+                        <div class="description-box mb-5 fw-light small-text">
+                            <p>
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                            </p>
+                        </div>
+                        <div class="result-box mb-5">
+                            <h3>Your payback period</h3>
+                            <div class="result rounded-corner xs-padding text-center big-text text-primary" id="Payback_Period">
+                                Approximately X months, X days
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Annual Revenue from Drone Survey Work</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
-                                <input type="number" name="Annual_Revenue_from_Drone_Survey_Work" value="70000">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Annual Operating Costs</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
-                                <input type="number" name="Annual_Operating_Costs" value="30000">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Average Savings per Day Using a Drone</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <div class="icon d-flex align-items-center justify-content-center fw-medium">£</div>
-                                <input type="number" name="Average_Savings_per_Day_Using_a_Drone" value="500">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Number of Days Drone Will Be Used Per Year</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <input type="number" name="Number_of_Days_Drone_Will_Be_Used_Per_Year" value="150">
+                        <div class="result-box">
+                            <h3>ROI over 3 years</h3>
+                            <div class="result rounded-corner xs-padding text-center big-text text-primary" id="ROI">
+                                245%
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-        <section class="background-gradient-default medium-container md-padding-top md-padding-bottom">
-            <div class="container">
-                <div class="inner px-5 content-margin">
-                    <h3>Who should we send your report to?</h3>
-                    <div class="description-box fw-light small-text">
-                        <p>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum
-                        </p>
-                    </div>
-                    <div class="row g-4 form-groups">
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Full Name</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <input type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Company</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <input type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Email Address</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <input type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <label for="" class="mb-2 fw-medium">Telephone</label>
-                            <div class="input-box d-flex rounded-corner border-default">
-                                <input type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="button-box button-accent">
-                                <button class="w-100" id="generate_report">Generate my report</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="inner your-result">
-                    <div id="results-position" class="md-padding-top"></div>
-                    <div class="results-holder sm-padding bg-primary rounded-corner text-white" id="results">
-                        <div id="results-animation">
-                            <div class="loader"></div>
-                        </div>
-                        <div id="results-box">
-                            <h2 class="mb-4">Your Results</h2>
-                            <hr class="mb-4">
-                            <div class="description-box mb-5 fw-light small-text">
-                                <p>
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                                </p>
-                            </div>
-                            <div class="result-box mb-5">
-                                <h3>Your payback period</h3>
-                                <div class="result rounded-corner xs-padding text-center big-text text-primary" id="Payback_Period">
-                                    Approximately X months, X days
-                                </div>
-                            </div>
-                            <div class="result-box">
-                                <h3>ROI over 3 years</h3>
-                                <div class="result rounded-corner xs-padding text-center big-text text-primary" id="ROI">
-                                    245%
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </form>
+        </div>
+    </section>
     <?php get_footer(); ?>
     <script>
         jQuery(document).ready(function() {
