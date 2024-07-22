0d01a00f5cc9f83936ca123cb8d1b51d27463934
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-22 20:51:10 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -295,8 +295,14 @@ function action_pre_get_posts($query)
         if (is_post_type_archive('industries') || is_post_type_archive('capabilities')) {
             $query->set('posts_per_page', -1);
         } else if (is_post_type_archive('events') || is_tax('events_category')) {
-            $query->set('meta_key', '_crb_event_start_date');
-            $query->set('orderby', 'meta_value_num');
+            $meta_query[] = [
+                'key'     => '_crb_event_start_date',
+                'value'   => date('Y/m/d'),
+                'compare' => '>=',
+                'type'    => 'DATETIME'
+            ];
+            $query->set('meta_query', $meta_query);
+            $query->set('orderby', 'meta_value');
             $query->set('order', 'ASC');
         } else {
             if (isset($_GET['posts_per_page'])) {
