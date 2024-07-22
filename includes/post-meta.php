832003cf6f8a26b826cde18c5767ef0059fc7bfe
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-22 11:40:04 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1696,6 +1696,12 @@ function __section_fields($name = 'sections')
                                             ->add_fields('drone_servicing', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display drone servicing blocks. </h3>'),
                                             ))
+                                            ->add_fields('three_year_servicing_plans', array(
+                                                Field::make('html', 'html')->set_html('<h3>This will display three year servicing plans. </h3>'),
+                                            ))
+                                            ->add_fields('remote_support', array(
+                                                Field::make('html', 'html')->set_html('<h3>This will display remote support. </h3>'),
+                                            ))
                                             ->add_fields('brands_logo_slider', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display brands logo slider. </h3>'),
                                             ))
