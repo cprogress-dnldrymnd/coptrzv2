5e88d94cd13c4bb57bd7cbf124238aa7424d9bc7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-22 20:38:47 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -2636,7 +2636,14 @@ function ___posts_header($key, $title, $taxonomy, $class = '')
     }
 
 
- 
+    if ($key == 'events_') {
+        $html .= "<div class='col-auto'>";
+        $html .= "<select name='sort'>";
+        $html .= "<option value='ASC'>Sort By: Latest</option>";
+        $html .= "<option value='DESC'>Sort By: Oldest</option>";
+        $html .= "</select>";
+        $html .= "</div>";
+    }
 
     $html .= "</div>";
     $html .= "</div>";
