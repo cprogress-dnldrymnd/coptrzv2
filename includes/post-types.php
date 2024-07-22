fe4cb8663d82678dc4766271ff2226929fbabd64
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-22 19:43:21 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -307,7 +307,7 @@ $Capabilities->rewrite = array(
 $Events = new newPostType();
 $Events->name = 'Events';
 $Events->singular_name = 'Event';
-$Events->icon = 'dashicons-camera-alt';
+$Events->icon = 'dashicons-cover-image';
 $Events->supports = array('title', 'revisions', 'editor', 'thumbnail');
 $Events->show_in_rest = true;
 $Events->rewrite = array(
@@ -563,21 +563,12 @@ $Guide_Category->args = array(
     )
 );
 
-$Slides = new newPostType();
-$Slides->name = 'Slides';
-$Slides->singular_name = 'Slide';
-$Slides->icon = 'dashicons-testimonial';
-$Slides->supports = array('title', 'revisions');
-$Slides->exclude_from_search = true;
-$Slides->publicly_queryable = false;
-$Slides->show_in_admin_bar = false;
-$Slides->has_archive = false;
 
 
 $Layouts = new newPostType();
 $Layouts->name = 'Layouts';
 $Layouts->singular_name = 'Layout';
-$Layouts->icon = 'dashicons-testimonial';
+$Layouts->icon = 'dashicons-layout';
 $Layouts->supports = array('title', 'revisions', 'page-attributes', 'editor');
 $Layouts->exclude_from_search = true;
 $Layouts->publicly_queryable = true;
@@ -598,54 +589,6 @@ $Popup->publicly_queryable = true;
 $Popup->show_in_admin_bar = true;
 $Popup->has_archive = false;
 $Popup->show_in_rest = true;
-
-
-
-$Model3d = new newPostType();
-$Model3d->name = '3D Model Libraries';
-$Model3d->singular_name = '3D Model Library';
-$Model3d->icon = 'dashicons-testimonial';
-$Model3d->supports = array('title', 'revisions', 'thumbnail', 'editor', 'excerpt');
-$Model3d->exclude_from_search = true;
-$Model3d->publicly_queryable = true;
-$Model3d->show_in_admin_bar = true;
-$Model3d->has_archive = false;
-$Model3d->show_in_rest = true;
-
-
-
-$Model3D_Category = new newTaxonomy();
-$Model3D_Category->taxonomy = 'Model3d_Category';
-$Model3D_Category->post_type = '3dmodellibraries';
-$Model3D_Category->args = array(
-    'label'        => 'Categories',
-    'labels' => array(
-        'name'                       => _x('Categories', 'Taxonomy General Name', 'text_domain'),
-        'singular_name'              => _x('Category', 'Taxonomy Singular Name', 'text_domain'),
-        'menu_name'                  => __('Category', 'text_domain'),
-        'all_items'                  => __('All Items', 'text_domain'),
-        'parent_item'                => __('Parent Item', 'text_domain'),
-        'parent_item_colon'          => __('Parent Item:', 'text_domain'),
-        'new_item_name'              => __('New Item Name', 'text_domain'),
-        'add_new_item'               => __('Add New Item', 'text_domain'),
-        'edit_item'                  => __('Edit Item', 'text_domain'),
-        'update_item'                => __('Update Item', 'text_domain'),
-        'view_item'                  => __('View Item', 'text_domain'),
-        'separate_items_with_commas' => __('Separate items with commas', 'text_domain'),
-        'add_or_remove_items'        => __('Add or remove items', 'text_domain'),
-        'choose_from_most_used'      => __('Choose from the most used', 'text_domain'),
-        'popular_items'              => __('Popular Items', 'text_domain'),
-        'search_items'               => __('Search Items', 'text_domain'),
-        'not_found'                  => __('Not Found', 'text_domain'),
-        'no_terms'                   => __('No items', 'text_domain'),
-        'items_list'                 => __('Items list', 'text_domain'),
-        'items_list_navigation'      => __('Items list navigation', 'text_domain'),
-    ),
-    'hierarchical' => true,
-    'query_var'    => true,
-    'show_in_rest' => true,
-);
-
 
 
 $Product_compare = new newPostType();
