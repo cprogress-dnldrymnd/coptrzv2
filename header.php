e3b0fa5a35b88bba0dfe56b55575c1d65d86509a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-22 14:18:48 +0800

    css

--- header.php
+++ header.php
@@ -18,14 +18,11 @@
 	<?php wp_head(); ?>
 </head>
 
-<?php
-$header_background = get__post_meta('header_background');
-?>
 
 <body <?php body_class(); ?>>
 	<header class="header small-text overflow-hidden">
 		<div class="container">
-			<div class="header-inner mt-20px rounded-10px <?= $header_background ?>">
+			<div class="header-inner mt-20px rounded-10px">
 				<div class="row justify-content-between">
 					<?php get_template_part('template-parts/header/header-left') ?>
 					<?php get_template_part('template-parts/header/header-menu') ?>
--- includes/hooks.php
+++ includes/hooks.php
@@ -308,15 +308,20 @@ add_action('pre_get_posts', 'action_pre_get_posts', 1);
 function action_body_class($classes)
 {
     $hero_hidden = get__post_meta('hero_hidden');
+    $header_background = get__post_meta('header_background');
+
     $product_category_page = false;
     if (is_product_taxonomy()) {
         $product_category_page = __get_product_taxonomy_page(get_queried_object()->term_id);
     }
     if ($hero_hidden) {
-        $classes[] = 'hero-hidden';
+        $classes[] = 'hero-bg-black';
+    }
+    if ($header_background) {
+        $classes[] = "hero-$header_background";
     }
 
-    if(!$product_category_page) {
+    if (!$product_category_page) {
         $classes[] = 'hide-price';
         $classes[] = 'hide-stock';
         $classes[] = 'product-loop-style-1';
