871ac2f57b857c272b17e67fbdb3933ba26ec13b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-23 23:03:32 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -246,7 +246,7 @@ function __header_menu() {
                     jQuery(this).next().toggleClass('active');
         
                     jQuery('.tab-links').addClass('d-none');
-                    jQuery($target).removeClass('d-none');
+                    jQuery($target).addClass('d-none');
                     e.preventDefault();
                 }
             );
