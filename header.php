193016500d9d59adc91ff87424a0e6477543a0d7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-23 13:52:38 +0800

    cs

--- header.php
+++ header.php
@@ -41,22 +41,21 @@
 
 		<?php
 
-		if (get_post_type() ==  'capabilities' && get_the_ID() != 271277) {
+		if (get_post_type() ==  'industries' && get_the_ID() != 65092) {
 
-			$sections = get__post_meta_by_id(271277, 'sections');
-			$hero_description = get__post_meta_by_id(271277, 'hero_description');
-			$hero_height = get__post_meta_by_id(271277, 'hero_height');
-			$hero_alignment = get__post_meta_by_id(271277, 'hero_alignment');
+			$sections = get__post_meta_by_id(65092, 'sections');
+			$hero_description = get__post_meta_by_id(65092, 'hero_description');
+			$hero_height = get__post_meta_by_id(65092, 'hero_height');
+			$hero_alignment = get__post_meta_by_id(65092, 'hero_alignment');
 
-			$hero_form_enable = get__post_meta_by_id(271277, 'hero_form_enable');
-			$hero_form_image = get__post_meta_by_id(271277, 'hero_form_image');
-			$hero_form_heading = get__post_meta_by_id(271277, 'hero_form_heading');
-			$hero_form_description = get__post_meta_by_id(271277, 'hero_form_description');
-			$hero_form_style = get__post_meta_by_id(271277, 'hero_form_style');
-			$hero_form = get__post_meta_by_id(271277, 'hero_form');
-			$buttons = get__post_meta_by_id(271277, 'buttons');
+			$hero_form_enable = get__post_meta_by_id(65092, 'hero_form_enable');
+			$hero_form_image = get__post_meta_by_id(65092, 'hero_form_image');
+			$hero_form_heading = get__post_meta_by_id(65092, 'hero_form_heading');
+			$hero_form_description = get__post_meta_by_id(65092, 'hero_form_description');
+			$hero_form_style = get__post_meta_by_id(65092, 'hero_form_style');
+			$hero_form = get__post_meta_by_id(65092, 'hero_form');
 
-			
+
 
 			$sections_r = get__post_meta_by_id(get_the_ID(), 'sections');
 			carbon_set_post_meta(get_the_ID(), 'sections', $sections);
@@ -69,13 +68,9 @@
 			carbon_set_post_meta(get_the_ID(), 'hero_form_description', $hero_form_description);
 			carbon_set_post_meta(get_the_ID(), 'hero_form_style', $hero_form_style);
 			carbon_set_post_meta(get_the_ID(), 'hero_form', $hero_form);
-			carbon_set_post_meta(get_the_ID(), 'buttons', $buttons);
 
 
-			$sections_after_main = get__post_meta_by_id(271277, 'sections_after_main');
-			$sections_after_main_r = get__post_meta_by_id(get_the_ID(), 'sections_after_main');
-			carbon_set_post_meta(get_the_ID(), 'sections_after_main', $sections_after_main);
-
+			carbon_set_post_meta(get_the_ID(), 'sections_after_main', $sections);
 
 			$data = array(
 				'ID' => get_the_ID(),
