5dc0e4be55914be635fab8552d273cc0c8a2f516
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-25 11:01:16 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -861,7 +861,7 @@ function ___sections($id = 'sections', $post_id = '')
                             $posts_list = $posts;
                         }
 
-                        $html .= "<div class='row g-4 same-image-height row-global-post' style='--image-padding: 35%'>";
+                        $html .= "<div class='row g-4 justify-content-center same-image-height row-global-post' style='--image-padding: 35%'>";
 
                         foreach ($posts_list as $post) {
                             $data = array(
