e1d035a4214895242ae7445c3fd0f1bf3a5e3b58
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-25 11:02:33 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -873,13 +873,15 @@ function ___sections($id = 'sections', $post_id = '')
                             );
                             if ($col_class) {
                                 $data['col'] = $col_class;
+                            } else {
+                                if ($col_class) {
+                                    $data['col'] = array(
+                                        'col-lg-4',
+                                        'col-md-6',
+                                    );
+                                }
                             }
-                            if ($col_class) {
-                                $data['col'] = array(
-                                    'col-lg-4',
-                                    'col-md-6',
-                                );
-                            }
+
                             $html .= __post_box($data);
                         }
 
