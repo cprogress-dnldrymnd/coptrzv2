9c9067e552cac9e4ac6815db98d94fe9a08af153
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-28 15:15:48 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,7 +262,7 @@ function __header_menu() {
                 setTimeout(function () {
                     $this.addClass('show-menu');
                     $this.next().addClass('show-menu');
-                }, 100);
+                }, 50);
 
                 jQuery('body').removeClass('mini-cart-active');
 
@@ -303,7 +303,7 @@ function __header_menu() {
                 setTimeout(function () {
                     $this.parent().parent().parent().removeClass('active');
                     $this.parent().parent().parent().prev().removeClass('active');
-                }, 100);
+                }, 300);
                 e.preventDefault();
             });
 
