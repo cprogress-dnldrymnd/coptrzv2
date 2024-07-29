c92a3655ee3afd4c24bdc8e4aed36959721afdc4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-29 14:35:42 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -71,13 +71,8 @@ function ___hero_modules($hero_alignment_args = false, $hero_height_args = false
         $hero_background_type = 'self-hosted';
     } else {
         $hero_background = get__post_meta('hero_background');
-        if ($hero_background) {
-            $hero_background_youtube = get__post_meta('hero_background_youtube');
-            $hero_background_type = get__post_meta('hero_background_type');
-        } else {
-            $hero_background = get_post_thumbnail_id();
-            $hero_background_type = 'self-hosted';
-        }
+        $hero_background_youtube = get__post_meta('hero_background_youtube');
+        $hero_background_type = get__post_meta('hero_background_type');
     }
 
 
