c84321396d03f4e766bdb4704bdeff41f7184b96
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-29 14:28:15 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -1147,7 +1147,7 @@ function __three_year_servicing_plans()
     ));
 
 
-    $html .= '<div class="filter-style-1"><div class="filter-box bg-light rounded-corner"> <div class="row"> <div class="col-6"> <input name="drone_size" value="small-drone" type="radio" id="small-drone" checked> <label class="rounded-corner" for="small-drone">Small Drone</label> </div> <div class="col-6"> <input name="drone_size" value="large-drone" type="radio" id="large-drone"> <label class="rounded-corner" for="large-drone">Large Drone</label> </div> </div> </div></div>';
+    $html .= '<div class="filter-style-1"><div class="filter-box bg-light rounded-corner"> <div class="row"> <div class="col-6"> <input name="drone_size" value="small-drone" type="radio" id="small-drone" checked> <label class="rounded-corner w-100 text-center" for="small-drone">Small Drone</label> </div> <div class="col-6"> <input name="drone_size" value="large-drone" type="radio" id="large-drone"> <label class="rounded-corner w-100 text-center" for="large-drone">Large Drone</label> </div> </div> </div></div>';
 
     $html .= "</div>";
 
