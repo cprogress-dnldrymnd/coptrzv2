2c415aed503035016a97a8cf944d78b60ad0b66c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-07-29 14:41:18 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -84,14 +84,15 @@ function ___hero_modules($hero_alignment_args = false, $hero_height_args = false
 
     $hero_form_enable = get__post_meta('hero_form_enable');
 
-    $hero_form_image_val = get__post_meta('hero_form_image');
 
     if (get_post_type() == 'guides') {
         if (!$hero_form_image_val) {
             $hero_form_image = get_post_thumbnail_id();
+        } else {
+            $hero_form_image = get__post_meta('hero_form_image');
         }
     } else {
-        $hero_form_image = $hero_form_image_val;
+        $hero_form_image = get__post_meta('hero_form_image');
     }
 
 
