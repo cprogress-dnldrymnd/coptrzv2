4236b0cb38f72c1e4b2d8de7f8a28abfb77369e7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-01 01:50:40 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="author" content="">
 	<meta name="format-detection" content="telephone=no">
 	<link rel="canonical" href="<?= canonical() ?>" />
