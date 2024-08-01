05f8b33c6c44256bf7ca2c2fd89655c741e0615e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-01 22:31:50 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -75,6 +75,7 @@ function get__theme_option($value)
 function enqueue_scripts()
 {
 	//wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css');
+	wp_enqueue_style('intl-tel', 'https://cdn.jsdelivr.net/npm/intl-tel-input@21.2.7/build/css/intlTelInput.css', NULL, coptz_version);
 	wp_enqueue_script('intlTelInput', 'https://cdn.jsdelivr.net/npm/intl-tel-input@21.2.7/build/js/intlTelInput.js', NULL, coptz_version);
 
 	//wp_enqueue_script('swiper', vendor_dir . 'swiper/js/swiper-bundle.min.js');
