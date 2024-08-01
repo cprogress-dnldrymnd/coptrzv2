cfec5ee4b24887ee458930ff4a849c8f0bacb7a7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-01 21:31:23 +0800

    Delete single-product.php

--- woocommerce/single-product.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<?php
-if (false === get_template_part('template-parts/single/single', get_post_type())) {
-    echo ___hero_modules();
-    echo do_shortcode(___sections('sections', get_the_ID()));
-}
-?>
-<?php get_footer(); ?>
