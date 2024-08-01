1661ad66d3e804c7ce94e09ee6bd143bac3ee2ab
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-01 19:30:56 +0800

    Update single-product.php

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <?php
 if (false === get_template_part('template-parts/single/single', get_post_type())) {
+    echo ___hero_modules();
     echo do_shortcode(___sections('sections', get_the_ID()));
 }
 ?>
