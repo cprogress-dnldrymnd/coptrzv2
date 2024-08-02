31aff9a1eb246d687df9342eb4b97849626119ab
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-02 17:57:09 +0800

    Update product-thumbnails.php

--- woocommerce/single-product/product-thumbnails.php
+++ woocommerce/single-product/product-thumbnails.php
@@ -35,17 +35,16 @@ foreach ($attachment_ids as $attachment_id) {
 <div class="product-thumb-holder">
 	<div thumbsSlider class="swiper product-thumb">
 		<div class="swiper-wrapper">
-			<div class="swiper-slide">
-				<?= apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($post_thumbnail_id), $post_thumbnail_id); ?>
-			</div>
-
 			<?php if ($image_ids) { ?>
 				<?php foreach ($image_ids as $key => $id) { ?>
 					<?php
-					$item = $key % 6;
-					echo $item;
+					$var = $key % 6;
 					?>
-
+					<?php if ($var == 0) { ?>
+						<div class="swiper-slide">
+							<?= apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($id), $post_thumbnail_id); ?>
+						</div>
+					<?php } ?>
 				<?php } ?>
 			<?php } ?>
 
