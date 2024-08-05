4681b457c7db8f9ae5909570d705cc980467760b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-05 18:30:06 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -79,7 +79,7 @@ function _pagination($has_pagination, $query, $data = false)
                                 <div class="nav-links">
                                     <?php
                                     $paginate_links = paginate_links(array(
-                                        'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+                                        'base'         => str_replace(999999999, '%#%', 'test.com'),
                                         'total'        => $query->max_num_pages,
                                         'current'      => max(1, get_query_var('paged')),
                                         'format'       => '?paged=%#%',
