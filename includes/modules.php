d858982a803ac7617ce7deade3f52c237d961709
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-05 17:51:54 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -94,12 +94,8 @@ function _pagination($has_pagination, $query, $url=false)
                                         'add_fragment' => '',
                                     ));
 
-                                    if ($url) {
-                                        echo str_replace('wp-admin/admin-ajax.php', $url, $paginate_links);
-                                    } else {
-                                        echo $paginate_links;
-                                    }
-
+                                  
+                                    echo $paginate_links;
 
                                     ?>
                                 </div>
