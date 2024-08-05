cd2ddbb0ff0c45d79e261735c1c62338e174b1ac
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-05 18:46:19 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -62,7 +62,7 @@ function _date_format($date_input, $include_year = false)
 }
 function modify_get_pagenum_link_defaults($result, $pagenum)
 {
-    return 'test.com';
+    return get_query_var('posts_per_page');
 }
 add_filter("get_pagenum_link", "modify_get_pagenum_link_defaults", 10, 2);
 
