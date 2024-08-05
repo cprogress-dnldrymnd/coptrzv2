773fa9e9a7c7239c5acbf891678d57959029d6ec
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-05 21:37:30 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1767,7 +1767,11 @@ function ____columns_modules($items, $id, $html = '')
                         $column_class[] = $column_width;
                     }
                     if ($column_width_tablet) {
-                        $column_class[] = $column_width_tablet;
+                        if(count($items) == 3 && $column_width_tablet == 'col-md-6') {
+                            $column_class[] = 'col-md-12';
+                        } else {
+                            $column_class[] = $column_width_tablet;
+                        }
                     }
                     if ($column_width_mobile) {
                         $column_class[] = $column_width_mobile;
