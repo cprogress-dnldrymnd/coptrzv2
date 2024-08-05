dbcb8871c58efc9ae0b69d79eee317cc0c1e057a
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-05 19:03:40 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -60,6 +60,23 @@ function _date_format($date_input, $include_year = false)
     }
     return $newDate;
 }
+function modify_get_pagenum_link_defaults($result, $pagenum)
+{
+    if (isset($_GET['url'])) {
+        $url =  $_GET['url'] . 'page/' . $pagenum;
+        if (isset($_GET['posts_per_page'])) {
+            $posts_per_page = '&posts_per_page=' . $_GET['posts_per_page'];
+        }
+        if (isset($_GET['s'])) {
+            $s = '&s=' . $_GET['s'];
+        }
+
+        return $url . '?' . $posts_per_page . $s;
+    } else {
+        return $result;
+    }
+}
+add_filter("get_pagenum_link", "modify_get_pagenum_link_defaults", 10, 2);
 
 function _pagination($has_pagination, $query, $data = false)
 {
