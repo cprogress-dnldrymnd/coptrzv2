f0a2e4cda0efbb085cedaeb0c52db16db953e417
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-06 19:13:49 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1587,9 +1587,8 @@ function ____gallery_modules($data)
             if ($column_width_mobile) {
                 $column_class_args[] = $column_width_mobile;
             }
-            $image_args['class'] = _attribute('class', array('image-box'));
+            $image_args['class'] = _attribute('class', array('image-box rounded-corner'));
             $image_args['size'] = 'large';
-            $column_class_args[] = 'rounded-corner';
             $column_grid_class = _attribute('class', $column_class_args);
             $html .= "<div class='row same-image-height $vertical_spacing $horizontal_spacing justify-content-center align-items-center'>";
      
