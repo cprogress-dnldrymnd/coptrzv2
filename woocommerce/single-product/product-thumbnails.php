437282512359872a2323de8d3468fdd2be2076e2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 16:54:39 +0800

    Update product-thumbnails.php

--- woocommerce/single-product/product-thumbnails.php
+++ woocommerce/single-product/product-thumbnails.php
@@ -36,7 +36,7 @@ foreach ($attachment_ids as $attachment_id) {
 	);
 	$key++;
 }
-
+var_dump($image_ids);
 $images_ids_per_slides = array_chunk($image_ids, 6);
 ?>
 <div class="product-thumb-holder">
@@ -53,6 +53,7 @@ $images_ids_per_slides = array_chunk($image_ids, 6);
 					echo '<div class="row g-4 w-100">';
 
 					foreach ($images_ids_per_slide as $image) {
+						$key = $image['key'];
 						echo "<div class='col-6' target='$key'>";
 						echo apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($image['id']), $post_thumbnail_id);
 						echo '</div>';
