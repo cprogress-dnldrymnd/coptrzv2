87a33033c30d3454ce4e00110e8dcad55a745d37
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 15:14:29 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1362,6 +1362,60 @@ function __section_fields($name = 'sections')
                                     'info-above-image'      => 'Info Above Image',
                                 )
                             ),
+                        Field::make('select', 'column_width', __('Column Width Desktop'))
+                            ->set_options(
+                                array(
+                                    'col-lg'     => 'Default',
+                                    'col-12'    => '100.00%',
+                                    'col-lg-11' => '91.67%',
+                                    'col-lg-10' => '83.33%',
+                                    'col-lg-9'  => '75.00%',
+                                    'col-lg-8'  => '67.00%',
+                                    'col-lg-7'  => '58.33%',
+                                    'col-lg-6'  => '50.00%',
+                                    'col-lg-5'  => '41.67%',
+                                    'col-lg-4'  => '33.33%',
+                                    'col-lg-3'  => '25.00%',
+                                    'col-lg-2'  => '16.67%',
+                                    'col-lg-1'  => '08.33%',
+                                )
+                            ),
+                        Field::make('select', 'column_width_tablet', __('Column Width Tablet'))
+                            ->set_options(
+                                array(
+                                    ''     => 'Default',
+                                    'col-md-12'    => '100.00%',
+                                    'col-md-11' => '91.67%',
+                                    'col-md-10' => '83.33%',
+                                    'col-md-9'  => '75.00%',
+                                    'col-md-8'  => '67.00%',
+                                    'col-md-7'  => '58.33%',
+                                    'col-md-6'  => '50.00%',
+                                    'col-md-5'  => '41.67%',
+                                    'col-md-4'  => '33.33%',
+                                    'col-md-3'  => '25.00%',
+                                    'col-md-2'  => '16.67%',
+                                    'col-md-1'  => '08.33%',
+                                )
+                            ),
+                        Field::make('select', 'column_width_mobile', __('Column Width Mobile'))
+                            ->set_options(
+                                array(
+                                    ''     => 'Default',
+                                    'col-12' => '100%',
+                                    'col-11' => '91.67%',
+                                    'col-10' => '83.33%',
+                                    'col-9'  => '75.00%',
+                                    'col-8'  => '67.00%',
+                                    'col-7'  => '58.33%',
+                                    'col-6'  => '50.00%',
+                                    'col-5'  => '41.67%',
+                                    'col-4'  => '33.33%',
+                                    'col-3'  => '25.00%',
+                                    'col-2'  => '16.67%',
+                                    'col-1'  => '08.33%',
+                                )
+                            ),
                         Field::make('complex', 'related_post', 'Related Posts')
                             ->add_fields('related_guides',  array(
                                 Field::make('hidden', 'post_type')->set_default_value('guides')->set_classes('d-none'),
