48f22b880183e9a2188ca6d045209bbd89743a7c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 20:12:20 +0800

    Update add-to-cart.php

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -25,47 +25,13 @@ $product_category_page = false;
 $col_class = 'col-sm-12';
 $class = 'mb-5';
 if (is_product_taxonomy()) {
-	$product_category_page = __get_product_taxonomy_page(get_queried_object()->term_id);
-	if ($product_category_page) {
-		$col_class = 'col-sm-6';
-		$class = '';
-	} else {
-		echo __description(array(
-			'description' => get_the_excerpt(),
-			'class' => _attribute('class', array('product-desc px-20px mb-4')),
-		));
-		echo '</div>';
-		
-	}
+	echo __description(array(
+		'description' => get_the_excerpt(),
+		'class' => _attribute('class', array('product-desc px-20px mb-4')),
+	));
+	echo '</div>';
 }
 ?>
-<div class="product-buttons <?= $class ?>">
-	<div class="row g-10px">
-		<?php if ($product_category_page) { ?>
-			<div class="<?= $col_class ?>">
-				<?php
-				echo apply_filters(
-					'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-					sprintf(
-						'<a href="%s" aria-describedby="woocommerce_loop_add_to_cart_link_describedby_%s" data-quantity="%s" class="%s" %s>%s</a>',
-						esc_url($product->add_to_cart_url()),
-						esc_attr($product->get_id()),
-						esc_attr(isset($args['quantity']) ? $args['quantity'] : 1),
-						esc_attr(isset($args['class']) ? $args['class'] : 'button'),
-						isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
-						esc_html($product->add_to_cart_text())
-					),
-					$product,
-					$args
-				);
-				?>
-				<span id="woocommerce_loop_add_to_cart_link_describedby_<?php echo esc_attr($product->get_id()); ?>" class="screen-reader-text">
-					<?php echo esc_html($args['aria-describedby_text']); ?>
-				</span>
-			</div>
-		<?php } ?>
-		<div class="<?= $col_class ?>">
-			<div class="button-box button-bordered"><a href="<?= get_the_permalink() ?>">Discover</a></div>
-		</div>
-	</div>
+<div class="product-buttons">
+	<div class="button-box button-bordered"><a href="<?= get_the_permalink() ?>">Discover</a></div>
 </div>
