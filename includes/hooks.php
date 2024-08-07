db82b7d1993d75637d827e6fb33cf4312cca7fd9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 17:56:44 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -242,11 +242,65 @@ function action__wp_footer()
             jQuery(document).ready(function() {
                 jQuery("<?= $layouts_global_val ?>").appendTo('#wp-admin-bar-layouts-menu');
             });
-            jQuery('#download-gvc').appendTo('.the-content > *:nth-child(2)');
         </script>
 
     <?php
     }
+    ?>
+    <script>
+        jQuery(document).ready(function() {
+            jQuery('#download-gvc').appendTo('.the-content > *:nth-child(2)');
+        });
+    </script>
+    <script>
+        // 2. This code loads the IFrame Player API code asynchronously.
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        // 3. This function creates an <iframe> (and YouTube player)
+        //    after the API code downloads.
+        var player;
+
+        function onYouTubeIframeAPIReady() {
+            player = new YT.Player('player', {
+                height: '390',
+                width: '640',
+                videoId: '9QVq1ZWgZjA',
+                playerVars: {
+                    'playsinline': 1
+                },
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            });
+        }
+
+        // 4. The API will call this function when the video player is ready.
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+
+        // 5. The API calls this function when the player's state changes.
+        //    The function indicates that when playing a video (state=1),
+        //    the player should play for six seconds and then stop.
+        var done = false;
+
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING && !done) {
+                setTimeout(stopVideo, 6000);
+                done = true;
+            }
+        }
+
+        function stopVideo() {
+            player.stopVideo();
+        }
+    </script>
+    <?php
 }
 
 add_action('wp_footer', 'action__wp_footer');
@@ -415,4 +469,3 @@ add_filter('wpcf7_form_tag_data_option', function ($data, $options, $args) {
 add_filter('woocommerce_gallery_thumbnail_size', function ($size) {
     return 'medium';
 });
-
