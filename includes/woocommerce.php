ad3babf1788caab13c42ffbc43c355412ec4bffd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 19:49:51 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -437,14 +437,21 @@ function __product_compare($id)
             $taxonomy_details = get_taxonomy($product_attribute);
             $product_attribute_values = get_the_terms($product['id'], $product_attribute);
 
-            $html .= "<div class='col-12'>";
+            $html .= "<div class='col-8'>";
             $html .= "<div class='inner inner-specs-list  h-100 d-flex align-items-center justify-content-center'>"; //inner
-            $html .= $taxonomy_details->labels->singular_name . ' | ' . $product_attribute_value->name;
-
+            $html .= $taxonomy_details->labels->singular_name . ' | ';
+            foreach ($product_attribute_values as $product_attribute_value) {
+                $html .= $product_attribute_value->name;
+            }
             $html .= "</div>";
             $html .= "</div>";
 
 
+            $html .= "<div class='col-4'>";
+            $html .= "<div class='inner inner-specs-list  h-100 d-flex align-items-center justify-content-center'>"; //inner
+        
+            $html .= "</div>";
+            $html .= "</div>";
         }
 
         $html .= "</div>";
