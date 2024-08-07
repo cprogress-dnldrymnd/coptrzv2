0e1c66e6791e5901b8bfcfd3573dcaeeafc3422e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 17:04:01 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -488,8 +488,8 @@ function __swipers() {
                     },
                 },
                 navigation: {
-                    nextEl: $id + ".swiper-button-next",
-                    prevEl: $id + ".swiper-button-prev",
+                    nextEl: $id + " .swiper-button-next",
+                    prevEl: $id + " .swiper-button-prev",
                 },
                 pagination: {
                     el: ".swiper-pagination",
@@ -517,8 +517,8 @@ function __swipers() {
                     },
                 },
                 navigation: {
-                    nextEl: $id + ".swiper-button-next",
-                    prevEl: $id + ".swiper-button-prev",
+                    nextEl: $id + " .swiper-button-next",
+                    prevEl: $id + " .swiper-button-prev",
                 },
                 pagination: {
                     el: ".swiper-pagination",
