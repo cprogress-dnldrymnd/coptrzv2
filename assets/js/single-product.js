6af7dd935df7765794c88be787fb1bcb1e4bde6b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 16:52:30 +0800

    cs

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -153,7 +153,6 @@ function __product_gallery() {
         slidesPerView: 1,
     });
 
-
     
 
 } 
--- woocommerce/single-product/product-thumbnails.php
+++ woocommerce/single-product/product-thumbnails.php
@@ -30,7 +30,10 @@ $attachment_ids = $product->get_gallery_image_ids();
 $image_ids[] = $post_thumbnail_id;
 $key = 0;
 foreach ($attachment_ids as $attachment_id) {
-	$image_ids[$key] = $attachment_id;
+	$image_ids[] = array(
+		'key' => $key,
+		'id' => $attachment_id
+	);
 	$key++;
 }
 
@@ -49,9 +52,10 @@ $images_ids_per_slides = array_chunk($image_ids, 6);
 					}
 					echo '<div class="row g-4 w-100">';
 
-					foreach ($images_ids_per_slide as $key => $id) {
+					foreach ($images_ids_per_slide as $image) {
+						$key = $image['key'];
 						echo "<div class='col-6' target='$key'>";
-						echo apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($id), $post_thumbnail_id);
+						echo apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($image['id']), $post_thumbnail_id);
 						echo '</div>';
 					}
 
