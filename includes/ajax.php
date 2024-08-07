3ede593dd727ec3b0c80605a1c2eb68c12e68e62
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 16:45:00 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -96,7 +96,6 @@ function brands_ajax()
 	$terms = get_terms(array(
 		'taxonomy'   => 'pa_brands',
 		'hide_empty' => false,
-		'number' => 100
 	));
 	$html = "<div class='row g-3 same-image-height' style='--object-fit: contain; --image-padding: 20%'>";
 	foreach ($terms as $term) {
