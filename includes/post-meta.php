c959888e4332ec6c7097b2b96493e98d46dbd324
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-07 15:53:39 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1208,7 +1208,7 @@ function __section_fields($name = 'sections')
                             'singular_name' => 'Section Item',
                         )
                     )
-                    ->set_collapsed(true)
+                    ->set_layout('tabbed-vertical')
                     ->add_fields('layouts', array(
                         Field::make('association', 'layouts', 'Select Layouts')
                             ->set_types(
