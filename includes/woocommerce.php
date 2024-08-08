f3717e70af95732a55882b3610a78972b761a40b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-08 14:07:37 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -686,9 +686,9 @@ function _product_grid_display($id)
         $status = get_post_status($id);
 
 
-        $html = "<ul class='products custom-product-grid h-100 m-0 p-0'>";
-        $html .= "<li class='product m-0 p-0 w-100 h-100 post-$id $stock_status'>";
-        $html .= "<div class='product-inner p-20px rounded-10px border-default h-100 bg-white'>";
+        $html = "<ul class='products d-inline custom-product-grid h-100 m-0 p-0'>";
+        $html .= "<li class='product d-inline m-0 p-0 w-100 h-100 post-$id $stock_status'>";
+        $html .= "<div class='product-inner h-100 p-20px rounded-10px border-default h-100 bg-white'>";
         $html .= "<a href='$permalink' class='woocommerce-LoopProduct-link woocommerce-loop-product__link'>";
         $html .= "<div class='wc-img-wrapper'>";
         $html .= "<img width='300' height='225' src='$post_thumnail' class='attachment-woocommerce_thumbnail size-woocommerce_thumbnail' alt='$title' decoding='async'>";
