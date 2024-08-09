be413892d0f7261751a4106fb1df6d29c598076d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-09 20:22:37 +0800

    Update header-right.php

--- template-parts/header/header-right.php
+++ template-parts/header/header-right.php
@@ -3,30 +3,16 @@ $SVG = new SVG;
 ?>
 <div class="col-auto d-flex align-items-center">
     <div class="row g-4 header-right">
-        <div class="col-auto d-flex align-items-center wc-components">
-            <a href="<?= get_permalink(get_option('woocommerce_myaccount_page_id')); ?>" class="header-icon account-icon text-white d-flex align-items-center">
-                <?= $SVG->user(); ?>
-            </a>
-        </div>
-        <div class="col-auto d-flex align-items-center wc-components">
-            <div class="mini-cart-wrapper">
-                <a href="#" id="mini-cart-button" class="header-icon cart-icon text-white d-flex align-items-center">
-                    <?= $SVG->cart(); ?>
-                    <div class="cart-number">
-                        <?= WC()->cart->get_cart_contents_count(); ?>
-                    </div>
-                </a>
-                <div class="mini-cart-holder bg-white rounded-10px mt-10px">
-                    <?php woocommerce_mini_cart() ?>
-                </div>
-            </div>
-        </div>
+   
         <div class="col-auto d-flex align-items-center d-lg-none ">
             <button class="menu-burger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvasMenu" aria-controls="offCanvasMenu">
                 <div class="icon">
                     <div class="menu"></div>
                 </div>
             </button>
+        </div>
+        <div class="col-auto">
+            <?= do_shortcode('[wpml_language_selector_widget]') ?>
         </div>
 
         <?php
