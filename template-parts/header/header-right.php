94608be48afc7f5b005b56b9f18c0fde0422f443
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-08-15 16:19:11 +0800

    Update header-right.php

--- template-parts/header/header-right.php
+++ template-parts/header/header-right.php
@@ -6,7 +6,19 @@ $SVG = new SVG;
         <div class="col-auto d-flex align-items-center">
             <?= do_shortcode('[wpml_language_selector_widget]') ?>
         </div>
-
+        <div class="col-auto d-flex align-items-center">
+            <div class="mini-cart-wrapper">
+                <a href="#" id="mini-cart-button" class="header-icon cart-icon text-white d-flex align-items-center">
+                    <?= $SVG->cart(); ?>
+                    <div class="cart-number">
+                        <?= WC()->cart->get_cart_contents_count(); ?>
+                    </div>
+                </a>
+                <div class="mini-cart-holder bg-white rounded-10px mt-10px">
+                    <?php woocommerce_mini_cart() ?>
+                </div>
+            </div>
+        </div>
         <div class="col-auto d-flex align-items-center d-lg-none ">
             <button class="menu-burger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvasMenu" aria-controls="offCanvasMenu">
                 <div class="icon">
