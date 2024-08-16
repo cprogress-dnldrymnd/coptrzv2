4de850c9e59756b044ad6d4f82b87e88730f3903
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-16 17:47:09 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1163,7 +1163,7 @@ function __hero_form_fields()
 }
 add_filter('carbon_fields_association_field_options_hero_form_post_wpcf7_contact_form', function ($query_arguments) {
 
-    $query_arguments['post_type'] = 'guides';
+    $query_arguments['post_type'] = 'wpcf7_contact_form';
     $query_arguments['post_status'] = array('publish', 'private');
 
     return $query_arguments;
