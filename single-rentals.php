f47a943c5373d959eca6b715e8f2ee10c4c81eb1
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-19 16:06:16 +0800

    Update single-rentals.php

--- single-rentals.php
+++ single-rentals.php
@@ -1,3 +1,6 @@
 <?php get_header() ?>
-<?= do_shortcode( '[layouts id=292999]' ) ?>
+<?php
+echo ___hero_modules('text-start', 'small-hero');
+?>
+<?= do_shortcode('[layouts id=292999]') ?>
 <?php get_footer() ?>
