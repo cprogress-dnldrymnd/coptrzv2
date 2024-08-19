93733c0e49a8124b3e49edf5b4a0537817cf5564
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-19 18:09:32 +0800

    Update single-rentals.php

--- single-rentals.php
+++ single-rentals.php
@@ -12,8 +12,8 @@ foreach ($attachment_ids as $attachment_id) {
 $images_ids_per_slides = array_chunk($image_ids, 6);
 
 echo ___hero_modules('text-start', 'small-hero');
-echo do_shortcode(___sections('sections', get_the_ID()));
 echo do_shortcode(get__post_meta('shortcode'));
+echo do_shortcode(___sections('sections', get_the_ID()));
 ?>
 
 <?= do_shortcode('[layouts id=292999]') ?>
