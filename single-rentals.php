0c1ef0b0e9d306526c9367f7947eff33a7fd1e13
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-19 18:14:44 +0800

    Update single-rentals.php

--- single-rentals.php
+++ single-rentals.php
@@ -1,11 +1,12 @@
 <?php get_header() ?>
 <?php
+$data_id = get__post_meta('data_id');
 echo ___hero_modules('text-start', 'small-hero');
 ?>
 
 <section class="booqable-rental md-padding">
   <div class="container">
-    <?= do_shortcode(get__post_meta('shortcode')); ?>
+    <?= do_shortcode('[booqable_detail id="' . $data_id . '"]'); ?>
   </div>
 </section>
 
