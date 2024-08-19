1fc414925748ce6590d7c9db0bc1fbb58d9f4901
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-19 17:45:38 +0800

    Update single-rentals.php

--- single-rentals.php
+++ single-rentals.php
@@ -1,9 +1,7 @@
 <?php get_header() ?>
-<?php 
-$attachment_ids = get__post_meta('gallery');
-?>
-
 <?php
+$attachment_ids = get__post_meta('gallery');
+$images_ids_per_slides = array_chunk($attachment_ids, 6);
 echo ___hero_modules('text-start', 'small-hero');
 echo do_shortcode(___sections('sections', get_the_ID()));
 ?>
@@ -32,6 +30,46 @@ echo do_shortcode(___sections('sections', get_the_ID()));
                   }
                   ?>
                 </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <div class="product-thumb-holder">
+              <div class="swiper product-thumb">
+                <div class="swiper-wrapper">
+                  <?php
+                  if ($image_ids) {
+                    $key = 1;
+
+                    foreach ($images_ids_per_slides as $images_ids_per_slide) {
+                      if (count($image_ids) > 6) {
+                        echo '<div class="swiper-slide">';
+                      }
+                      echo '<div class="row g-4 w-100">';
+
+                      foreach ($images_ids_per_slide as $image) {
+                        $key = $image['key'];
+                        echo "<div class='col-6 thumb-nav' target='$key'>";
+                        echo apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($image['id']), $post_thumbnail_id);
+                        echo '</div>';
+                      }
+
+
+                      echo '</div>';
+                      if (count($image_ids) > 6) {
+                        echo '</div>';
+                      }
+                    }
+                  }
+                  ?>
+
+                </div>
+                <?php if (count($attachment_ids) > 6) { ?>
+                  <div class="swiper-nav d-inline-flex">
+                    <div class="swiper-button-prev swiper-button-prev-thumb"></div>
+                    <div class="swiper-button-next swiper-button-next-thumb"></div>
+                  </div>
+                <?php } ?>
               </div>
             </div>
           </div>
