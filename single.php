d463cef2eca780f9697438483ab2ad5efbbbb3ae
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-28 18:03:15 +0800

    cs

--- single.php
+++ single.php
@@ -1,9 +1,8 @@
 <?php get_header(); ?>
 <?php
 if (false === get_template_part('template-parts/single/single', get_post_type())) {
-    //echo ___hero_modules('text-start', 'small-hero');
-    //echo do_shortcode(___sections('sections', get_the_ID()));
-    the_content();
+    echo ___hero_modules('text-start', 'small-hero');
+    echo do_shortcode(___sections('sections', get_the_ID()));
 }
 ?>
 <?php get_footer(); ?>
--- templates/page-modules.php
+++ templates/page-modules.php
@@ -7,8 +7,7 @@
 <?php get_header(); ?>
 <div class="modules">
     <?php
-    echo ___hero_modules();
-    echo do_shortcode(___sections('sections', get_the_ID()));
+    the_content();
     ?>
 </div>
 
