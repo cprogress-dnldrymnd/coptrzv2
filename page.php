ed92f5a7d3ed9e2f1f26010319666c5257897bc5
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-28 18:31:52 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,16 +1,8 @@
 <?php get_header(); ?>
 
-<?= do_shortcode(___hero_modules()) ?>
-
-<?php
-$container_width = get__post_meta('container_width');
-?>
-<section class="default-page <?= $container_width ?> md-padding-top md-padding-bottom no-overflow">
-    <div class="container">
-        <?php
-        the_content();
-        ?>
-    </div>
-</section>
-
+<div class="modules default-page <?= $container_width ?>">
+    <?php
+    the_content();
+    ?>
+</div>
 <?php get_footer(); ?>
