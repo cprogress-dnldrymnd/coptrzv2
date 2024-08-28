bced220edb14e933fae16924d94ace4b23ad9d52
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-28 20:06:47 +0800

    cs

--- template-parts/single/single-capabilities.php
+++ template-parts/single/single-capabilities.php
@@ -1,30 +1,8 @@
 <?php
-$related_products_heading = get__post_meta('related_products_heading');
-$related_products = get__post_meta('related_products');
-$related_casestudies_heading = get__post_meta('related_casestudies_heading');
-$related_casestudies = get__post_meta('related_casestudies');
 
 
 echo ___hero_modules();
-echo do_shortcode(___sections('sections', get_the_ID()));
+the_content();
 
-if ($related_products) {
-    $related_products_array = array();
-    foreach ($related_products as $related_product) {
-        $related_products_array[] = $related_product['id'];
-    }
-    echo __linked_products($related_products_array, false, false, false, $related_products_heading, false, true, false, 'Related-Products');
-}
-
-if ($related_casestudies) {
-    $data = array(
-        'col' => false,
-        'featured' => false,
-        'taxonomy' => 'casestudies_category',
-        'style' => 'style-1',
-        'elements' => array('image', 'category', 'title', 'excerpt', 'button'),
-    );
-    echo do_shortcode(__related_posts($related_casestudies, $data, $related_casestudies_heading, 'Case-Studies'));
-}
 
 echo do_shortcode(___sections('sections_after_main', get_the_ID()));
