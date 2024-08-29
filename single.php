2b25087b013a293a4a79f041a4dd7bb69c61ed93
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-29 20:26:55 +0800

    Update single.php

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 <?php
 if (false === get_template_part('template-parts/single/single', get_post_type())) {
     echo ___hero_modules('text-start', 'small-hero');
-    the_content();
+    echo do_shortcode(get_post_meta(get_the_ID(), '_sections_html', true));
 }
 ?>
 <?php get_footer(); ?>
