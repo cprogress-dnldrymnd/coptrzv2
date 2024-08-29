35522b86974c0c5f47c730ed4f3a51c9803ce2ff
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-29 14:01:55 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -23,6 +23,12 @@ function action_module_content()
                 if ($sections_after_main) {
                     update_post_meta(get_the_ID(), '_sections_after_main_html', $sections_after_main);
                 }
+
+                $my_post = array(
+                    'ID'           => get_the_ID(),
+                    'post_content' => '',
+                );
+                wp_update_post($my_post);
             }
         }
 
