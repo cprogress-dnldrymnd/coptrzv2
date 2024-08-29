f2b3d0e1bde8d49c6cc5e77de8a027d72c74501b
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-29 20:17:04 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -109,9 +109,7 @@ function enqueue_scripts()
 	}
 
 	if (is_checkout()) {
-		wp_enqueue_style('intl-tel', 'https://cdn.jsdelivr.net/npm/intl-tel-input@21.2.7/build/css/intlTelInput.css', NULL, coptz_version);
 		wp_enqueue_style('checkout-style', assets_dir . 'scss/checkout/checkout.css', NULL, coptz_version);
-		wp_enqueue_script('intl-tel', 'https://cdn.jsdelivr.net/npm/intl-tel-input@21.2.7/build/js/intlTelInput.min.js', NULL, coptz_version);
 		wp_register_script('checkout-js', assets_dir . 'js/checkout.js', ['jquery'], coptz_version);
 
 		$countries_obj = new WC_Countries();
