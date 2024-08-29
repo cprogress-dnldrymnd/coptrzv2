8578f97fe54e4fcab968454d973b7bb5b17412d4
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-08-29 17:28:43 +0800

    Update single-product.js

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -139,6 +139,58 @@ function __product_gallery() {
     var product_thumb = new Swiper('.product-thumb', {
         loop: true,
         autoplay: false,
+        spaceBetween: 10,
+        breakpoints: {
+            0: {
+                slidesPerView: 3,
+            },
+
+            768: {
+                slidesPerView: 4,
+            },
+
+
+            992: {
+                slidesPerView: 5,
+            },
+
+            1200: {
+                slidesPerView: 6,
+            },
+
+        },
+    });
+
+    var product_main_image = new Swiper('.product-main-image', {
+        loop: true,
+        autoplay: false,
+        slidesPerView: 1,
+        thumbs: {
+            swiper: product_thumb,
+        },
+    });
+
+} 
+/*without add to cart
+function __product_variation() {
+
+    jQuery('input[name="variation-radio"]').change(function (e) {
+        $value = jQuery(this).val();
+        jQuery('input[name="variation_id"]').val($value);
+
+        setTimeout(function () {
+            jQuery('.single_add_to_cart_button').removeClass('disabled wc-variation-selection-needed');
+        }, 500);
+
+        e.preventDefault();
+    });
+}
+
+function __product_gallery() {
+
+    var product_thumb = new Swiper('.product-thumb', {
+        loop: true,
+        autoplay: false,
         spaceBetween: 0,
         slidesPerView: 1,
         navigation: {
@@ -160,4 +212,4 @@ function __product_gallery() {
     });
 
 
-} 
+} */
