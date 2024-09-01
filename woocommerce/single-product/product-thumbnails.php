a14fb0882351f32b666373828db413ebf5ebce42
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-01 16:14:22 +0800

    Update product-thumbnails.php

--- woocommerce/single-product/product-thumbnails.php
+++ woocommerce/single-product/product-thumbnails.php
@@ -20,30 +20,65 @@ defined('ABSPATH') || exit;
 
 // Note: `wc_get_gallery_image_html` was added in WC 3.3.2 and did not exist prior. This check protects against theme overrides being used on older versions of WC.
 if (!function_exists('wc_get_gallery_image_html')) {
-  return;
+	return;
 }
 
 global $product;
 $post_thumbnail_id = $product->get_image_id();
 $attachment_ids = $product->get_gallery_image_ids();
+
+$image_ids[] = array(
+	'key' => 0,
+	'id' => $post_thumbnail_id
+);
+$key = 1;
+foreach ($attachment_ids as $attachment_id) {
+	$image_ids[] = array(
+		'key' => $key,
+		'id' => $attachment_id
+	);
+	$key++;
+}
+$images_ids_per_slides = array_chunk($image_ids, 6);
+if (count($image_ids) > 1) {
 ?>
-<div class="product-thumb-holder mt-20px">
-  <div thumbsSlider class="swiper product-thumb">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide">
-        <?= apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($post_thumbnail_id), $post_thumbnail_id); ?>
-      </div>
-      <?php
-      if ($attachment_ids && $product->get_image_id()) {
-        foreach ($attachment_ids as $attachment_id) {
-          ?>
-              <div class="swiper-slide">
-                <?= apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($attachment_id), $attachment_id); ?>
-              </div>
-          <?php
-        }
-      }
-      ?>
-    </div>
-  </div>
-</div>
+	<div class="product-thumb-holder">
+		<div class="swiper product-thumb">
+			<div class="swiper-wrapper">
+				<?php
+				if ($image_ids) {
+					$key = 1;
+
+					foreach ($images_ids_per_slides as  $images_ids_per_slide) {
+						if (count($image_ids) > 6) {
+							echo '<div class="swiper-slide">';
+						}
+						echo '<div class="row g-4 w-100">';
+
+						foreach ($images_ids_per_slide as $image) {
+							$key = $image['key'];
+							echo "<div class='col-6 thumb-nav' target='$key'>";
+							echo apply_filters('woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html($image['id']), $post_thumbnail_id);
+							echo '</div>';
+						}
+
+
+						echo '</div>';
+						if (count($image_ids) > 6) {
+							echo '</div>';
+						}
+					}
+				}
+				?>
+
+			</div>
+			<?php if (count($image_ids) > 6) { ?>
+				<div class="swiper-nav d-inline-flex">
+					<div class="swiper-button-prev swiper-button-prev-thumb"></div>
+					<div class="swiper-button-next swiper-button-next-thumb"></div>
+				</div>
+			<?php } ?>
+		</div>
+		>
+	</div>
+<?php } ?>
