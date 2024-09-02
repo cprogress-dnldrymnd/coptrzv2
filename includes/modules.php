79172bdd2b261d70f2e120dc201dfbcb76eef82b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-02 17:23:45 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -1920,7 +1920,7 @@ function ____columns_modules($items, $id, $html = '')
         if ($align_items) {
             $row_class[] = $align_items;
         } else {
-            if ($same_image_height = '') {
+            if ($same_image_height == '') {
                 $row_class[] = 'align-items-center';
             }
         }
