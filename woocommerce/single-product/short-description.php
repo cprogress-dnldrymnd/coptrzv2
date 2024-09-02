e5a761156a5572016f80f836ff8379e3af2456fc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-02 15:45:55 +0800

    cs

---
+++ woocommerce/single-product/short-description.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Single product short description
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/short-description.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.3.0
+ */
+
+if (! defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
+
+global $post;
+
+$short_description = apply_filters('woocommerce_short_description', $post->post_excerpt);
+
+if (! $short_description) {
+	return;
+}
+
+?>
+<div class="woocommerce-product-details__short-description">
+	<?php echo $short_description; // WPCS: XSS ok. 
+	?>
+</div>
+
+<div class="after-add-to-cart w-100">
+	<div class="icon-list">
+		<ul class="list-inline d-flex">
+			<li class="d-flex align-items-center">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+					<path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"></path>
+				</svg>
+				<span>Call Us Now On <a href="tel:+443301654996">0330 165 4996</a></span>
+			</li>
+		</ul>
+	</div>
+</div>
