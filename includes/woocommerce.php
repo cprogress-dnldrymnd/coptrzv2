99a75588b4c148cca406077f044099f2bfdb07f8
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-02 16:49:25 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -408,7 +408,8 @@ function request_info()
 {
     $id = apply_filters('wpml_object_id', 299743, 'post');
     $html = '<div class="button-box button-bordered request-info">';
-    $html .= "<a class='rounded-10px ' data-bs-toggle='modal' data-bs-target='#modal-$id' target='_self'>Request Info</button>";
+    $html .= "<a class='rounded-10px ' data-bs-toggle='modal' data-bs-target='#modal-$id' target='_self'>";
+    $html .= __('Request Info','coptrz');
     $html .= '</a>';
     $html .= '</div>';
     echo $html;
