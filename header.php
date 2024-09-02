2cac032f9a351b09212e8b9a32a22fe7cc96e388
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-02 09:59:41 +0800

    Update header.php

--- header.php
+++ header.php
@@ -60,7 +60,9 @@
 		$string = "";
 		$args = "opensbquotenumberpostsquote:-1,quotepost_typequote:quoteproductquote,quotefieldsquote:quoteidsquote,quotepost_statusquote:quotepublishquote,quotetax_queryquote:opensbquoterelationquote:quoteANDquote,quote0quote:opensbquotetaxonomyquote:quoteproduct_catquote,quotefieldquote:quoteterm_idquote,quotetermsquote:openbquote32quoteclosebclosesbclosesbclosesb";
 		$product_slider_args_normal = product_slider_args_normal($args);
+		echo '<pre>';
 		var_dump(json_decode($product_slider_args_normal));
+		echo '</pre>';
 		?>
 
 		<?php
