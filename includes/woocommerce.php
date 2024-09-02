22c9c1d06c60c128f0d378ad6c7d4f5613a4ff74
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-02 15:34:18 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -406,9 +406,7 @@ function bbloomer_display_quantity_plus()
 function request_info()
 {
     $html = '<div class="button-box button-bordered buy-now-button ">';
-    $html .= "<button class='rounded-10px ' data-bs-toggle='modal' data-bs-target='#modal-299743' target='_self'>Enquire Now</button>";
-    $html .= 'Request Info';
-    $html .= '</button>';
+    $html .= "<button class='rounded-10px ' data-bs-toggle='modal' data-bs-target='#modal-299743' target='_self'>Request Info</button>";
     $html .= '</div>';
     echo $html;
 }
