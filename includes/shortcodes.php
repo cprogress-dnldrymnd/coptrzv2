cca07136d24c27ee85085558b9b19f6e0e8038de
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-02 09:54:45 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -712,7 +712,7 @@ class Shortcodes
         );
         $product_slider_args_normal = product_slider_args_normal($args);
         $args_val = json_decode($product_slider_args_normal);
-        return var_dump($args_val);
+        return $args_val;
     }
 }
 $Shortcodes = new Shortcodes;
