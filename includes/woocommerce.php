8d5123f700ed4bc47ab27343f9dfc0fc8fdce408
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-02 16:08:20 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -406,7 +406,7 @@ function bbloomer_display_quantity_plus()
 
 function request_info()
 {
-    $id = apply_filters('wpml_object_id', 268179, 'post');
+    $id = apply_filters('wpml_object_id', 299743, 'post');
     $html = '<div class="button-box button-bordered request-info">';
     $html .= "<a class='rounded-10px ' data-bs-toggle='modal' data-bs-target='#modal-$id' target='_self'>Request Info</button>";
     $html .= '</a>';
