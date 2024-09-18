254972624209df4ced96b4f88632998cf0da9423
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-18 22:38:24 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -747,9 +747,12 @@ function ___sections($id = 'sections', $post_id = '')
                             $layout_id = $layout['id'];
                             $layouts_global[] = $layout['id'];
                             $html .= "[layouts id='$layout_id']";
-                            $layouts_arr[] = get_post_meta($post_id, '_layouts', true);
+
+                            $post_id_val = apply_filters('wpml_object_id', $post_id, 'post');
+
+                            $layouts_arr[] = get_post_meta($post_id_val, '_layouts', true);
                             array_push($layouts_arr, $layout_id);
-                            update_post_meta($post_id, '_layouts', $layouts_arr);
+                            update_post_meta($post_id_val, '_layouts', $layouts_arr);
                         }
                         break;
                     case 'heading':
