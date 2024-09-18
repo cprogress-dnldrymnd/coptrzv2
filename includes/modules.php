bf09c7216273b83071905e18a5d3ba6f25644df3
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-09-18 22:48:33 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -738,7 +738,7 @@ function ___sections($id = 'sections', $post_id = '')
                     $html .= __background($background);
                 }
             }
-            $layouts_arr = get_post_meta($post_id, '_layouts', true);
+            $layouts_arr[] = get_post_meta($post_id, '_layouts', true);
             foreach ($section_items as $key => $items) {
                 $type = $items['_type'];
                 switch ($type) {
@@ -748,7 +748,7 @@ function ___sections($id = 'sections', $post_id = '')
                             $layout_id = $layout['id'];
                             $layouts_global[] = $layout['id'];
                             $html .= "[layouts id='$layout_id']";
-                            array_push($layouts_arr, $layout_id);
+                            array_merge($layouts_arr, array($layout_id));
                             update_post_meta($post_id, '_layouts', $layouts_arr);
                         }
                         break;
