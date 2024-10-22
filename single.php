fe54712087105ed361993e2a9709ac359352cdb9
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-22 21:59:39 +0800

    Update single.php

--- single.php
+++ single.php
@@ -1,8 +1,22 @@
-<?php get_header(); ?>
+<?php
+$hide_on_list = get__post_meta('hide_on_list');
+if ($hide_on_list) {
+    get_header('landing-v2');
+} else {
+    get_header();
+}
+?>
 <?php
 if (false === get_template_part('template-parts/single/single', get_post_type())) {
     echo ___hero_modules('text-start', 'small-hero');
     echo do_shortcode(get_post_meta(get_the_ID(), '_sections_html', true));
 }
 ?>
-<?php get_footer(); ?>
+
+<?php
+if ($hide_on_list) {
+    get_footer('landing');
+} else {
+    get_footer();
+}
+?>
