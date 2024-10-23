14f975c5e8e2a3c419fa76ff2a87eb2eadc97b2c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-23 19:04:50 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -91,10 +91,10 @@ function action_admin_enqueue_scripts($hook)
     $selector['casestudies'] = $select_casestudies;
 
 
-    wp_enqueue_style('admin', get_template_directory_uri() . '/admin/css/admin.css');
-    wp_register_script('admin', get_template_directory_uri() . '/admin/js/admin.js');
-    wp_localize_script('admin', 'selector', $selector);
-    wp_enqueue_script('admin');
+    wp_enqueue_style('admin_js', get_template_directory_uri() . '/admin/css/admin.css');
+    wp_register_script('admin_js', get_template_directory_uri() . '/admin/js/admin.js');
+    wp_localize_script('admin_js', 'selector', $selector);
+    wp_enqueue_script('admin_js');
 }
 add_action('admin_enqueue_scripts', 'action_admin_enqueue_scripts');
 /*-----------------------------------------------------------------------------------*/
