992bc618af2d01dddd881fcff2233e9499641053
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-23 19:01:05 +0800

    Update admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -3,104 +3,26 @@ jQuery(document).ready(function ($) {
     button_selector();
 });
 function codemirror() {
+  
+
     setTimeout(function () {
-        if (typeof tinymce !== 'undefined') {
-            // Loop through each textarea
-            var textareaId = 'wysiwyg-editor-field';
-            // TinyMCE settings to mimic classic editor
-            tinymce.init({
-                selector: '#' + textareaId,  // Use the unique ID
-                plugins: 'lists link charmap paste textcolor',
-                toolbar: 'formatselect | bold italic | bullist numlist | link | forecolor | charmap | pastetext | removeformat',
-                block_formats: 'Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Preformatted=pre',
-                toolbar_location: 'top',
-                menubar: false,
-                statusbar: false,
-                branding: false,
-                height: 300
-            });
+  
 
-
-
-        } else {
-            console.error('TinyMCE is not loaded.');
+        if (jQuery('textarea[name="carbon_fields_compact_input[_header_scripts]"').length > 0) {
+            wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_header_scripts]"'), cm_settings.ce_html);
         }
-        var $target;
 
-        jQuery(document).on("click", '.wysiwyg-editor-trigger', function (event) {
-            jQuery('#wysiwyg-editor').addClass('active');
-            $target = jQuery(this).parent().parent().parent().prev().find('textarea');
-            tinymce.get(textareaId).setContent($target.val());
-        });
-
-        jQuery(document).on("click", '.close-wysiwyg-trigger', function (event) {
-            jQuery('#wysiwyg-editor').removeClass('active');
-        });
-
-
-        jQuery(document).on("click", '.submit-wysiwyg-trigger', function (event) {
-            jQuery('#wysiwyg-editor').removeClass('active');
-            $target.val(tinymce.get(textareaId).getContent());
-        });
+        if (jQuery('textarea[name="carbon_fields_compact_input[_footer_scripts]"').length > 0) {
+            wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_footer_scripts]"'), cm_settings.ce_html);
+        }
 
 
 
-    }, 1000);
+        if (jQuery('textarea[name="carbon_fields_compact_input[_body_scripts]"').length > 0) {
+            wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_body_scripts]"'), cm_settings.ce_html);
+        }
 
-
-    function codemirror() {
-        setTimeout(function () {
-            if (jQuery('textarea[name="carbon_fields_compact_input[_page_custom_css]"').length > 0) {
-                wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_page_custom_css]"'), cm_settings.ce_css);
-                wp.data.subscribe(function () {
-                    // Obtain the CodeMirror instance
-                    var cm = jQuery('textarea[name="carbon_fields_compact_input[_page_custom_css]"').next('.CodeMirror').get(0).CodeMirror;
-                    cm.save(); // copy the content of the editor into the textarea.
-                });
-            }
-            if (jQuery('textarea[name="carbon_fields_compact_input[_page_header_scripts]"').length > 0) {
-                wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_page_header_scripts]"'), cm_settings.ce_html);
-                wp.data.subscribe(function () {
-                    // Obtain the CodeMirror instance
-                    var cm = jQuery('textarea[name="carbon_fields_compact_input[_page_header_scripts]"').next('.CodeMirror').get(0).CodeMirror;
-                    cm.save(); // copy the content of the editor into the textarea.
-                });
-            }
-    
-            if (jQuery('textarea[name="carbon_fields_compact_input[_page_footer_scripts]"').length > 0) {
-                wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_page_footer_scripts]"'), cm_settings.ce_html);
-                wp.data.subscribe(function () {
-                    // Obtain the CodeMirror instance
-                    var cm = jQuery('textarea[name="carbon_fields_compact_input[_page_footer_scripts]"').next('.CodeMirror').get(0).CodeMirror;
-                    cm.save(); // copy the content of the editor into the textarea.
-                });
-            }
-    
-            if (jQuery('textarea[name="carbon_fields_compact_input[_page_body_scripts]"').length > 0) {
-                wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_page_body_scripts]"'), cm_settings.ce_html);
-                wp.data.subscribe(function () {
-                    // Obtain the CodeMirror instance
-                    var cm = jQuery('textarea[name="carbon_fields_compact_input[_page_body_scripts]"').next('.CodeMirror').get(0).CodeMirror;
-                    cm.save(); // copy the content of the editor into the textarea.
-                });
-            }
-    
-            if (jQuery('textarea[name="carbon_fields_compact_input[_header_scripts]"').length > 0) {
-                wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_header_scripts]"'), cm_settings.ce_html);
-            }
-    
-            if (jQuery('textarea[name="carbon_fields_compact_input[_footer_scripts]"').length > 0) {
-                wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_footer_scripts]"'), cm_settings.ce_html);
-            }
-    
-    
-    
-            if (jQuery('textarea[name="carbon_fields_compact_input[_body_scripts]"').length > 0) {
-                wp.codeEditor.initialize(jQuery('textarea[name="carbon_fields_compact_input[_body_scripts]"'), cm_settings.ce_html);
-            }
-    
-        }, 500);
-    }
+    }, 500);
 
 }
 
