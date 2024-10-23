1bc30ab832e76ed0a8295b11046ca58a56a13206
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-23 19:22:18 +0800

    Update content-single-product.php

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -36,12 +36,10 @@ global $product;
 
 $pa_brands = $product->get_attribute('pa_brands');
 $category = get_the_terms($product_id, 'product_cat');
-
-
-$data['sku']      = $product->get_sku();
 if ($product->get_price()) {
 	$data['price']    = $product->get_price();
 }
+$data['sku']      = $product->get_sku();
 $data['name']     = $product->get_name();
 $data['brand']    = $pa_brands;
 $data['category'] = $category[0]->name;
