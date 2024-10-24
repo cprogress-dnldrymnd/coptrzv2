f28f26a72c0b01af23652c7e9f09f8156e684300
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-24 16:08:26 +0800

    Update short-description.php

--- woocommerce/single-product/short-description.php
+++ woocommerce/single-product/short-description.php
@@ -46,10 +46,3 @@ if (! $short_description) {
 		</ul>
 	</div>
 </div>
-
-<?php 
-global $product;
-
-if(!$product->get_price() || $product->get_stock_status() == 'outofstock') {
-	request_info();
-}
