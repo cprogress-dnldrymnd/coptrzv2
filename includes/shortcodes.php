859dfdceee10e019bf0caaf4015404fba2b9ae17
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-29 16:24:21 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -800,6 +800,7 @@ class Shortcodes
                                      * @hooked woocommerce_template_single_sharing - 50
                                      * @hooked WC_Structured_Data::generate_product_data() - 60
                                      */
+                                    WC_Structured_Data::generate_product_data();
                                     do_action('woocommerce_single_product_summary');
                                     ?>
                                 </div>
