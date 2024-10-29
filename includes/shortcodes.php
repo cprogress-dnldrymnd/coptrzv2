b0accd3e424ead1271e168979efb0229b8a7c1fd
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-29 16:12:11 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -869,3 +869,4 @@ add_shortcode('permalink', array($Shortcodes, 'permalink'));
 add_shortcode('product_slider', array($Shortcodes, 'product_slider'));
 add_shortcode('post_id', array($Shortcodes, 'post_id'));
 add_shortcode('rentals_script', array($Shortcodes, 'rentals_script'));
+add_shortcode('product_add_to_cart', array($Shortcodes, 'product_add_to_cart'));
