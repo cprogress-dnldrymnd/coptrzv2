4e440c07a34418452f06059140eef0d2c3029b38
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-29 18:16:19 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -3240,6 +3240,7 @@ function __product_add_to_cart($id)
         'featured_image' => $id
     ));
 
+    $html .= do_shortcode("[add_to_cart id='$id']");
     $html .= "</div>";
 
     return $html;
