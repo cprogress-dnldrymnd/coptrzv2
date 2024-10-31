759cfb4ba64b89fe49cb7dbf12fe45c4d420dfff
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-31 22:30:29 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -987,7 +987,6 @@ function custom_product_variation_training($product_id, $delivery_method = 'onli
 
                 $html .= "<div class='button-box button-bordered mt-3'>";
 
-                echo $post_type_key;
 
                 if ($post_type_key == 'product') {
                     $html .= "<a href='?add-to-cart=$product_id' data-quantity='1' class='product-btn button product_type_simple add_to_cart_button ajax_add_to_cart' data-product_id='$product_id' data-product_sku='$sku' rel='nofollow'><span class='product-data d-none'>$data_encode</span> Add to basket</a>";
