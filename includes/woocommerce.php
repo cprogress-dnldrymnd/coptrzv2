814bf86392763efa14b00a57a55459b1f120c069
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-31 22:24:52 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -869,7 +869,7 @@ function date_compare_oldest($element1, $element2)
 
 
 
-function custom_product_variation_training($product_id, $delivery_method = 'online-self-paced', $location = false, $post_type = 'product')
+function custom_product_variation_training($product_id, $delivery_method = 'online-self-paced', $location = false, $post_type)
 {
     $SVG = new SVG;
     $product = wc_get_product($product_id);
