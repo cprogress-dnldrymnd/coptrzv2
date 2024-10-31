0f560bea150516fbcdc9df43988e0cef027ed8cb
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-31 22:18:00 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -988,6 +988,8 @@ function custom_product_variation_training($product_id, $delivery_method = 'onli
                 $html .= "<div class='button-box button-bordered mt-3'>";
 
                 if ($post_type == 'product') {
+    echo $post_type;
+
                     $html .= "<a href='?add-to-cart=$product_id' data-quantity='1' class='product-btn button product_type_simple add_to_cart_button ajax_add_to_cart' data-product_id='$product_id' data-product_sku='$sku' rel='nofollow'><span class='product-data d-none'>$data_encode</span> Add to basket</a>";
                 } else {
                     $basket_url =   wc_get_cart_url();
