8fa72e09243f21ab1e3ade1b2d7c94047ea3a792
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-10-31 22:29:53 +0800

    Update single-product.js

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -56,7 +56,6 @@ function __training_ajax($delivery_method = false) {
         }
         $location = jQuery('select[name="location"]').val();
 
-        console.log($post_type_key);
 
         if ($delivery_method_val == 'classroom') {
             jQuery('.col-location').removeClass('d-none');
