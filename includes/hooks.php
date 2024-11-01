787e6d50bb58942e3a74bbd5b0ef48bc27fed431
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-01 16:01:13 +0800

    s

--- includes/hooks.php
+++ includes/hooks.php
@@ -509,3 +509,14 @@ function action_body_scripts()
 }
 
 add_action('wp_body_open', 'action_body_scripts');
+
+
+function remove_canonical()
+{
+
+    // Disable for 'search' page
+    if (is_category()) {
+        add_filter('wpseo_canonical', '__return_false',  10, 1);
+    }
+}
+add_action('wp', 'remove_canonical');
