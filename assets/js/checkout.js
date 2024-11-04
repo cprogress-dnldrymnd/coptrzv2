b2a4ea6bd013e4a57c05f473aea0a0d0989195b2
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-05 00:44:43 +0800

    Update checkout.js

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -6,13 +6,39 @@ jQuery(document).ready(function () {
     phone_input();
 });
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+function utm_parameters() {
+    utm_val('utm_source');
+}
+
+function utm_val(name) {
+    var val = GetURLParameter(name);
+    jQuery('input[name="' + name + '"]').val(val);
+    console.log(val);
+}
+
 function phone_input() {
     const input = document.querySelector("#billing_phone");
     window.intlTelInput(input, {
         initialCountry: "gb",
         onlyCountries: countries,
         strictMode: true,
-        countrySearch:false,
+        countrySearch: false,
     });
 }
 function input_fields_checkout() {
