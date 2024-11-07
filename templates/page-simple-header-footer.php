031084edf090dacd382a7aa70c316835151d040d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-07 22:44:01 +0800

    Update page-simple-header-footer.php

--- templates/page-simple-header-footer.php
+++ templates/page-simple-header-footer.php
@@ -10,7 +10,7 @@ get_header('simple');
 ?>
 <div class="modules">
     <?php
-    echo ___hero_modules();
+    echo do_shortcode(___hero_modules());
     echo do_shortcode(get_post_meta(get_the_ID(), '_sections_html', true));
     ?>
 </div>
