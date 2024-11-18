33521cb747a420af8f9cf4f4df495d2c0cccb73d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:46:23 +0800

    cs

--- template-parts/product-form/section-1.php
+++ template-parts/product-form/section-1.php
@@ -1,7 +1,7 @@
-<section class="section-label">
-    Section 1
-</section>
 <section class="section section-1 md-padding-top md-padding-bottom  text-center" id="section-1">
+    <div class="section-label">
+        Section 1
+    </div>
     <div class="container">
         <div class="text-center heading-box"><span><?= get_option('heading_prefix'); ?></span>
             <h2>A Palm-Sized Drone for Vlogs</h2>
