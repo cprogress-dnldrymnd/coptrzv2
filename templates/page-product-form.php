e235b0dccc5d492b9c33903ff0a93f2ee13246c3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:54:45 +0800

    Update page-product-form.php

--- templates/page-product-form.php
+++ templates/page-product-form.php
@@ -11,8 +11,8 @@ get_header();
 <div class="modules">
     <?php
     echo do_shortcode(___hero_modules());
-    echo get_page_template_slug();
     ?>
+    <?php get_template_part('template-parts/product-form/section-1') ?>
 </div>
 <?php
 get_footer();
