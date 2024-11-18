f795247a59804e1e2ac8d41753fde723b14f5385
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:33:43 +0800

    Update customizer.php

--- includes/customizer.php
+++ includes/customizer.php
@@ -2,18 +2,9 @@
 function customizer_section_1($wp_customize)
 {
     // Add a new section to the Customizer
-    $wp_customize->add_section('section_1', array(
-        'title'    => __('Section 1', 'coptrz'),
-        'priority' => 160, // Adjust the priority to control the section's position
-        'active_callback' => function () {
-            if (get_page_template_slug() == 'templates/page-product-form.php') {
-                return true;
-            } else {
-                return false;
-            }
-        },
-    ));
 
+    __add_section($wp_customize);
+    
     // Add a text control to the section
     $wp_customize->add_setting('my_page_text', array(
         'default'           => 'Default Text',
@@ -27,3 +18,19 @@ function customizer_section_1($wp_customize)
     ));
 }
 add_action('customize_register', 'customizer_section_1');
+
+
+function __add_section($wp_customize)
+{
+    return $wp_customize->add_section('section_1', array(
+        'title'    => __('Section 1', 'coptrz'),
+        'priority' => 160, // Adjust the priority to control the section's position
+        'active_callback' => function () {
+            if (get_page_template_slug() == 'templates/page-product-form.php') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+    ));
+}
