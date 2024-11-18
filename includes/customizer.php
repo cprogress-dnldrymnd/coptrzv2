665b555decae0ac5d18aa34f2d6f0aaf64a81e87
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:37:20 +0800

    Update customizer.php

--- includes/customizer.php
+++ includes/customizer.php
@@ -4,25 +4,14 @@ function customizer_section_1($wp_customize)
     // Add a new section to the Customizer
 
     __add_section($wp_customize, 'section_1', 'Section 1');
-
-    // Add a text control to the section
-    $wp_customize->add_setting('my_page_text', array(
-        'default'           => 'Default Text',
-        'sanitize_callback' => 'sanitize_text_field',
-    ));
-
-    $wp_customize->add_control('my_page_text', array(
-        'label'    => __('Page Text', 'coptrz'),
-        'section'  => 'section_1',
-        'type'     => 'text',
-    ));
+    __add_field($wp_customize, 'section_1', 'heading_prefix', 'Heading Prefix');
 }
 add_action('customize_register', 'customizer_section_1');
 
 
 function __add_section($wp_customize, $section_id, $section_name)
 {
-    return $wp_customize->add_section($section_id, array(
+    $wp_customize->add_section($section_id, array(
         'title'    => __($section_name, 'coptrz'),
         'priority' => 160, // Adjust the priority to control the section's position
         'active_callback' => function () {
@@ -33,4 +22,23 @@ function __add_section($wp_customize, $section_id, $section_name)
             }
         },
     ));
+
+    return;
+}
+
+function __add_field($wp_customize, $section_id, $field_id, $field_name)
+{
+    // Add a text control to the section
+    $wp_customize->add_setting($field_id, array(
+        'default'           => 'Lorem ipsum',
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('my_page_text', array(
+        'label'    => __($field_name, 'coptrz'),
+        'section'  => $section_id,
+        'type'     => 'text',
+    ));
+
+    return;
 }
