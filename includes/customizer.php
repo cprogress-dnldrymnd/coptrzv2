8b8fa21fd5169651c6502c1fc5393f7d698b26a6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:30:35 +0800

    Update customizer.php

--- includes/customizer.php
+++ includes/customizer.php
@@ -1,7 +1,18 @@
 <?php
-function my_theme_customize_register($wp_customize)
+function customizer_section_1($wp_customize)
 {
-    __add_section($wp_customize);
+    // Add a new section to the Customizer
+    $wp_customize->add_section('section_1', array(
+        'title'    => __('Section 1', 'coptrz'),
+        'priority' => 160, // Adjust the priority to control the section's position
+        'active_callback' => function () {
+            if (get_page_template_slug() == 'templates/page-product-form.php') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+    ));
 
     // Add a text control to the section
     $wp_customize->add_setting('my_page_text', array(
@@ -15,20 +26,4 @@ function my_theme_customize_register($wp_customize)
         'type'     => 'text',
     ));
 }
-add_action('customize_register', 'my_theme_customize_register');
-
-
-function __add_section($wp_customize)
-{
-    return $wp_customize->add_section('section_1', array(
-        'title'    => __('Section 1', 'coptrz'),
-        'priority' => 160, // Adjust the priority to control the section's position
-        'active_callback' => function () {
-            if (get_page_template_slug() == 'templates/page-product-form.php') {
-                return true;
-            } else {
-                return false;
-            }
-        },
-    ));
-}
+add_action('customize_register', 'customizer_section_1');
