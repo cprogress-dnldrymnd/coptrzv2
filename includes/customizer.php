1c86c7cf7b039c7c14e108eee1b120e016bd39d2
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:34:47 +0800

    Update customizer.php

--- includes/customizer.php
+++ includes/customizer.php
@@ -3,8 +3,8 @@ function customizer_section_1($wp_customize)
 {
     // Add a new section to the Customizer
 
-    __add_section($wp_customize);
-    
+    __add_section($wp_customize, 'section_1', 'Section 1');
+
     // Add a text control to the section
     $wp_customize->add_setting('my_page_text', array(
         'default'           => 'Default Text',
@@ -20,10 +20,10 @@ function customizer_section_1($wp_customize)
 add_action('customize_register', 'customizer_section_1');
 
 
-function __add_section($wp_customize)
+function __add_section($wp_customize, $section_id, $section_name)
 {
-    return $wp_customize->add_section('section_1', array(
-        'title'    => __('Section 1', 'coptrz'),
+    return $wp_customize->add_section($section_id, array(
+        'title'    => __($section_name, 'coptrz'),
         'priority' => 160, // Adjust the priority to control the section's position
         'active_callback' => function () {
             if (get_page_template_slug() == 'templates/page-product-form.php') {
