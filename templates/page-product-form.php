991854ac9a43965a0f899e027518f5cb00d53960
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:13:43 +0800

    cs

--- templates/page-product-form.php
+++ templates/page-product-form.php
@@ -13,11 +13,15 @@ get_header();
     echo do_shortcode(___hero_modules());
     ?>
     <div class="product-form">
-        <div class="col-lg-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3">
 
-        </div>
-        <div class="col-lg-9">
-            <?php get_template_part('template-parts/product-form/section-1') ?>
+                </div>
+                <div class="col-lg-9">
+                    <?php get_template_part('template-parts/product-form/section-1') ?>
+                </div>
+            </div>
         </div>
     </div>
 
