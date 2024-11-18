93399291bb05bb5cbbce38fb08403dd4f42a1a2c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:29:46 +0800

    Update customizer.php

--- includes/customizer.php
+++ includes/customizer.php
@@ -1,9 +1,27 @@
 <?php
 function my_theme_customize_register($wp_customize)
 {
-    // Add a new section to the Customizer
-    $wp_customize->add_section('my_page_section', array(
-        'title'    => __('My Page Settings', 'my-theme'),
+    __add_section($wp_customize);
+
+    // Add a text control to the section
+    $wp_customize->add_setting('my_page_text', array(
+        'default'           => 'Default Text',
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control('my_page_text', array(
+        'label'    => __('Page Text', 'coptrz'),
+        'section'  => 'my_page_section',
+        'type'     => 'text',
+    ));
+}
+add_action('customize_register', 'my_theme_customize_register');
+
+
+function __add_section($wp_customize)
+{
+    return $wp_customize->add_section('section_1', array(
+        'title'    => __('Section 1', 'coptrz'),
         'priority' => 160, // Adjust the priority to control the section's position
         'active_callback' => function () {
             if (get_page_template_slug() == 'templates/page-product-form.php') {
@@ -13,17 +31,4 @@ function my_theme_customize_register($wp_customize)
             }
         },
     ));
-
-    // Add a text control to the section
-    $wp_customize->add_setting('my_page_text', array(
-        'default'           => 'Default Text',
-        'sanitize_callback' => 'sanitize_text_field',
-    ));
-
-    $wp_customize->add_control('my_page_text', array(
-        'label'    => __('Page Text', 'my-theme'),
-        'section'  => 'my_page_section',
-        'type'     => 'text',
-    ));
 }
-add_action('customize_register', 'my_theme_customize_register');
