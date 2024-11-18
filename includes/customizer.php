c0365d7819ceb4131087f54fb1b79b1bf2fff6ba
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-18 11:54:14 +0800

    Update customizer.php

--- includes/customizer.php
+++ includes/customizer.php
@@ -2,11 +2,11 @@
 function customizer_section_1($wp_customize)
 {
     // Add a new section to the Customizer
-
+    $placeholder = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, asperiores! Et pariatur sunt animi. Hic, itaque eligendi explicabo eum aspernatur, numquam quidem facere eius debitis culpa veritatis? Odio, velit adipisci.';
     __add_section($wp_customize, 'section_1', 'Section 1');
-    __add_field($wp_customize, 'section_1', 'section_1_heading_prefix', 'Heading Prefix', 'text');
-    __add_field($wp_customize, 'section_1', 'section_1_heading', 'Heading', 'text');
-    __add_field($wp_customize, 'section_1', 'section_1_description', 'Description', 'textarea');
+    __add_field($wp_customize, 'section_1', 'section_1_heading_prefix', 'Heading Prefix', 'Lorem ipsum', 'text');
+    __add_field($wp_customize, 'section_1', 'section_1_heading', 'Heading', 'Lorem ipsum dolor sit amet', 'text');
+    __add_field($wp_customize, 'section_1', 'section_1_description', 'Description', $placeholder, 'textarea');
 }
 add_action('customize_register', 'customizer_section_1');
 
