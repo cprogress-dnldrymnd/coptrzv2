2c81a786dfddbb287354c22686a83a81455af406
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-19 20:32:27 +0800

    cs

--- includes/customizer.php
+++ includes/customizer.php
@@ -6,7 +6,7 @@ function customizer_section_1($wp_customize)
     $image_placeholder = 'https://coptrz.com/wp-content/uploads/2023/05/placeholder-image.webp';
 
     /*Section Video */
-    __add_section($wp_customize, 'section_video', '--Section 1');
+    __add_section($wp_customize, 'section_video', '--Section Video');
     __add_field($wp_customize, 'section_video', 'section_video_url', 'Video URL', 'Lorem ipsum', 'text');
     __add_field($wp_customize, 'section_video', 'section_video_heading', 'Heading', 'Lorem ipsum dolor sit amet', 'text');
     __add_field($wp_customize, 'section_video', 'section_video_description', 'Description', $placeholder, 'textarea');
---
+++ template-parts/product-form/section-video.php
@@ -0,0 +1,16 @@
+<?php if (current_user_can('administrator') && isset($_GET['customize_changeset_uuid'])) { ?>
+    <div class="section-label">
+        Section Video
+    </div>
+<?php } ?>
+<section class="section section-0 normal-margins xl-padding-top background-overlay default text-white sm-margin-top  ms-20px me-20px rounded-corner" id="section-0">
+    <div class="background-image background-overlay"><video autoplay="" loop="" muted="" src="https://coptrz.com/wp-content/uploads/2024/10/dji-neo-video.mp4" id="html5_video_22mfolsjg9h"></video></div>
+    <div class="container">
+        <div class="position-relative container-inner xs-padding-top xs-padding-bottom xs-padding-left xs-padding-right">
+            <h3>The Future Takes Shape</h3>
+            <div class="description-box">
+                <p>DJI Neo keeps pace as your personal photographer and ensures you are always in the spotlight.</p>
+            </div>
+        </div>
+    </div>
+</section>
