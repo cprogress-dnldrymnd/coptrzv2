d0ca023e164f1e23a6dd0ac23f6dcdb06282a95a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-11-19 20:33:32 +0800

    Update section-video.php

--- template-parts/product-form/section-video.php
+++ template-parts/product-form/section-video.php
@@ -1,16 +1,26 @@
+<?php
+$section_video_url = get_option('section_video_url');
+$section_video_heading = get_option('section_video_heading');
+$section_video_description = get_option('section_video_description');
+
+?>
 <?php if (current_user_can('administrator') && isset($_GET['customize_changeset_uuid'])) { ?>
     <div class="section-label">
         Section Video
     </div>
 <?php } ?>
 <section class="section section-0 normal-margins xl-padding-top background-overlay default text-white sm-margin-top  ms-20px me-20px rounded-corner" id="section-0">
-    <div class="background-image background-overlay"><video autoplay="" loop="" muted="" src="https://coptrz.com/wp-content/uploads/2024/10/dji-neo-video.mp4" id="html5_video_22mfolsjg9h"></video></div>
+    <div class="background-image background-overlay"><video autoplay loop muted src="<?= $section_video_url ?>"></video></div>
     <div class="container">
         <div class="position-relative container-inner xs-padding-top xs-padding-bottom xs-padding-left xs-padding-right">
-            <h3>The Future Takes Shape</h3>
-            <div class="description-box">
-                <p>DJI Neo keeps pace as your personal photographer and ensures you are always in the spotlight.</p>
-            </div>
+            <?php if ($section_2_heading) { ?>
+                <h3><?= $section_2_heading ?></h3>
+            <?php } ?>
+            <?php if ($section_2_description) { ?>
+                <div class="description-box mx-auto" style="max-width: 1000px;">
+                    <?= wpautop($section_2_description) ?>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </section>
