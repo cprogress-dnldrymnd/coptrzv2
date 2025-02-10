17ad964f23a3354e2779e2c112dd68c142a49145
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-02-10 18:18:49 +0800

    Update single-landingpages.php

--- single-landingpages.php
+++ single-landingpages.php
@@ -5,6 +5,7 @@ get_header('landing-v2');
     <?php
     echo ___hero_modules();
     echo do_shortcode(get_post_meta(get_the_ID(), '_sections_html', true));
+    echo do_shortcode(get_post_meta(get_the_ID(), '_sections_after_main_html', true));
     ?>
 </div>
 <?php
