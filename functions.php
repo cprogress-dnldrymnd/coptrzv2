f0723f0f1b32257c671d8cf2bfead1e463d2d291
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-14 18:14:11 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -233,13 +233,11 @@ add_filter('wpcf7_validate_email*', 'wpcf7_validate_mod_gov_uk', 20, 2);
 
 function wpcf7_validate_mod_gov_uk($result, $tag)
 {
-	if (get_the_ID() == 372958) {
-		if ($tag->name == 'email') { // Replace 'your-email' with your actual email field name
-			$value = isset($_POST[$tag->name]) ? trim($_POST[$tag->name]) : '';
-			if (! filter_var($value, FILTER_VALIDATE_EMAIL) || ! preg_match('/@mod\.gov\.uk$/', $value)) {
-				$result->invalidate($tag, 'Please use a @mod.gov.uk email address');
-			}
+	if ($tag->name == 'email') { // Replace 'your-email' with your actual email field name
+		$value = isset($_POST[$tag->name]) ? trim($_POST[$tag->name]) : '';
+		if (! filter_var($value, FILTER_VALIDATE_EMAIL) || ! preg_match('/@mod\.gov\.uk$/', $value)) {
+			$result->invalidate($tag, 'Please use a @mod.gov.uk email address');
 		}
-		return $result;
 	}
+	return $result;
 }
