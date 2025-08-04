319bbab882cb8439a495b08e889755dac9a6451c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-04 17:06:41 +0800

    cs

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -6954,7 +6954,7 @@ Container::make('post_meta', __('Custom CSS'))
 /*-----------------------------------------------------------------------------------*/
 /* Custom CSS
 /*-----------------------------------------------------------------------------------*/
-Container::make('post_meta', __('Custom CSS'))
+Container::make('post_meta', __('Quiz Settings'))
     ->where('post_type', '=', 'quiz')
     ->add_fields(
         array(
