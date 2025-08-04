995f918a1c39ea9f2e158b098fd2bb182f8505d3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-04 17:50:31 +0800

    Update single-quiz.php

--- single-quiz.php
+++ single-quiz.php
@@ -14,9 +14,18 @@ $quiz_questions = get__post_meta('quiz_questions');
     ?>
     <section class="quiz-question--section">
         <div class="container">
-            <h3>
+            <h3 class="question--text">
                 <?= $question_text ?>
             </h3>
+            <div class="question--choices">
+                <div class="row g-4 justify-content-center">
+                    <?php foreach ($question_choices as $question_choice) { ?>
+                        <div class="col-lg-4">
+                            <input type="<?= $question_type ?>" name="<?= $question_key ?>" value="<?= $question_choice['choice'] ?>">
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
         </div>
     </section>
 <?php } ?>
