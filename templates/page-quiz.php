310457015e0fc890c640dc68ebd0c51cc4a45037
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-05 14:34:59 +0800

    Update page-quiz.php

--- templates/page-quiz.php
+++ templates/page-quiz.php
@@ -2,4 +2,15 @@
 /*-----------------------------------------------------------------------------------*/
 /* Template Name: Quiz
 /*-----------------------------------------------------------------------------------*/
+?>
+<style>
+    .rh-widget.rh-widget {
+        height: auto !important
+    }
+
+    .rh-widget.rh-widget iframe {
+        height: 100vh !important
+    }
+</style>
+<?php
 the_content();
