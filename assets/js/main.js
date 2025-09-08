88ea39704620e092a70864f0f612a0ae4402554d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-08 21:08:30 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -310,15 +310,6 @@ function __header_menu() {
     }, 500);
     if (jQuery('header').length > 0) {
         if (window.innerWidth > 991) {
-            if (jQuery('.submenu4').length > 0) {
-
-                jQuery('.submenu4').each(function (index, element) {
-                    $parent = jQuery(this).parents('.submenu2').find('>.row');
-                    $parent.find('>div:not(.submenu--4-parent)').attr('class', 'col-lg-4');
-                    jQuery(this).appendTo($parent.find('.submenu--4-parent'));
-
-                });
-            }
             jQuery(".has-submenu").hover(
                 function () {
                     jQuery(this).find(' > .nav-link').addClass('active');
@@ -466,7 +457,15 @@ function __header_menu() {
                 jQuery('body').removeClass('mobile-menu-active');
             });
         }
+        if (jQuery('.submenu4').length > 0) {
 
+            jQuery('.submenu4').each(function (index, element) {
+                $parent = jQuery(this).parents('.submenu2').find('>.row');
+                $parent.find('>div:not(.submenu--4-parent)').attr('class', 'col-lg-4');
+                jQuery(this).appendTo($parent.find('.submenu--4-parent'));
+
+            });
+        }
     }
 
 
