7893a68ebe059c0c33c9edb8412f4d855eb1f47b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-08 20:32:43 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -351,7 +351,7 @@ function __header_menu() {
 
             jQuery('.submenu3 .nav-link').hover(
                 function (e) {
-                    jQuery('.has-children-tab.nav-link.active').removeClass('active');
+                    jQuery('.submenu3 .has-children-tab.nav-link.active').removeClass('active');
                     $target = jQuery(this).attr('url_target');
                     jQuery($target).removeClass('d-none');
                     jQuery(this).toggleClass('active');
