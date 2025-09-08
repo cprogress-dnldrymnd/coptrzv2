1531666a48d53e2b103dc9829988135d3b615cd0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-08 19:08:47 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ function __shop_coptrz_link() {
     });
 }
 
-function __utm_parameters() {   
+function __utm_parameters() {
     setTimeout(function () {
         utm_val('utm_campaign');
         utm_val('utm_source');
@@ -423,6 +423,10 @@ function __header_menu() {
             });
         }
 
+    }
+
+    if (jQuery('.submenu4').length > 0) {
+        console.log('xx');
     }
 }
 
