227c6c104c879a4dfab7980acf187b17a0f5f37a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-15 23:02:31 +0800

    Update modules.php

--- includes/modules.php
+++ includes/modules.php
@@ -191,7 +191,7 @@ function _pagination($has_pagination, $query, $data = false)
 function ___hero_modules($hero_alignment_args = false, $hero_height_args = false)
 {
     $id = get_the_ID();
-    $hero_heading = get__post_meta('hero_heading');
+    $hero_heading = do_shortcode(get__post_meta('hero_heading'));
     $hero_description = do_shortcode(get__post_meta('hero_description'));
     $hero_hidden = get__post_meta('hero_hidden');
     if (get_post_type() == '') {
