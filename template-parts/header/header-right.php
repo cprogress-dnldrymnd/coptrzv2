2d4cdeea77c56d5f42a6f5715f158be9b6aa2834
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-10 15:07:33 +0800

    Update header-right.php

--- template-parts/header/header-right.php
+++ template-parts/header/header-right.php
@@ -18,14 +18,27 @@ $SVG = new SVG;
                 <?= $SVG->academy(); ?>
             </a>
         </div>
-        <?php if (get_post_type() == 'rentals' || get_the_ID() == 292371 || get_the_ID() == 292384) { ?>
-            <div class="col-auto d-flex align-items-center mini-cart">
-                <?php
-
+        <div class="col-auto d-flex align-items-center mini-cart">
+            <?php
+            if (get_post_type() == 'rentals' || get_the_ID() == 292371 || get_the_ID() == 292384) {
                 echo do_shortcode('[booqable_cart_button href="' . get_site_url() . '/rental-basket"]');
-                ?>
-            </div>
-        <?php } ?>
+            } else {
+            ?>
+                <div class="mini-cart-wrapper">
+                    <a href="#" id="mini-cart-button" class="header-icon cart-icon text-white d-flex align-items-center">
+                        <?= $SVG->cart(); ?>
+                        <div class="cart-number">
+                            <?= WC()->cart->get_cart_contents_count(); ?>
+                        </div>
+                    </a>
+                    <div class="mini-cart-holder bg-white rounded-10px mt-10px">
+                        <?php woocommerce_mini_cart() ?>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
         <div class="col-auto d-flex align-items-center d-lg-none ">
             <button class="menu-burger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvasMenu"
                 aria-controls="offCanvasMenu">
