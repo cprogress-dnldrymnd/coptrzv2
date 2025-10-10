46679a654b7ee64990ab6908462006c583c1ab6b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-10 15:07:04 +0800

    cs

--- template-parts/header/header-right.php
+++ template-parts/header/header-right.php
@@ -18,27 +18,14 @@ $SVG = new SVG;
                 <?= $SVG->academy(); ?>
             </a>
         </div>
-        <div class="col-auto d-flex align-items-center mini-cart">
-            <?php
-            if (get_post_type() == 'rentals' || get_the_ID() == 292371 || get_the_ID() == 292384) {
+        <?php if (get_post_type() == 'rentals' || get_the_ID() == 292371 || get_the_ID() == 292384) { ?>
+            <div class="col-auto d-flex align-items-center mini-cart">
+                <?php
+
                 echo do_shortcode('[booqable_cart_button href="' . get_site_url() . '/rental-basket"]');
-            } else {
-            ?>
-                <div class="mini-cart-wrapper">
-                    <a href="#" id="mini-cart-button" class="header-icon cart-icon text-white d-flex align-items-center">
-                        <?= $SVG->cart(); ?>
-                        <div class="cart-number">
-                            <?= WC()->cart->get_cart_contents_count(); ?>
-                        </div>
-                    </a>
-                    <div class="mini-cart-holder bg-white rounded-10px mt-10px">
-                        <?php woocommerce_mini_cart() ?>
-                    </div>
-                </div>
-            <?php
-            }
-            ?>
-        </div>
+                ?>
+            </div>
+        <?php } ?>
         <div class="col-auto d-flex align-items-center d-lg-none ">
             <button class="menu-burger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvasMenu"
                 aria-controls="offCanvasMenu">
