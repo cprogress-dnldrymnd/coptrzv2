20f20cb021e4b92fbd33221ef78569b63e559ea5
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-10 15:09:23 +0800

    Update content-single-product.php

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -43,42 +43,7 @@ if (post_password_required()) {
 			<h2 class="text-center">Buy <?php the_title() ?></h2>
 			<?php if (get__post_meta('shopify_product_link')) { ?>
 				<?php do_action('woocommerce_before_single_product_shopify_link'); ?>
-			<?php } else { ?>
-				<div class="row g-4">
-					<div class="col-7 position-relative">
-						<?php
-						/**
-						 * Hook: woocommerce_before_single_product_summary.
-						 *
-						 * @hooked woocommerce_show_product_sale_flash - 10
-						 * @hooked woocommerce_show_product_images - 20
-						 */
-
-						do_action('woocommerce_before_single_product_summary');
-
-						?>
-					</div>
-					<div class="col-5">
-						<div class="summary entry-summary">
-							<?php
-							/**
-							 * Hook: woocommerce_single_product_summary.
-							 *
-							 * @hooked woocommerce_template_single_title - 5
-							 * @hooked woocommerce_template_single_rating - 10
-							 * @hooked woocommerce_template_single_price - 10
-							 * @hooked woocommerce_template_single_excerpt - 20
-							 * @hooked woocommerce_template_single_add_to_cart - 30
-							 * @hooked woocommerce_template_single_meta - 40
-							 * @hooked woocommerce_template_single_sharing - 50
-							 * @hooked WC_Structured_Data::generate_product_data() - 60
-							 */
-							do_action('woocommerce_single_product_summary');
-							?>
-						</div>
-					</div>
-				</div>
-			<?php } ?>
+			<?php }  ?>
 		</div>
 	</section>
 
