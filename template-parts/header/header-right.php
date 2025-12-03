921c3f1d17860cde40ecac3bdb25638ef964fef7
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-04 00:46:53 +0800

    Update header-right.php

--- template-parts/header/header-right.php
+++ template-parts/header/header-right.php
@@ -60,7 +60,7 @@ $SVG = new SVG;
             'button_text'       => $button_text,
             'button_url'        => $button_url,
             'button_url_custom' => $button_url_custom,
-            'button_style'      => $button_style . ' col-auto button-accent button-small d-none d-lg-block button-s',
+            'button_style'      => $button_style . ' col-auto button-accent button-small d-none d-lg-block button-store',
             'button_target'     => $button_target,
         )));
 
