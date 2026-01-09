573e243940c14d5b7db4a0114027887ab86030a4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-09 21:41:53 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -954,8 +954,6 @@ class Shortcodes
         $document = get__post_meta_by_id($id, 'document');
         if ($document) {
             return wp_get_attachment_url($document);
-        } else {
-            return 'xx';
         }
     }
 }
