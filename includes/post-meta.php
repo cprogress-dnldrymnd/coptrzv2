e09b56cb5c317dd386d10843dcf3f3976f3adfc6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-22 19:12:09 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1412,7 +1412,7 @@ function __section_fields($name = 'sections')
                             ))
                             ->add_fields('dji_air_series', array(
                                 Field::make('html', 'html')->set_html('<h3>This will display DJI Air Series servicing blocks. </h3>'),
-                            ))                                      
+                            ))
                             ->add_fields('dji_mavic_series', array(
                                 Field::make('html', 'html')->set_html('<h3>This will display DJI Mavic Series servicing blocks. </h3>'),
                             ))
@@ -2080,12 +2080,12 @@ function __section_fields($name = 'sections')
                                             ))
                                             ->add_fields('three_year_servicing_plans', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display three year servicing plans. </h3>'),
-                                        ))                                          ->add_fields('dji_mini_series', array(
+                                            ))->add_fields('dji_mini_series', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display DJI Mini Series servicing blocks. </h3>'),
                                             ))
                                             ->add_fields('dji_air_series', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display DJI Air Series servicing blocks. </h3>'),
-                                            ))                                      ->add_fields('dji_mavic_series', array(
+                                            ))->add_fields('dji_mavic_series', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display DJI Mavic Series servicing blocks. </h3>'),
                                             ))
                                             ->add_fields('dji_m30_series', array(
@@ -2105,7 +2105,7 @@ function __section_fields($name = 'sections')
                                             ))
                                             ->add_fields('parrot_anafi_ai', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display Parrot Anafi Ai servicing blocks. </h3>'),
-                                        ))                                          ->add_fields('parrot_anafi_ukr', array(
+                                            ))->add_fields('parrot_anafi_ukr', array(
                                                 Field::make('html', 'html')->set_html('<h3>This will display Parrot Anafi UKR servicing blocks. </h3>'),
                                             ))
                                             ->add_fields('elios_3', array(
@@ -5627,6 +5627,8 @@ function __section_fields($name = 'sections')
                             ))
                             ->set_layout('tabbed-vertical')
                     ))
+                    ->add_fields('drone_servicing_grid', __servicing_fields())
+
                     ->add_fields('product', array(
                         Field::make('association', 'product', 'Product')
                             ->set_types(
