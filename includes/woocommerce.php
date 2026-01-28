ffcab4b459a01655bb56004d0c77ae5a0448f03b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-28 18:35:30 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -1056,7 +1056,7 @@ function custom_product_variation_training($product_id, $delivery_method = 'onli
                     $html .= "<a href='?add-to-cart=$product_id' data-quantity='1' class='product-btn button product_type_simple add_to_cart_button ajax_add_to_cart' data-product_id='$product_id' data-product_sku='$sku' rel='nofollow'><span class='product-data d-none'>$data_encode</span> Add to basket</a>";
                 } else {
                     $basket_url =   wc_get_cart_url();
-                    if (has_term('training', 'product_cat')) {
+                    if (has_term('training', 'product_cat', get_the_ID())) {
                         $button_text = 'Book Now';
                     } else {
                         $button_text = 'Buy Now';
