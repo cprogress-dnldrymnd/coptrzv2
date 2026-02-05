6e798b53839cd2e394d25b596623e5fbdb79a375
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-05 20:05:38 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -988,7 +988,7 @@ class Shortcodes
             return '<div class="document-thumbnail image-box">' . $image . '</div>';
         } else {
             $hero_form_document_redirect = get__post_meta_by_id($document_id, 'hero_form_document_redirect');
-            $hero_form_document_redirect_id = isset($args['hero_form_document_redirect_id']) ? $args['hero_form_document_redirect_id'] : false;
+            $hero_form_document_redirect_id = isset($hero_form_document_redirect) ? $hero_form_document_redirect: false;
             $image = get_the_post_thumbnail($hero_form_document_redirect_id, 'large');
             return '<div class="document-thumbnail image-box">' . $image . '</div>';
         }
