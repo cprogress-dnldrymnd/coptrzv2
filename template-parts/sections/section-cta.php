00e0e9abba0ee7b2d5033acfd3375fd726962b31
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 17:01:44 +0800

    Update section-cta.php

--- template-parts/sections/section-cta.php
+++ template-parts/sections/section-cta.php
@@ -1,10 +1,14 @@
 <section class="cta--section">
     <div class="container">
-        <div class="container-wrapper">
+        <div class="container-wrapper position-relative lg-padding rounded overflow-hidden" style="--bs-border-radius: 20px">
             <div class="background-image no-overlay"><img src="" alt=""></div>
             <h2>
                 What Changes After Deployment
             </h2>
+            <div class="desc-box mb-4">
+                <p>When DJI Enterprise is deployed correctly, operational outcomes improve quickly and measurably. These examples show how UK organisations have reduced risk, improved efficiency and delivered better results with Coptrz.</p>
+            </div>
+            <div class="button-accent col-auto button-box text-center mt-5"><a class="rounded-10px " href="#Benefits" target="_self">Explore Solutions by Industry</a></div>
         </div>
     </div>
 </section>
