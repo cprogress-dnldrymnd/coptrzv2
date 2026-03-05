433e666b0377817351747a8a625753922065c685
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 17:13:21 +0800

    Update section-case-study.php

--- template-parts/sections/section-case-study.php
+++ template-parts/sections/section-case-study.php
@@ -1,105 +1,31 @@
+<?
+$post_ids = [291423, 272246, 272244, 272242];
+?>
+
 <section class="guides bg-accent-3 py-6 text-white">
     <div class="container">
         <div class="container-wrapper">
             <h2 class="heading-style mb-5">UK Enterprise Examples</h2>
-            <div class="guides-wrapper">
-                <h3 class="text-accent mb-4">Decision Resources</h3>
-                <div class="guides-items ">
-                    <div class="guide-item-holder">
-                        <a href="#" class="guide-item d-flex align-items-start align-items-md-center justify-content-between gap-4 flex-column flex-md-row">
-                            <div class="guide-item-left d-flex align-items-center gap-4">
-                                <div class="image-box">
-                                    <img src="https://coptrz.com/wp-content/uploads/2026/03/DJI-Matrice-400-UK-Buyers-Guide-Thumbnail.png" alt="">
+            <div class="case-study-wrapper same-image-height">
+                <div class="swiper">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($post_ids as $post) { ?>
+                            <?php
+                            ?>
+                            <div class="swiper-slide">
+                                <div class="swiper-slide-inner">
+                                    <div class="image-box">
+                                        <?= get_the_post_thumbnail($post, 'large') ?>
+                                    </div>
+                                    <div class="content-box">
+                                        <h4><?= get_the_title($post) ?></h4>
+                                        <a href="<?= get_the_permalink($post) ?>">
+                                            View Case Study
+                                        </a>
+                                    </div>
                                 </div>
-                                <div class="guide-name">DJI Matrice 400 UK Buyer's Guide</div>
                             </div>
-                            <div class="guide-item-right text-right w-100 w-md-auto">
-                                <span class="guide-link d-inline-flex align-items-center gap-2">
-                                    <span>Access Guide</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11">
-                                        <g id="arrow" opacity="0.5">
-                                            <g id="Dribbble-Light-Preview" transform="translate(-300 -6643)">
-                                                <g id="icons" transform="translate(56 160)">
-                                                    <path id="arrow_right-_346_" data-name="arrow_right-[#346]" d="M264,6488.27l-5.657-5.27-1.414,1.22,3.243,3.01H244v1.95h16.172l-3.243,3.35,1.414,1.47Z" fill="currentColor" fill-rule="evenodd" />
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                </span>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="guide-item-holder">
-                        <a href="#" class="guide-item d-flex align-items-start align-items-md-center justify-content-between gap-4 flex-column flex-md-row">
-                            <div class="guide-item-left d-flex align-items-center gap-4">
-                                <div class="image-box">
-                                    <img src="https://coptrz.com/wp-content/uploads/2026/03/DJI-Matrice-400-UK-Buyers-Guide-Thumbnail.png" alt="">
-                                </div>
-                                <div class="guide-name">DJI Matrice 400 UK Buyer's Guide</div>
-                            </div>
-                            <div class="guide-item-right text-right w-100 w-md-auto">
-                                <span class="guide-link d-inline-flex align-items-center gap-2">
-                                    <span>Access Guide</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11">
-                                        <g id="arrow" opacity="0.5">
-                                            <g id="Dribbble-Light-Preview" transform="translate(-300 -6643)">
-                                                <g id="icons" transform="translate(56 160)">
-                                                    <path id="arrow_right-_346_" data-name="arrow_right-[#346]" d="M264,6488.27l-5.657-5.27-1.414,1.22,3.243,3.01H244v1.95h16.172l-3.243,3.35,1.414,1.47Z" fill="currentColor" fill-rule="evenodd" />
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                </span>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="guide-item-holder">
-                        <a href="#" class="guide-item d-flex align-items-start align-items-md-center justify-content-between gap-4 flex-column flex-md-row">
-                            <div class="guide-item-left d-flex align-items-center gap-4">
-                                <div class="image-box">
-                                    <img src="https://coptrz.com/wp-content/uploads/2026/03/DJI-Matrice-400-UK-Buyers-Guide-Thumbnail.png" alt="">
-                                </div>
-                                <div class="guide-name">DJI Matrice 400 UK Buyer's Guide</div>
-                            </div>
-                            <div class="guide-item-right text-right w-100 w-md-auto">
-                                <span class="guide-link d-inline-flex align-items-center gap-2">
-                                    <span>Access Guide</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11">
-                                        <g id="arrow" opacity="0.5">
-                                            <g id="Dribbble-Light-Preview" transform="translate(-300 -6643)">
-                                                <g id="icons" transform="translate(56 160)">
-                                                    <path id="arrow_right-_346_" data-name="arrow_right-[#346]" d="M264,6488.27l-5.657-5.27-1.414,1.22,3.243,3.01H244v1.95h16.172l-3.243,3.35,1.414,1.47Z" fill="currentColor" fill-rule="evenodd" />
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                </span>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="guide-item-holder">
-                        <a href="#" class="guide-item d-flex align-items-start align-items-md-center justify-content-between gap-4 flex-column flex-md-row">
-                            <div class="guide-item-left d-flex align-items-center gap-4">
-                                <div class="image-box">
-                                    <img src="https://coptrz.com/wp-content/uploads/2026/03/DJI-Matrice-400-UK-Buyers-Guide-Thumbnail.png" alt="">
-                                </div>
-                                <div class="guide-name">DJI Matrice 400 UK Buyer's Guide</div>
-                            </div>
-                            <div class="guide-item-right text-right w-100 w-md-auto">
-                                <span class="guide-link d-inline-flex align-items-center gap-2">
-                                    <span>Access Guide</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11">
-                                        <g id="arrow" opacity="0.5">
-                                            <g id="Dribbble-Light-Preview" transform="translate(-300 -6643)">
-                                                <g id="icons" transform="translate(56 160)">
-                                                    <path id="arrow_right-_346_" data-name="arrow_right-[#346]" d="M264,6488.27l-5.657-5.27-1.414,1.22,3.243,3.01H244v1.95h16.172l-3.243,3.35,1.414,1.47Z" fill="currentColor" fill-rule="evenodd" />
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                </span>
-                            </div>
-                        </a>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
