e72aa596d5fc82ad9eb9b0eb35e4becb9fb665f0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 18:24:32 +0800

    Update section-cta.php

--- template-parts/sections/section-cta.php
+++ template-parts/sections/section-cta.php
@@ -1,6 +1,6 @@
 <section class="cta--section text-white my-6">
     <div class="container">
-        <div class="container-wrapper position-relative lg-padding-top lg-padding-bottom sm-padding-left sm-padding-right rounded overflow-hidden" style="--bs-border-radius: 20px">
+        <div class="container-wrapper bg-primary position-relative lg-padding-top lg-padding-bottom sm-padding-left sm-padding-right rounded overflow-hidden" style="--bs-border-radius: 20px">
             <div class="background-image no-overlay"><img src="https://coptrz.com/wp-content/uploads/2026/03/Group-1654.png" alt=""></div>
             <div class="inner position-relative small-width">
                 <h2>
