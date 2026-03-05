80d1eb1eca9e543c06cfb08a3e9ca24bee7a2ae0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 18:23:44 +0800

    Update section-logos.php

--- template-parts/sections/section-logos.php
+++ template-parts/sections/section-logos.php
@@ -1,4 +1,4 @@
-<section class="logos" aria-label="Trusted by logos">
+<section class="logos my-6" aria-label="Trusted by logos">
     <div class="container">
         <div class="wrap position-relative overflow-hidden">
             <div class="label">Partners & Clients</div>
