5a9657b138a673ee84dbc5ace6c06ff59635af05
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 19:28:32 +0800

    Update page-enterprise.php

--- templates/page-enterprise.php
+++ templates/page-enterprise.php
@@ -206,6 +206,12 @@
         color: var(--accent-4);
     }
 
+    @media(max-width: 767px) {
+        .usp-bar {
+            font-size: 14px;
+        }
+    }
+
     /**end usp bar */
 
     /**number-box-section */
