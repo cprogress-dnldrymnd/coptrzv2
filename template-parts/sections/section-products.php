b2e270f2721bfce23e79feccb8bd7a9cf8691dcb
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 17:36:35 +0800

    Update section-products.php

--- template-parts/sections/section-products.php
+++ template-parts/sections/section-products.php
@@ -2,11 +2,11 @@
 $post_ids = [372804, 372813, 372840, 372842, 373370];
 ?>
 
-<section class="products-section my-padding" id="Related-Products">
+<section class="products-section my-6" id="Related-Products">
     <div class="container ">
         <div class="container-wrapper">
             <h2 class="heading-style">Tools That Enable These Outcomes</h2>
-            <div class="desc-box mb-4">
+            <div class="desc-box mb-5">
                 <p>DJI Enterprise products support different outcomes depending on how and where they are deployed. Selecting the right aircraft, payload and software is not a catalogue exercise. It is an operational decision that affects uptime, data quality and long-term resilience.</p>
             </div>
             <h3 class="text-accent mb-4">Featured DJI Enterprise Products</h3>
