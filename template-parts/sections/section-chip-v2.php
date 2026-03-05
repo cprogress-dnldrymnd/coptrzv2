efc3c23e84463d671f58b136af691d26dbf536bb
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 18:40:40 +0800

    Update section-chip-v2.php

--- template-parts/sections/section-chip-v2.php
+++ template-parts/sections/section-chip-v2.php
@@ -18,45 +18,45 @@
                     <div class="chip" style="--border-color: #2DA1FF; --text-color: #FFFFFF">
                         <div class="row g-3">
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p>Trusted by over <span class="fw-semibold">12,000 operators</span></p>
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p><span class="fw-semibold">4.9 ★ average rating</span> at scale</p>
                                 </div>
                             </div>
 
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p>UK-based technical and operational experts</span></p>
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p>Established UK fulfilment and support infrastructure</p>
                                 </div>
                             </div>
 
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p><span class="fw-semibold">Coptrz Academy</span> ensuring operator competence and continuity</p>
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p><span class="fw-semibold">DJI Certified Service Centre</span> </p>
                                 </div>
                             </div>
 
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p>Proven delivery across critical national infrastructure, defence and public safety sectors</p>
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="inner medium-text h-100 text-center">
+                                <div class="inner medium-text h-100 text-center d-flex align-items-center justify-content-center">
                                     <p>Over a decade of commercial drone operations experience</p>
                                 </div>
                             </div>
