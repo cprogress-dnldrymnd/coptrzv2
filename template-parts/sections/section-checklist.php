c5036ea3cefaf13a9771d6ce22eda9ab5b9f83d9
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 16:07:25 +0800

    Update section-checklist.php

--- template-parts/sections/section-checklist.php
+++ template-parts/sections/section-checklist.php
@@ -18,6 +18,51 @@
                     <span class="text">Teams exploring drone adoption and building an internal business case</span>
                 </div>
             </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="inner rounded p-3 bg-accent-5 d-flex gap-3 align-items-start medium-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Tick" width="22.5" height="22.5" viewBox="0 0 22.5 22.5">
+                        <circle id="Ellipse_39" data-name="Ellipse 39" cx="9" cy="9" r="9" transform="translate(2 2)" fill="#fff" />
+                        <path id="tick-circle-svgrepo-com_23_" data-name="tick-circle-svgrepo-com (23)" d="M13.25,2A11.25,11.25,0,1,0,24.5,13.25,11.269,11.269,0,0,0,13.25,2Zm5.378,8.662-6.379,6.379a.842.842,0,0,1-1.192,0L7.872,13.858a.843.843,0,1,1,1.193-1.193l2.588,2.588L17.435,9.47a.843.843,0,0,1,1.193,1.192Z" transform="translate(-2 -2)" fill="#2da1ff" />
+                    </svg>
+                    <span class="text">Organisations preparing for a first enterprise deployment</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="inner rounded p-3 bg-accent-5 d-flex gap-3 align-items-start medium-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Tick" width="22.5" height="22.5" viewBox="0 0 22.5 22.5">
+                        <circle id="Ellipse_39" data-name="Ellipse 39" cx="9" cy="9" r="9" transform="translate(2 2)" fill="#fff" />
+                        <path id="tick-circle-svgrepo-com_23_" data-name="tick-circle-svgrepo-com (23)" d="M13.25,2A11.25,11.25,0,1,0,24.5,13.25,11.269,11.269,0,0,0,13.25,2Zm5.378,8.662-6.379,6.379a.842.842,0,0,1-1.192,0L7.872,13.858a.843.843,0,1,1,1.193-1.193l2.588,2.588L17.435,9.47a.843.843,0,0,1,1.193,1.192Z" transform="translate(-2 -2)" fill="#2da1ff" />
+                    </svg>
+                    <span class="text">Teams responsible for inspection, surveying or monitoring critical assets</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="inner rounded p-3 bg-accent-5 d-flex gap-3 align-items-start medium-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Tick" width="22.5" height="22.5" viewBox="0 0 22.5 22.5">
+                        <circle id="Ellipse_39" data-name="Ellipse 39" cx="9" cy="9" r="9" transform="translate(2 2)" fill="#fff" />
+                        <path id="tick-circle-svgrepo-com_23_" data-name="tick-circle-svgrepo-com (23)" d="M13.25,2A11.25,11.25,0,1,0,24.5,13.25,11.269,11.269,0,0,0,13.25,2Zm5.378,8.662-6.379,6.379a.842.842,0,0,1-1.192,0L7.872,13.858a.843.843,0,1,1,1.193-1.193l2.588,2.588L17.435,9.47a.843.843,0,0,1,1.193,1.192Z" transform="translate(-2 -2)" fill="#2da1ff" />
+                    </svg>
+                    <span class="text">Organisations deploying drones across multiple sites or departments</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="inner rounded p-3 bg-accent-5 d-flex gap-3 align-items-start medium-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Tick" width="22.5" height="22.5" viewBox="0 0 22.5 22.5">
+                        <circle id="Ellipse_39" data-name="Ellipse 39" cx="9" cy="9" r="9" transform="translate(2 2)" fill="#fff" />
+                        <path id="tick-circle-svgrepo-com_23_" data-name="tick-circle-svgrepo-com (23)" d="M13.25,2A11.25,11.25,0,1,0,24.5,13.25,11.269,11.269,0,0,0,13.25,2Zm5.378,8.662-6.379,6.379a.842.842,0,0,1-1.192,0L7.872,13.858a.843.843,0,1,1,1.193-1.193l2.588,2.588L17.435,9.47a.843.843,0,0,1,1.193,1.192Z" transform="translate(-2 -2)" fill="#2da1ff" />
+                    </svg>
+                    <span class="text">Buyers accountable to procurement, legal or risk committees</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="inner rounded p-3 bg-accent-5 d-flex gap-3 align-items-start medium-text">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Tick" width="22.5" height="22.5" viewBox="0 0 22.5 22.5">
+                        <circle id="Ellipse_39" data-name="Ellipse 39" cx="9" cy="9" r="9" transform="translate(2 2)" fill="#fff" />
+                        <path id="tick-circle-svgrepo-com_23_" data-name="tick-circle-svgrepo-com (23)" d="M13.25,2A11.25,11.25,0,1,0,24.5,13.25,11.269,11.269,0,0,0,13.25,2Zm5.378,8.662-6.379,6.379a.842.842,0,0,1-1.192,0L7.872,13.858a.843.843,0,1,1,1.193-1.193l2.588,2.588L17.435,9.47a.843.843,0,0,1,1.193,1.192Z" transform="translate(-2 -2)" fill="#2da1ff" />
+                    </svg>
+                    <span class="text">Projects where downtime, rework or poor support has real operational cost</span>
+                </div>
+            </div>
         </div>
     </div>
 </section>
