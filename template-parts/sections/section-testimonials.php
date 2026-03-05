bbfa31472f9d1b4e3cd573188f28593608f50cd0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 18:08:53 +0800

    Update section-testimonials.php

--- template-parts/sections/section-testimonials.php
+++ template-parts/sections/section-testimonials.php
@@ -2,19 +2,12 @@
 echo ____post_grid_module(array(
     'id'                      => 'testimonial-slider',
     'is_slider'               => true,
-    'number_of_slides'        => 1,
-    'number_of_slides_tablet' => 1,
+    'number_of_slides'        => 4,
+    'number_of_slides_tablet' => 2,
     'number_of_slides_mobile' => 1,
     'post_grid_id'            => 'testimonial-slider',
     'post_elements'           => array(
-        array(
-            "_type"             => "icon",
-            "icon"              => "271265",
-            "icon_color"        => "text-accent",
-            "icon_color_custom" => "",
-            "icon_width"        => "",
-            "icon_height"       => ""
-        ),
+      
         array(
             "_type"              => "custom_field_1",
             "custom_field_key"   => "_testimonial_content",
