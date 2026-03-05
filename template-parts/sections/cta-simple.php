cb1171796500a978d73c477d91ac39ce056d9701
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 18:00:31 +0800

    Create cta-simple.php

---
+++ template-parts/sections/cta-simple.php
@@ -0,0 +1,21 @@
+<section class="cta--section cta-simple text-white my-6">
+    <div class="container">
+        <div class="container-wrapper position-relative sm-padding background-primary rounded overflow-hidden" style="--bs-border-radius: 20px">
+            <div class="inner position-relative small-width">
+                <div class="row g-3 justify-content-between">
+                    <div class="col-lg-7">
+                        <h2 class="fs-24">
+                            Coptrz DJI Learning Center
+                        </h2>
+                        <div class="desc-box mb-4">
+                            <p>Enterprise adoption succeeds when teams are confident, consistent and supported early. Our Learning Center exists to reduce ramp-up time, align stakeholders and help DJI Enterprise systems deliver outcomes faster through practical guidance and real-world deployment insight.</p>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <div class="button-accent col-auto button-box  mt-5"><a class="rounded-10px " href="#Benefits" target="_self">Explore Solutions by Industry</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
