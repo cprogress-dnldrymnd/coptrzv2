da5e2d884a6562bf0b35a02ff0f78a20c03338e0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 17:03:46 +0800

    Update section-cta.php

--- template-parts/sections/section-cta.php
+++ template-parts/sections/section-cta.php
@@ -2,13 +2,15 @@
     <div class="container">
         <div class="container-wrapper position-relative lg-padding rounded overflow-hidden" style="--bs-border-radius: 20px">
             <div class="background-image no-overlay"><img src="https://coptrz.com/wp-content/uploads/2026/03/Group-1654.png" alt=""></div>
-            <h2>
-                What Changes After Deployment
-            </h2>
-            <div class="desc-box mb-4">
-                <p>When DJI Enterprise is deployed correctly, operational outcomes improve quickly and measurably. These examples show how UK organisations have reduced risk, improved efficiency and delivered better results with Coptrz.</p>
+            <div class="inner position-relative">
+                <h2>
+                    What Changes After Deployment
+                </h2>
+                <div class="desc-box mb-4">
+                    <p>When DJI Enterprise is deployed correctly, operational outcomes improve quickly and measurably. These examples show how UK organisations have reduced risk, improved efficiency and delivered better results with Coptrz.</p>
+                </div>
+                <div class="button-accent col-auto button-box  mt-5"><a class="rounded-10px " href="#Benefits" target="_self">Explore Solutions by Industry</a></div>
             </div>
-            <div class="button-accent col-auto button-box  mt-5"><a class="rounded-10px " href="#Benefits" target="_self">Explore Solutions by Industry</a></div>
         </div>
     </div>
 </section>
