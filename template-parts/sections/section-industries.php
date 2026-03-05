0bfad8018180d0adcc49941b215bf73fe523e8c3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 16:49:36 +0800

    Update section-industries.php

--- template-parts/sections/section-industries.php
+++ template-parts/sections/section-industries.php
@@ -7,6 +7,63 @@
             <div class="desc-box mb-5">
                 <p>Enterprise outcomes differ by environment, accountability and risk profile. DJI Enterprise delivers the strongest results when deployed with sector-specific intent.</p>
             </div>
+
+            <div class="row g-4 same-image-height row-global-post" style="--image-padding: 35%">
+                <div class="col-lg-3 col-md-6">
+                    <div class="post-box column-holder position-relative overflow-hidden h-100 content-margin style-1">
+                        <div class="image-box rounded-corner overflow-hidden">
+                            <a class="text-inherit text-decoration-none" href="https://coptrz.com/guides/surveying-construction-drone-guide-calculator-2/" target="_self">
+                                <img src="https://coptrz.com/wp-content/uploads/2026/03/Construction-surveying.png" alt="">
+                            </a>
+                        </div>
+                        <div class="content-box content-margin  px-20px pb-20px"><a class="text-inherit text-decoration-none" href="https://coptrz.com/guides/surveying-construction-drone-guide-calculator-2/" target="_self">
+                                <h4 class="small-heading">Construction & Surveying</h4>
+                            </a>
+                            <div class="description-box small-text excerpt-no-limit mb-0__related_posts">
+                                <p>
+                                    Accelerate site capture, improve survey accuracy and create auditable progress records that reduce rework, disputes and downstream cost.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="post-box column-holder position-relative overflow-hidden h-100 content-margin style-1">
+                        <div class="image-box rounded-corner overflow-hidden">
+                            <a class="text-inherit text-decoration-none" href="https://coptrz.com/guides/surveying-construction-drone-guide-calculator-2/" target="_self">
+                                <img src="https://coptrz.com/wp-content/uploads/2026/03/Construction-surveying.png" alt="">
+                            </a>
+                        </div>
+                        <div class="content-box content-margin  px-20px pb-20px"><a class="text-inherit text-decoration-none" href="https://coptrz.com/guides/surveying-construction-drone-guide-calculator-2/" target="_self">
+                                <h4 class="small-heading">Construction & Surveying</h4>
+                            </a>
+                            <div class="description-box small-text excerpt-no-limit mb-0__related_posts">
+                                <p>
+                                    Accelerate site capture, improve survey accuracy and create auditable progress records that reduce rework, disputes and downstream cost.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="post-box column-holder position-relative overflow-hidden h-100 content-margin style-1">
+                        <div class="image-box rounded-corner overflow-hidden">
+                            <a class="text-inherit text-decoration-none" href="https://coptrz.com/guides/surveying-construction-drone-guide-calculator-2/" target="_self">
+                                <img src="https://coptrz.com/wp-content/uploads/2026/03/Construction-surveying.png" alt="">
+                            </a>
+                        </div>
+                        <div class="content-box content-margin  px-20px pb-20px"><a class="text-inherit text-decoration-none" href="https://coptrz.com/guides/surveying-construction-drone-guide-calculator-2/" target="_self">
+                                <h4 class="small-heading">Construction & Surveying</h4>
+                            </a>
+                            <div class="description-box small-text excerpt-no-limit mb-0__related_posts">
+                                <p>
+                                    Accelerate site capture, improve survey accuracy and create auditable progress records that reduce rework, disputes and downstream cost.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
