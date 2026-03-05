fdfe68307128df27e8ec400a770dd30346f501b8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 16:11:34 +0800

    Create section-guides.php

---
+++ template-parts/sections/section-guides.php
@@ -0,0 +1,10 @@
+<section class="guides bg-accent-3 py-6">
+    <div class="container">
+        <h2 class="heading-style">Make The Right DJI Decision The First Time</h2>
+        <div class="desc-box">
+            <p>
+                Make The Right DJI Decision The First Time
+            </p>
+        </div>
+    </div>
+</section>
