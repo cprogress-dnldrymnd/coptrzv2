02aa075cfa1a3db95fcece6cfe506092a9e066f9
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 15:46:29 +0800

    Update page-enterprise.php

--- templates/page-enterprise.php
+++ templates/page-enterprise.php
@@ -92,5 +92,6 @@
 get_template_part('template-parts/sections/section-hero');
 get_template_part('template-parts/sections/section-usp');
 get_template_part('template-parts/sections/section-two-columns');
+get_template_part('template-parts/sections/section-number-box');
 ?>
 <?php get_footer(); ?>
