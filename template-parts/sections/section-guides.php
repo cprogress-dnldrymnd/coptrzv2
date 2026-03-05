3fd3ea9480408c7e5eadd946aa8833640ce1c5fc
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 16:12:26 +0800

    Update section-guides.php

--- template-parts/sections/section-guides.php
+++ template-parts/sections/section-guides.php
@@ -1,9 +1,9 @@
-<section class="guides bg-accent-3 py-6">
+<section class="guides bg-accent-3 py-6 text-white">
     <div class="container">
         <h2 class="heading-style">Make The Right DJI Decision The First Time</h2>
         <div class="desc-box">
             <p>
-                Make The Right DJI Decision The First Time
+                Enterprise buying decisions carry consequences. Fixing the wrong choice later is slower, more expensive and harder to justify internally. These outcome-led resources help teams validate decisions upfront, reduce internal friction and avoid regret. Each resource is designed to support internal justification, not just technical selection.
             </p>
         </div>
     </div>
