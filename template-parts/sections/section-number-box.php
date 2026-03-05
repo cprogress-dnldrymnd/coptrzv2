e04f97be1453620b1e9fd3583c70ad1750ab1c02
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 15:42:43 +0800

    Update section-number-box.php

--- template-parts/sections/section-number-box.php
+++ template-parts/sections/section-number-box.php
@@ -1,9 +1,30 @@
-<section class="number-box">
+<section class="number-box-section">
     <div class="container">
         <h2 class="heading-style">How Coptrz Supports You End-To-End</h2>
         <div class="row g-3">
             <div class="col-lg-3">
-                
+                <div class="number-box">
+                    <span class="number">1</span>
+                    <span>Select</span>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="number-box">
+                    <span class="number">1</span>
+                    <span>Select</span>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="number-box">
+                    <span class="number">1</span>
+                    <span>Select</span>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="number-box">
+                    <span class="number">1</span>
+                    <span>Select</span>
+                </div>
             </div>
         </div>
     </div>
