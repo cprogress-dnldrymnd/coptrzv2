607030b2bb57b9d9669e43eb150ec9410f2e6993
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-05 18:06:49 +0800

    cs

---
+++ template-parts/sections/section-testimonials.php
@@ -0,0 +1,38 @@
+<?php
+echo ____post_grid_module(array(
+    'id'                      => 'testimonial-slider',
+    'is_slider'               => true,
+    'number_of_slides'        => 1,
+    'number_of_slides_tablet' => 1,
+    'number_of_slides_mobile' => 1,
+    'post_grid_id'            => 'testimonial-slider',
+    'post_elements'           => array(
+        array(
+            "_type"             => "icon",
+            "icon"              => "271265",
+            "icon_color"        => "text-accent",
+            "icon_color_custom" => "",
+            "icon_width"        => "",
+            "icon_height"       => ""
+        ),
+        array(
+            "_type"              => "custom_field_1",
+            "custom_field_key"   => "_testimonial_content",
+            "custom_field_type"  => "p",
+            "custom_field_class" => "testimonial-content"
+        ),
+        array(
+            "_type"             => "post_title",
+            "text_before"       => "-",
+            "text_after"        => "",
+            "tag"               => "p",
+            "text_color"        => "",
+            "text_color_custom" => ""
+        )
+    ),
+    'post_type'               => array(
+        array(
+            "_type" => "testimonials",
+        )
+    )
+));
--- templates/page-enterprise.php
+++ templates/page-enterprise.php
@@ -380,5 +380,6 @@ get_template_part('template-parts/sections/section-cta');
 get_template_part('template-parts/sections/section-case-study');
 get_template_part('template-parts/sections/section-products');
 get_template_part('template-parts/sections/section-cta-simple');
+get_template_part('template-parts/sections/section-testimonials');
 ?>
 <?php get_footer(); ?>
