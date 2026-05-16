5d865012f30af0d77bcd6dd58ba25018a5d89360
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-16 17:41:46 +0800

    cs

--- includes/hooks.php
+++ includes/hooks.php
@@ -85,6 +85,7 @@ function custom_theme_block_editor_setup()
      */
     add_theme_support('appearance-tools');
     add_theme_support('custom-spacing');
+    add_theme_support( 'align-wide' );
     add_theme_support('disable-custom-colors');
 }
 
