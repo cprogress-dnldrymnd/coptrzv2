56345498b0451ec5ea43bd47cd8565f8f8bee6c5
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-06-03 19:56:24 +0800

    Update _required_files.php

--- includes/_required_files.php
+++ includes/_required_files.php
@@ -1,6 +1,7 @@
 <?php
 require_once('schema.php');
 require_once('post-types.php');
+require_once('svg.php');
 
 /**
  * Conditional File Loading Execution
@@ -13,13 +14,11 @@ if (!$is_blocks_editor) {
     require_once('elements.php');
     require_once('modules.php');
     require_once('ajax.php');
-    require_once('svg.php');
 } else {
-    if(!is_admin()) {
+    if (!is_admin()) {
         require_once('elements.php');
         require_once('modules.php');
         require_once('ajax.php');
-        require_once('svg.php');
     }
 }
 
