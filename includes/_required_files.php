923da2e07d385a1be3dc8365ed4d30034233a3e4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-06-03 19:11:48 +0800

    Update _required_files.php

--- includes/_required_files.php
+++ includes/_required_files.php
@@ -9,7 +9,7 @@ require_once('post-types.php');
  */
 $is_blocks_editor = function_exists('dd_is_blocks_editor_template_active') && dd_is_blocks_editor_template_active();
 
-if (! $is_blocks_editor && is_admin()) {
+if (! $is_blocks_editor && !is_admin()) {
     require_once('elements.php');
     require_once('modules.php');
     require_once('ajax.php');
