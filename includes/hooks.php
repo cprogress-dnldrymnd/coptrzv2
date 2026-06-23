84fc9bcb694527aa81fed445fa6bfbddc878777f
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-06-24 00:57:34 +0800

    remove page template builder style 1

--- includes/hooks.php
+++ includes/hooks.php
@@ -698,4 +698,3 @@ function register_cf7_pdf_url_attribute($out, $pairs, $atts)
     return $out;
 }
 
-
--- includes/page-templates/style-1/sections/section-logos.php
+++
@@ -1,9 +0,0 @@
-<?php $s=$GLOBALS['ep_section']; ?>
-<section class="logos my-6" aria-label="Trusted by logos">
-    <div class="container">
-        <div class="wrap flex-column flex-lg-row position-relative overflow-hidden">
-            <div class="label"><?=esc_html(pts1_field($s,'label','Partners & Clients'))?></div>
-            <?php if($sc=pts1_field($s,'shortcode')): ?><div class="logo-row" aria-hidden="true"><?=do_shortcode($sc)?></div><?php endif; ?>
-        </div>
-    </div>
-</section>
